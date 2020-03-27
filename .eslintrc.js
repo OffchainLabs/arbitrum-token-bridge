af832f228d81ae971c451f3c962912796f886f7e
dzgoldman <dzgoldman@wesleyan.edu>
2020-03-27 12:58:54 -0400

    big commit! linter, basic frontend skeleton - header and tab interfaces, web3/arb providers hook

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+    extends: [
+      "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+      "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+      "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+      "plugin:prettier/recommended" // Enable
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true // Allows for the parsing of JSX
+      }
+    },
+    rules: {
+      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+      // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+    settings: {
+      react: {
+        version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+      }
+    }
+  };
