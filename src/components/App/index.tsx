d9fcbe8c99d9011a56e6a742b9edd2bcd90d5fef
dzgoldman <dzgoldman@wesleyan.edu>
2020-03-27 12:58:54 -0400

    big commit! linter, basic frontend skeleton - header and tab interfaces, web3/arb providers hook

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,60 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import styles from './styles.module.scss'
 import { statement } from '@babel/template'
-import { getInjectedWeb3 } from '../../util/web3'
+import { getInjectedWeb3 } from 'util/web3'
 import * as ethers from 'ethers'
-import * as ArbProviderEthers from "arb-provider-ethers"
-import { ArbProvider } from "arb-provider-ethers"
+import * as ArbProviderEthers from 'arb-provider-ethers'
+import { ArbProvider } from 'arb-provider-ethers'
+import useWeb3Data from 'hooks/web3'
+import Header from 'components/Header'
+import TabsContainer from 'components/TabsContainer'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
-  const [ethAddress, setEthAddress] = useState("")
-  const [ethProvider, setEthProvider] = useState<ethers.providers.JsonRpcProvider>()
-  const [arbProvider, setArbProviderf] = useState<ArbProviderEthers.ArbProvider>()
-  const [arbWallet, setArbWallet] = useState<ethers.Signer>()
-  const [ethWallet, setEthWallet] = useState<ethers.Signer>()
-  const [contracts, setContracts] = useState({})
-  const [web3, setWeb3] = useState<ethers.providers.JsonRpcProvider>()
+  const [{ ethAddress, vmId }, updateWallets] = useWeb3Data()
 
-  useEffect(()=>{
-   (async ()=> {
-    const url =  process.env.REACT_APP_ARB_VALIDATOR_URL || "";
-    let ethProvider = await getInjectedWeb3()
-
-    // const ethProvider = new ethers.providers.Web3Provider(standardProvider)
-    setEthProvider(ethProvider)
-    const arbProvider = new ArbProvider( url,
-      new ethers.providers.Web3Provider(ethProvider)
-    );
-    setArbProviderf(arbProvider)
-  })()
-
-  }, [])
-
-   const updateWallets = ()=>{
-    (async ()=>{
-      let ethWalletSigner = ethWallet
-      if (ethProvider){
-        const ethWallet = ethProvider.getSigner(0)
-        setEthWallet(ethWallet)
-
-        const ethAddress = await ethWallet.getAddress()
-        setEthAddress(ethAddress)
-      }
-
-      if (arbProvider){
-        setArbWallet(arbProvider.getSigner(0))
-      }
-    })()
-
-  }
-  useEffect(updateWallets, [ethProvider, arbProvider ])
-
-  if (!ethAddress){
-    return <div>"loading..."</div>
-  }
-
-  return <div  className={styles.App}>xyz</div>
+  return (
+    <div className="container">
+      <div className="row">
+        <Header ethAddress={ethAddress} vmId={vmId} />
+      </div>
+      <div className="row">
+        <div id="bridgebody">
+          <TabsContainer />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
