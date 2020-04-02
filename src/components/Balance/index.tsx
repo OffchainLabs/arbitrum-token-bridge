4f6da00737f04cb9f54ca1787396e61ae498704a
dzgoldman <dzgoldman@wesleyan.edu>
2020-04-02 12:41:10 -0400

    eth balance UI and move various interfaces to types module

---
+++ src/components/Balance/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+import { Balances } from 'types'
+
+type BalanceProps = {
+  assetName: string
+  balances: Balances
+}
+
+const Balance = ({ balances, assetName }: BalanceProps) => {
+  if (!balances) {
+    return <div></div>
+  }
+  const { balance, arbChainBalance, totalArbBalance, lockBoxBalance } = balances
+  return (
+    <div>
+      <div className="row">
+        <h3>{assetName} Info</h3>
+      </div>
+      <div className="row">
+        User {assetName} Balance on Ethereum: {balance}
+      </div>
+      <div className="row">
+        User {assetName} balance on ArbChain: {arbChainBalance}
+      </div>
+      <div className="row">
+        Total {assetName} held by ArbChain: {totalArbBalance}
+      </div>
+      <div className="row">User Lockbox Balance: {lockBoxBalance}</div>
+    </div>
+  )
+}
+export default Balance
--- src/components/TabsContainer/index.tsx
+++ src/components/TabsContainer/index.tsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react'
-
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
+import Balance from 'components/Balance'
+import { Balances, Template } from 'types'
 
-export default () => {
+type TabProps = {
+  ethBalances: Balances
+}
+const TabsContainer = (data: Template) => {
   const [key, setKey] = useState('eth')
+
   return (
     <Tabs
       id="controlled-tab-example"
@@ -12,14 +17,16 @@ export default () => {
       onSelect={(k: string) => setKey(k)}
     >
       <Tab eventKey="eth" title="ETH">
-        <div>1</div>
+        <Balance assetName={'ETH'} balances={data.ethBalances} />
       </Tab>
       <Tab eventKey="erc20" title="ERC-20">
-        <div>2</div>
+        <div>erc20</div>
       </Tab>
       <Tab eventKey="erc721" title="ERC-721">
-        <div>3</div>
+        <div>erc721</div>
       </Tab>
     </Tabs>
   )
 }
+
+export default TabsContainer
