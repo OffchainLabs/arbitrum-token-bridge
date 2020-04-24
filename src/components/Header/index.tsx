89fb149ab511062ce246462d4dd985da70f2bf03
dzgoldman <dzgoldman@wesleyan.edu>
2020-04-24 16:47:25 -0400

    move totals to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
-
+import {
+  BridgeBalance, ERC721Balance
+} from 'arb-token-bridge'
+import { formatEther } from 'ethers/utils'
 interface Web3Data {
   ethAddress: string
   vmId: string
+  ethBalance: BridgeBalance | undefined
+  erc20Balance: BridgeBalance | undefined
+  erc721Balance: ERC721Balance | undefined
 }
-const Header = ({ ethAddress, vmId }: Web3Data) => {
+const Header = ({ ethAddress, vmId, ethBalance, erc20Balance, erc721Balance }: Web3Data) => {
+  console.warn(erc20Balance);
+
   return (
     <div className="col-lg-12">
       <h1 className="text-center">Arbitrum Token Bridge</h1>
@@ -14,6 +22,18 @@ const Header = ({ ethAddress, vmId }: Web3Data) => {
       <p>
         ArbChain address: <span id="rollupAddress">{vmId}</span>
       </p>
+      {ethBalance && <p>
+        Total ETH On Arb Chain: <span>{formatEther(ethBalance.totalArbBalance)}</span>
+      </p>
+      }
+       {erc20Balance && <p>
+        Total {erc20Balance.asset} On Arb Chain: <span>{formatEther(erc20Balance.totalArbBalance)}</span>
+      </p>
+      }
+      { erc721Balance && <p>
+        Total # of {erc721Balance.asset} NFTs On Arb Chain: <span>{erc721Balance.totalArbTokens.length}</span>
+      </p>
+      }
       <hr />
     </div>
   )
