162d3522e39e18c94a69a8152805e908ed3a3822
dzgoldman <dzgoldman@wesleyan.edu>
2020-07-29 16:49:25 -0400

    update provider strategy

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import Injecter from './components/App/Injecter'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Injecter />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
