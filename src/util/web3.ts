2e9d66a5903bc4aafc56b5ca8bf0307821cc2005
dzgoldman <dzgoldman@wesleyan.edu>
2020-08-05 11:27:11 -0400

    new network detector/injector stuff

--- src/util/web3.ts
+++ src/util/web3.ts
@@ -4,11 +4,13 @@ interface InjectedEthereumProvider
   extends ethers.ethers.providers.AsyncSendable {
   enable?: () => Promise<string[]>
   on: any
+  networkVersion: string
 }
 
 declare global {
   interface Window {
     ethereum?: InjectedEthereumProvider
+    reloadListenerSet: boolean
   }
 }
 
@@ -19,7 +21,7 @@ export function web3Injected(
 }
 
 export async function getInjectedWeb3(): Promise<
-  ethers.providers.JsonRpcProvider
+  [ethers.providers.JsonRpcProvider, string]
 > {
   if (web3Injected(window.ethereum)) {
     try {
@@ -28,8 +30,17 @@ export async function getInjectedWeb3(): Promise<
     } catch (e) {
       throw new Error('Failed to enable window.ethereum: ' + e.message)
     }
+    // TODO: hacky, move this
+    !window.reloadListenerSet &&
+      window.ethereum.on('networkChanged', (chainId: number) => {
+        window.reloadListenerSet = true
+        window.location.reload()
+      })
 
-    return new ethers.providers.Web3Provider(window.ethereum)
+    return [
+      new ethers.providers.Web3Provider(window.ethereum),
+      window.ethereum.networkVersion
+    ]
   }
 
   throw new Error('No web3 injection detected')
