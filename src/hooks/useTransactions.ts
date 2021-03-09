3dc3bae5180672510e97bbf30d6511c6e1275803
dzgoldman <dzgoldman@wesleyan.edu>
2021-03-09 12:34:18 -0800

    lint

--- src/hooks/useTransactions.ts
+++ src/hooks/useTransactions.ts
@@ -19,7 +19,13 @@ export type TxnStatus = 'pending' | 'success' | 'failure' | 'confirmed'
  */
 
 type TransactionBase = {
-  type: 'deposit' | 'withdraw' | 'lockbox' | 'approve'| 'connext-deposit' | 'connext-withdraw'
+  type:
+    | 'deposit'
+    | 'withdraw'
+    | 'lockbox'
+    | 'approve'
+    | 'connext-deposit'
+    | 'connext-withdraw'
   status: TxnStatus
   value: string | null
   txID?: string
