3975fa83c91806f8d4e4df6fd2f9ea236de3166e
dzgoldman <dzgoldman@wesleyan.edu>
2021-06-16 13:17:35 -0400

    add mainnet whitelist, bump version

--- src/util/tokenList.ts
+++ src/util/tokenList.ts
@@ -3,13 +3,16 @@ interface NetWorkTokens {
   blackList: string[]
 }
 
-import { mainnetWhitelist, mainnetBlackList } from './mainnnetTokenLists'
+import { mainnetBlackList } from './mainnnetTokenLists'
 type TokenLists = {
   [key: string]: NetWorkTokens
 }
+
+const mainnetWhitelist = ["0xc944e90c64b2c07662a292be6244bdf05cda44a7", "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", "0x514910771af9ca656af840dff83e8264ecf986ca", "0x2ba592f78db6436527729929aaf6c908497cb200", "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", "0x6810e776880c02933d47db1b9fc05908e5386b96"]
+
 const tokenLists: TokenLists = {
   '1': {
-    whiteList: mainnetWhitelist.map(a => a.address.toLocaleLowerCase()),
+    whiteList: mainnetWhitelist,
     blackList: mainnetBlackList
       .map(a => a.address.toLocaleLowerCase())
       .map(a => a.toLocaleLowerCase())
