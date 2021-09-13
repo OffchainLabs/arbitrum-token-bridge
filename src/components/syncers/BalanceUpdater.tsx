2149dc32f5dd92a46be3e530ba9dbeb501c775ea
dzgoldman <dzgoldman@wesleyan.edu>
2021-09-13 17:24:01 -0400

    remove token balance updater; decrease eth updater window

--- src/components/syncers/BalanceUpdater.tsx
+++ src/components/syncers/BalanceUpdater.tsx
@@ -19,22 +19,17 @@ const BalanceUpdater = (): JSX.Element => {
   }, [bridge])
 
   useEffect(() => {
+    if (latestBridge.current) {
+      latestTokenBridge?.current?.eth?.updateBalances()
+    }
     const interval = setInterval(() => {
       if (latestBridge.current) {
         latestTokenBridge?.current?.eth?.updateBalances()
       }
-    }, 10000)
+    }, 5000)
     return () => clearInterval(interval)
   }, [bridge])
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (latestBridge.current) {
-        latestTokenBridge?.current?.token?.updateBalances()
-      }
-    }, 25000)
-    return () => clearInterval(interval)
-  }, [bridge])
 
   return <></>
 }
