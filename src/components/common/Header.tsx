48c1e901388f22ff5daa0d73da0222225697de94
dzgoldman <dzgoldman@wesleyan.edu>
2021-09-15 19:17:04 -0400

    add TOS link to header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -100,7 +100,7 @@ const Header: React.FC = () => {
                   className="hidden md:inline-block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                   rel="noopener noreferrer"
                 >
-                  Documentation
+                  Docs
                 </a>
                 <a
                   href="/data/token-list-42161.json"
@@ -109,6 +109,14 @@ const Header: React.FC = () => {
                   rel="noopener noreferrer"
                 >
                   Token List
+                </a>
+                <a
+                  href="/tos"
+                  target="_blank"
+                  className="hidden md:inline-block text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  rel="noopener noreferrer"
+                >
+                  Terms of Service
                 </a>
               </div>
             </div>
