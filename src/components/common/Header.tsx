49109bd1e5c27e83038445d22517c9ca2a9747e2
Saša Šijak <me@ssijak.com>
2021-09-15 22:26:55 +0200

    Added explorer menu linking to mainnet and rinkeby explorers

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,5 +1,69 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function ExplorerMenu() {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className=" hidden md:inline-flex text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+          Options
+          <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="z-50 origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="https://explorer.arbitrum.io/"
+                  target="_blank"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                  rel="noreferrer"
+                >
+                  Mainnet
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="https://rinkeby-explorer.arbitrum.io/"
+                  target="_blank"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                  rel="noreferrer"
+                >
+                  Rinkeby
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
 const Header: React.FC = () => {
   return (
     <header>
@@ -29,6 +93,7 @@ const Header: React.FC = () => {
                 >
                   Portal
                 </a>
+                <ExplorerMenu />
                 <a
                   href="https://explorer.arbitrum.io/"
                   target="_blank"
