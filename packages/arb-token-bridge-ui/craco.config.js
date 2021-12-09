b4ca5e6ca0970403543396684373d33a82a2a1b5
fred <fredlacs@gmail.com>
2021-12-09 10:49:20 +0000

    Add bundle size viewer

--- packages/arb-token-bridge-ui/craco.config.js
+++ packages/arb-token-bridge-ui/craco.config.js
@@ -1,7 +1,27 @@
-module.exports = {
-    style: {
-        postcss: {
-      plugins: [require('tailwindcss'), require('autoprefixer')]
-        },
+const webpack = require("webpack")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
+
+module.exports = function ({ env }) {
+  const isProductionBuild = process.env.NODE_ENV === "production"
+  const analyzerMode = process.env.REACT_APP_INTERACTIVE_ANALYZE
+    ? "server"
+    : "static" // : "json"
+
+  const plugins = []
+
+  if (isProductionBuild) {
+    plugins.push(new BundleAnalyzerPlugin({ analyzerMode }))
+  }
+
+  return {
+    webpack: {
+      plugins,
     },
+    style: {
+      postcss: {
+        plugins: [require('tailwindcss'), require('autoprefixer')]
+      },
+    },
+  }
 }
