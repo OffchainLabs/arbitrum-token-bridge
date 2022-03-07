255009107cd34b1d58a14aedca416ecc1d3add8a
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-03-07 15:14:20 +0100

    fix inline svg

--- packages/arb-token-bridge-ui/src/components/TokenModal/TokenImportModal.tsx
+++ packages/arb-token-bridge-ui/src/components/TokenModal/TokenImportModal.tsx
@@ -270,14 +270,14 @@ export function TokenImportModal({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M10 2C5.58172 2 2 5.58172 2 10C2 14.4183 5.58172 18 10 18C14.4183 18 18 14.4183 18 10C18 5.58172 14.4183 2 10 2ZM0 10C0 4.47715 4.47715 0 10 0C15.5228 0 20 4.47715 20 10C20 15.5228 15.5228 20 10 20C4.47715 20 0 15.5228 0 10Z"
                 fill="#CD0000"
               />
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M10 12C9.44771 12 9 11.5523 9 11L9 5C9 4.44772 9.44772 4 10 4C10.5523 4 11 4.44772 11 5L11 11C11 11.5523 10.5523 12 10 12Z"
                 fill="#CD0000"
               />
