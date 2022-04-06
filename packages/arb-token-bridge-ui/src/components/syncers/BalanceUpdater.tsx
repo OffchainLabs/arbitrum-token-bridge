ef94df7059cbe08f9c61a7e2bafe8052be568d84
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-04-06 19:34:26 +0200

    get rid of useInterval

--- packages/arb-token-bridge-ui/src/components/syncers/BalanceUpdater.tsx
+++ packages/arb-token-bridge-ui/src/components/syncers/BalanceUpdater.tsx
@@ -1,9 +1,7 @@
 import { useEffect } from 'react'
-
 import { useLatest } from 'react-use'
 
 import { useAppState } from '../../state'
-import { useInterval } from '../common/Hooks'
 import { useSigners } from '../../hooks/useSigners'
 
 // Updates all balances periodically
@@ -14,9 +12,13 @@ const BalanceUpdater = (): JSX.Element => {
   } = useAppState()
   const latestTokenBridge = useLatest(arbTokenBridge)
 
-  useInterval(() => {
-    latestTokenBridge?.current?.eth?.updateBalances()
-  }, 5000)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      latestTokenBridge?.current?.eth?.updateBalances()
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [])
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -26,6 +28,7 @@ const BalanceUpdater = (): JSX.Element => {
         )
       }
     }, 10000)
+
     return () => clearInterval(interval)
   }, [selectedToken])
 
