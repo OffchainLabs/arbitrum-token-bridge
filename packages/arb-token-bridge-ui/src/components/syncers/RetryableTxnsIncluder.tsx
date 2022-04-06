01ab4ddeb86a664dbb530ba771ecbc7085d051f3
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-04-06 10:13:57 +0200

    fix retryable tx includer

--- packages/arb-token-bridge-ui/src/components/syncers/RetryableTxnsIncluder.tsx
+++ packages/arb-token-bridge-ui/src/components/syncers/RetryableTxnsIncluder.tsx
@@ -1,30 +1,31 @@
-import { useCallback, useEffect, useContext } from 'react'
-import { useActions, useAppState } from '../../state'
-import { BridgeContext } from '../App/App'
-
-import { useInterval } from '../common/Hooks'
+import { useCallback, useEffect } from 'react'
 import { L1TransactionReceipt, L1ToL2MessageStatus } from '@arbitrum/sdk'
 
-const RetryableTxnsIncluder = (): JSX.Element => {
+import { useActions, useAppState } from '../../state'
+
+import { useInterval } from '../common/Hooks'
+import { useSigners } from '../../hooks/useSigners'
+
+export function RetryableTxnsIncluder(): JSX.Element {
   const actions = useActions()
+  const { l1Signer, l2Signer } = useSigners()
   const {
     app: { arbTokenBridge, arbTokenBridgeLoaded }
   } = useAppState()
-  const bridge = useContext(BridgeContext)
 
   const checkAndUpdateFailedRetryables = useCallback(async () => {
-    if (!bridge) {
+    // This should never be the case
+    if (typeof l1Signer === 'undefined' || typeof l2Signer === 'undefined') {
       return
     }
+
     const failedRetryablesToRedeem = actions.app.getFailedRetryablesToRedeem()
 
     for (let depositTx of failedRetryablesToRedeem) {
       const depositTxRec = new L1TransactionReceipt(
-        await bridge.l1Provider.getTransactionReceipt(depositTx.txId)
+        await l1Signer.provider.getTransactionReceipt(depositTx.txId)
       ) //**TODO: not found, i.e., reorg */
-      const l1ToL2Msg = await depositTxRec.getL1ToL2Message(
-        bridge.l2Bridge.l2Signer
-      )
+      const l1ToL2Msg = await depositTxRec.getL1ToL2Message(l2Signer)
       const status = await l1ToL2Msg.status()
       if (status !== L1ToL2MessageStatus.FUNDS_DEPOSITED_ON_L2) {
         arbTokenBridge?.transactions?.updateL1ToL2MsgData(
@@ -34,13 +35,14 @@ const RetryableTxnsIncluder = (): JSX.Element => {
         )
       }
     }
-  }, [arbTokenBridge?.transactions?.addTransactions, bridge])
+  }, [arbTokenBridge?.transactions?.addTransactions, l1Signer, l2Signer])
 
   /**
    * For every L1 deposit, we ensure the relevant L1ToL2MessageIsIncluded
    */
   const checkAndAddMissingL1ToL2Messagges = useCallback(async () => {
-    if (!bridge) {
+    // This should never be the case
+    if (typeof l1Signer === 'undefined' || typeof l2Signer === 'undefined') {
       return
     }
 
@@ -49,18 +51,16 @@ const RetryableTxnsIncluder = (): JSX.Element => {
 
     for (let depositTx of l1DepositsWithUntrackedL2Messages) {
       const depositTxRec = new L1TransactionReceipt(
-        await bridge.l1Provider.getTransactionReceipt(depositTx.txID)
+        await l1Signer.provider.getTransactionReceipt(depositTx.txID)
       ) //**TODO: not found, i.e., reorg */
-      const l1ToL2Msg = await depositTxRec.getL1ToL2Message(
-        bridge.l2Bridge.l2Signer
-      )
+      const l1ToL2Msg = await depositTxRec.getL1ToL2Message(l2Signer)
 
       arbTokenBridge?.transactions?.updateL1ToL2MsgData(
         depositTx.txID,
         l1ToL2Msg
       )
     }
-  }, [arbTokenBridge?.transactions?.addTransactions, bridge])
+  }, [arbTokenBridge?.transactions?.addTransactions, l1Signer, l2Signer])
 
   const { forceTrigger: forceTriggerUpdate } = useInterval(
     checkAndAddMissingL1ToL2Messagges,
@@ -80,5 +80,3 @@ const RetryableTxnsIncluder = (): JSX.Element => {
 
   return <></>
 }
-
-export { RetryableTxnsIncluder }
