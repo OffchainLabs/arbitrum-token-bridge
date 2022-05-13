f0001ab6234966c3b10ed3419a47bcd09e2ec31a
gzeon <95478735+gzeoneth@users.noreply.github.com>
2022-05-13 15:48:26 +0800

    fix: missing seqNum in pending deposit-l1 txs

--- packages/arb-token-bridge-ui/src/components/syncers/PendingTransactionsUpdater.tsx
+++ packages/arb-token-bridge-ui/src/components/syncers/PendingTransactionsUpdater.tsx
@@ -7,6 +7,12 @@ import { useActions, useAppState } from '../../state'
 import { useInterval } from '../common/Hooks'
 import { useNetworksAndSigners } from '../../hooks/useNetworksAndSigners'
 
+import { L1TransactionReceipt } from '@arbitrum/sdk'
+
+interface TransactionReceiptWithSeqNum extends TransactionReceipt {
+  seqNum?: number // for l1-initiati
+}
+
 export function PendingTransactionsUpdater(): JSX.Element {
   const actions = useActions()
   const {
@@ -27,7 +33,21 @@ export function PendingTransactionsUpdater(): JSX.Element {
         return null
       }
 
-      return provider.getTransactionReceipt(tx.txID)
+      if (tx.type == 'deposit-l1' && !tx.seqNum) {
+        // We need to get the seqNum for deposit tx if its missing
+        return provider
+          .getTransactionReceipt(tx.txID)
+          .then(async (txr: TransactionReceiptWithSeqNum) => {
+            const l1ToL2Msg = await new L1TransactionReceipt(
+              txr
+            ).getL1ToL2Message(provider)
+            const seqNum = l1ToL2Msg.messageNumber
+            txr.seqNum = seqNum.toNumber()
+            return txr
+          })
+      } else {
+        return provider.getTransactionReceipt(tx.txID)
+      }
     },
     [l1Signer, l2Signer]
   )
@@ -44,17 +64,23 @@ export function PendingTransactionsUpdater(): JSX.Element {
       // eslint-disable-next-line consistent-return
       return Promise.all(
         pendingTransactions.map((tx: Transaction) => getTransactionReceipt(tx))
-      ).then((txReceipts: (TransactionReceipt | null)[]) => {
-        txReceipts.forEach((txReceipt: TransactionReceipt | null, i) => {
-          if (!txReceipt) {
-            console.info(
-              'Transaction receipt not yet found:',
-              pendingTransactions[i].txID
-            )
-          } else {
-            arbTokenBridge?.transactions?.updateTransaction(txReceipt)
+      ).then((txReceipts: (TransactionReceiptWithSeqNum | null)[]) => {
+        txReceipts.forEach(
+          (txReceipt: TransactionReceiptWithSeqNum | null, i) => {
+            if (!txReceipt) {
+              console.info(
+                'Transaction receipt not yet found:',
+                pendingTransactions[i].txID
+              )
+            } else {
+              arbTokenBridge?.transactions?.updateTransaction(
+                txReceipt,
+                undefined,
+                txReceipt.seqNum
+              )
+            }
           }
-        })
+        )
       })
     }
   }, [getTransactionReceipt, arbTokenBridge, arbTokenBridgeLoaded])
