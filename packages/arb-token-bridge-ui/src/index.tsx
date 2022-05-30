f0bec21af027b750f7f83f67b6222ab51f6600d8
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-05-30 12:24:52 +0200

    add nitro devnet as custom network

--- packages/arb-token-bridge-ui/src/index.tsx
+++ packages/arb-token-bridge-ui/src/index.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom'
 
 import App from './components/App/App'
 import reportWebVitals from './reportWebVitals'
+import { registerNitroDevnet } from './util/networks'
 
 import 'tippy.js/dist/tippy.css'
 import 'tippy.js/themes/light.css'
 
 import './styles/tailwind.css'
+
+registerNitroDevnet()
 
 ReactDOM.render(<App />, document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
