bdb4de463b30971abaf65d7904c1ae1a03eb6a5c
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-06-21 20:15:36 +0200

    fix: don't change to loading state when switching to partner network (#15)

--- packages/arb-token-bridge-ui/src/hooks/useNetworksAndSigners.tsx
+++ packages/arb-token-bridge-ui/src/hooks/useNetworksAndSigners.tsx
@@ -12,6 +12,7 @@ import {
 } from '@ethersproject/providers'
 import { L1Network, L2Network, getL1Network, getL2Network } from '@arbitrum/sdk'
 import { useWallet } from '@arbitrum/use-wallet'
+import { useLatest } from 'react-use'
 
 import { rpcURLs } from '../util/networks'
 import { modalProviderOpts } from '../util/modelProviderOpts'
@@ -94,6 +95,7 @@ export function NetworksAndSignersProvider(
     ...defaults,
     status: defaultStatus
   })
+  const latestResult = useLatest(result)
 
   useEffect(() => {
     async function tryConnect() {
@@ -111,58 +113,75 @@ export function NetworksAndSignersProvider(
   }, [connect])
 
   // TODO: Don't run all of this when an account switch happens. Just derive signers from networks?
-  const update = useCallback((web3Provider: Web3Provider, address: string) => {
-    setResult({ ...defaults, status: UseNetworksAndSignersStatus.LOADING })
+  const update = useCallback(
+    async (web3Provider: Web3Provider, address: string) => {
+      async function isSwitchingToPartnerNetwork() {
+        const nextChainId = (await web3Provider.getNetwork()).chainId
+        const current = latestResult.current
 
-    getL1Network(web3Provider)
-      .then(async l1Network => {
-        // Web3Provider is connected to an L1 network. We instantiate a provider for the L2 network.
-        const [l2NetworkChainId] = l1Network.partnerChainIDs
-        const l2Provider = new JsonRpcProvider(rpcURLs[l2NetworkChainId])
-        const l2Network = await getL2Network(l2Provider)
+        if (current.isConnectedToArbitrum) {
+          return nextChainId === current.l2.network.partnerChainID
+        }
 
-        setResult({
-          status: UseNetworksAndSignersStatus.CONNECTED,
-          l1: {
-            network: l1Network,
-            signer: web3Provider.getSigner(0)
-          },
-          l2: {
-            network: l2Network,
-            signer: l2Provider.getSigner(address!)
-          },
-          isConnectedToArbitrum: false
+        return current.l1.network?.partnerChainIDs.includes(nextChainId)
+      }
+
+      // Don't switch to loading state when switching to partner network
+      if (!(await isSwitchingToPartnerNetwork())) {
+        setResult({ ...defaults, status: UseNetworksAndSignersStatus.LOADING })
+      }
+
+      getL1Network(web3Provider)
+        .then(async l1Network => {
+          // Web3Provider is connected to an L1 network. We instantiate a provider for the L2 network.
+          const [l2NetworkChainId] = l1Network.partnerChainIDs
+          const l2Provider = new JsonRpcProvider(rpcURLs[l2NetworkChainId])
+          const l2Network = await getL2Network(l2Provider)
+
+          setResult({
+            status: UseNetworksAndSignersStatus.CONNECTED,
+            l1: {
+              network: l1Network,
+              signer: web3Provider.getSigner(0)
+            },
+            l2: {
+              network: l2Network,
+              signer: l2Provider.getSigner(address!)
+            },
+            isConnectedToArbitrum: false
+          })
         })
-      })
-      .catch(() => {
-        getL2Network(web3Provider)
-          .then(async l2Network => {
-            // Web3Provider is connected to an L2 network. We instantiate a provider for the L1 network.
-            const l1NetworkChainId = l2Network.partnerChainID
-            const l1Provider = new JsonRpcProvider(rpcURLs[l1NetworkChainId])
-            const l1Network = await getL1Network(l1Provider)
+        .catch(() => {
+          getL2Network(web3Provider)
+            .then(async l2Network => {
+              // Web3Provider is connected to an L2 network. We instantiate a provider for the L1 network.
+              const l1NetworkChainId = l2Network.partnerChainID
+              const l1Provider = new JsonRpcProvider(rpcURLs[l1NetworkChainId])
+              const l1Network = await getL1Network(l1Provider)
 
-            setResult({
-              status: UseNetworksAndSignersStatus.CONNECTED,
-              l1: {
-                network: l1Network,
-                signer: l1Provider.getSigner(address!)
-              },
-              l2: {
-                network: l2Network,
-                signer: web3Provider.getSigner(0)
-              },
-              isConnectedToArbitrum: true
+              setResult({
+                status: UseNetworksAndSignersStatus.CONNECTED,
+                l1: {
+                  network: l1Network,
+                  signer: l1Provider.getSigner(address!)
+                },
+                l2: {
+                  network: l2Network,
+                  signer: web3Provider.getSigner(0)
+                },
+                isConnectedToArbitrum: true
+              })
             })
-          })
-          .catch(() => {
-            setResult({
-              status: UseNetworksAndSignersStatus.NOT_SUPPORTED,
-              ...defaults
+            .catch(() => {
+              setResult({
+                status: UseNetworksAndSignersStatus.NOT_SUPPORTED,
+                ...defaults
+              })
             })
-          })
-      })
-  }, [])
+        })
+    },
+    [latestResult]
+  )
 
   useEffect(() => {
     if (provider && account && network) {
