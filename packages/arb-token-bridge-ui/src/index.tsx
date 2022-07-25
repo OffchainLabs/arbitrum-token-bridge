5da96a7334a31f5a03faac1e0d62fd8fc687e155
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-07-25 20:57:42 +0200

    chore: set up sentry (#32)
    * chore: set up sentry
    
    * fix: update tracesSampleRate to 0.2

--- packages/arb-token-bridge-ui/src/index.tsx
+++ packages/arb-token-bridge-ui/src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
 
 import App from './components/App/App'
 import reportWebVitals from './reportWebVitals'
@@ -19,6 +21,12 @@ registerArbitrumGoerliRollup()
 if (process.env.NODE_ENV === 'development') {
   registerLocalNetwork()
 }
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 0.2
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
 // If you want to start measuring performance in your app, pass a function
