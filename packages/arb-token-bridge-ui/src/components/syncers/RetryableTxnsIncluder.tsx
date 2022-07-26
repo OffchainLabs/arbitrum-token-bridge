3ace9f78736dab20a467a5deeee6e946e2cd6cb2
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-07-26 15:12:26 +0200

    fix: catch tx not found when checking retryables (#339)
    * fix: handle tx not found when checking retryables
    
    * bump version
    
    * remove unnecessary checks
    
    * fix: handle tx not found when checking failed retryables

--- packages/arb-token-bridge-ui/src/components/syncers/RetryableTxnsIncluder.tsx
+++ packages/arb-token-bridge-ui/src/components/syncers/RetryableTxnsIncluder.tsx
@@ -18,19 +18,22 @@ export function RetryableTxnsIncluder(): JSX.Element {
   } = useAppState()
 
   const checkAndUpdateFailedRetryables = useCallback(async () => {
-    // This should never be the case
-    if (typeof l1Signer === 'undefined' || typeof l2Signer === 'undefined') {
-      return
-    }
-
     const failedRetryablesToRedeem = actions.app.getFailedRetryablesToRedeem()
 
     for (let depositTx of failedRetryablesToRedeem) {
-      const depositTxRec = new L1TransactionReceipt(
-        await l1Signer.provider.getTransactionReceipt(depositTx.txId)
-      ) //**TODO: not found, i.e., reorg */
-      const l1ToL2Msg = await depositTxRec.getL1ToL2Message(l2Signer.provider)
+      const depositTxReceipt = await l1Signer.provider.getTransactionReceipt(
+        depositTx.txId
+      )
+
+      // TODO: Handle tx not found
+      if (!depositTxReceipt) {
+        return
+      }
+
+      const l1TxReceipt = new L1TransactionReceipt(depositTxReceipt)
+      const l1ToL2Msg = await l1TxReceipt.getL1ToL2Message(l2Signer.provider)
       const status = await l1ToL2Msg.status()
+
       if (status !== L1ToL2MessageStatus.FUNDS_DEPOSITED_ON_L2) {
         arbTokenBridge?.transactions?.fetchAndUpdateL1ToL2MsgStatus(
           depositTx.txId,
@@ -46,21 +49,23 @@ export function RetryableTxnsIncluder(): JSX.Element {
    * For every L1 deposit, we ensure the relevant L1ToL2MessageIsIncluded
    */
   const checkAndAddMissingL1ToL2Messagges = useCallback(async () => {
-    // This should never be the case
-    if (typeof l1Signer === 'undefined' || typeof l2Signer === 'undefined') {
-      return
-    }
-
     const l1DepositsWithUntrackedL2Messages =
       actions.app.l1DepositsWithUntrackedL2Messages()
 
     for (let depositTx of l1DepositsWithUntrackedL2Messages) {
-      const depositTxRec = new L1TransactionReceipt(
-        await l1Signer.provider.getTransactionReceipt(depositTx.txID)
-      ) //**TODO: not found, i.e., reorg */
+      const depositTxReceipt = await l1Signer.provider.getTransactionReceipt(
+        depositTx.txID
+      )
+
+      // TODO: Handle tx not found
+      if (!depositTxReceipt) {
+        return
+      }
+
+      const l1TxReceipt = new L1TransactionReceipt(depositTxReceipt)
 
       if (depositTx.assetType === AssetType.ETH) {
-        const [ethDepositMessage] = await depositTxRec.getEthDepositMessages(
+        const [ethDepositMessage] = await l1TxReceipt.getEthDepositMessages(
           l2Signer.provider
         )
 
@@ -69,7 +74,7 @@ export function RetryableTxnsIncluder(): JSX.Element {
           ethDepositMessage
         )
       } else {
-        const l1ToL2Msg = await depositTxRec.getL1ToL2Message(l2Signer)
+        const l1ToL2Msg = await l1TxReceipt.getL1ToL2Message(l2Signer)
         const status = await l1ToL2Msg.status()
 
         arbTokenBridge?.transactions?.fetchAndUpdateL1ToL2MsgStatus(
