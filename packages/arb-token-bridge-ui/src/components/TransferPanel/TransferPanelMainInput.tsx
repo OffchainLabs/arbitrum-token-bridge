c0b3bea0840de4126f648887a0b1aaca3c8c76ef
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-08-08 09:45:50 +0200

    extract main input component

---
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMainInput.tsx
@@ -0,0 +1,71 @@
+import Loader from 'react-loader-spinner'
+
+import { TokenButton } from './TokenButton'
+
+type MaxButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading: boolean
+}
+
+function MaxButton(props: MaxButtonProps) {
+  const { loading, className = '' } = props
+
+  if (loading) {
+    return (
+      <div className="px-3">
+        <Loader type="TailSpin" color="#999999" height={16} width={16} />
+      </div>
+    )
+  }
+
+  return (
+    <button
+      type="button"
+      className={`p-2 text-sm font-light text-gray-9 ${className}`}
+      {...props}
+    >
+      MAX
+    </button>
+  )
+}
+
+export type TransferPanelMainInputProps =
+  React.InputHTMLAttributes<HTMLInputElement> & {
+    errorMessage?: string
+    maxButtonProps: MaxButtonProps & {
+      visible: boolean
+    }
+  }
+
+export function TransferPanelMainInput(props: TransferPanelMainInputProps) {
+  const { errorMessage, maxButtonProps } = props
+  const { visible: maxButtonVisible } = maxButtonProps
+
+  const borderClassName =
+    typeof errorMessage !== 'undefined'
+      ? 'border border-[#cd0000]'
+      : 'border border-gray-9'
+
+  return (
+    <>
+      <div
+        className={`flex h-16 flex-row items-center rounded-lg bg-white ${borderClassName}`}
+      >
+        <TokenButton />
+        <div className="h-full border-r border-gray-4" />
+        <div className="flex h-full flex-grow flex-row items-center justify-center px-3">
+          <input
+            type="number"
+            placeholder="Enter amount"
+            className="h-full w-full bg-transparent text-lg font-light placeholder:text-gray-9 lg:text-3xl"
+            {...props}
+          />
+          {maxButtonVisible && <MaxButton {...maxButtonProps} />}
+        </div>
+      </div>
+
+      {typeof errorMessage !== 'undefined' && (
+        <span className="text-sm text-brick">{errorMessage}</span>
+      )}
+    </>
+  )
+}
