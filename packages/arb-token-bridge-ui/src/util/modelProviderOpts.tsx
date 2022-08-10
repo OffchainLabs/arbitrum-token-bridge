7ed33e20f9ec27611d86e0f970275a3122dc7f6e
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-08-10 14:59:23 +0200

    fix: add missing options for WalletConnect

--- packages/arb-token-bridge-ui/src/util/modelProviderOpts.tsx
+++ packages/arb-token-bridge-ui/src/util/modelProviderOpts.tsx
@@ -1,24 +1,19 @@
-// @ts-ignore
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import WalletLink from 'walletlink'
 
-import { rpcURLs } from './networks'
+import { ChainId, rpcURLs } from './networks'
 
 export const modalProviderOpts = {
   cacheProvider: true,
   providerOptions: {
-    // mewconnect: {
-    //   package: MewConnect,
-    //   options: {
-    //     infuraId: process.env.REACT_APP_INFURA_KEY // required
-    //   }
-    // },
     walletconnect: {
       package: WalletConnectProvider,
       options: {
         rpc: {
-          42161: rpcURLs[42161],
-          421611: rpcURLs[421611]
+          [ChainId.ArbitrumOne]: rpcURLs[ChainId.ArbitrumOne],
+          [ChainId.ArbitrumNova]: rpcURLs[ChainId.ArbitrumNova],
+          [ChainId.ArbitrumRinkeby]: rpcURLs[ChainId.ArbitrumRinkeby],
+          [ChainId.ArbitrumGoerli]: rpcURLs[ChainId.ArbitrumGoerli]
         },
         infuraId: process.env.REACT_APP_INFURA_KEY // required
       }
@@ -26,15 +21,12 @@ export const modalProviderOpts = {
     walletlink: {
       package: WalletLink,
       options: {
-        appName: 'Arb Bridge', // Required
+        appName: 'Arbitrum Bridge', // Required
         infuraId: process.env.REACT_APP_INFURA_KEY, // Required unless you provide a JSON RPC url; see `rpc` below
-        chainId: 42161, // Optional. It defaults to 1 if not provided
+        chainId: ChainId.ArbitrumOne, // Optional. It defaults to 1 if not provided
         appLogoUrl: null, // Optional. Application logo image URL. favicon is used if unspecified
         darkMode: false // Optional. Use dark theme, defaults to false
       }
     }
-    // authereum: {
-    //   package: Authereum
-    // }
   }
 }
