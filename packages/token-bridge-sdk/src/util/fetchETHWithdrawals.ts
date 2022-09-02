c19129aef505428af2b65792626c9e3a955c910c
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-09-02 11:20:28 +0200

    add jsdoc

--- packages/token-bridge-sdk/src/util/fetchETHWithdrawals.ts
+++ packages/token-bridge-sdk/src/util/fetchETHWithdrawals.ts
@@ -3,10 +3,20 @@ import { L2ToL1MessageReader } from '@arbitrum/sdk'
 
 import { getETHWithdrawals as graph_getETHWithdrawals } from './graph'
 
+/**
+ * Fetches initiated ETH withdrawals from subgraph in range of [fromBlock, toBlock].
+ *
+ * @param query Query params
+ * @param query.address Account address
+ * @param query.fromBlock Start at this block number (including)
+ * @param query.toBlock Stop at this block number (including)
+ * @param query.l1NetworkId Chain id for the corresponding L1 network
+ */
 export function fetchETHWithdrawalsFromSubgraph({
   address,
   fromBlock,
   toBlock,
+  // TODO: Change this to the L2 network once we have all subgraphs up
   l1NetworkId
 }: {
   address: string
@@ -22,6 +32,15 @@ export function fetchETHWithdrawalsFromSubgraph({
   )
 }
 
+/**
+ * Fetches initiated ETH withdrawals from event logs in range of [fromBlock, toBlock].
+ *
+ * @param query Query params
+ * @param query.address Account address
+ * @param query.fromBlock Start at this block number (including)
+ * @param query.toBlock Stop at this block number (including)
+ * @param query.l2Provider Provider for the L2 network
+ */
 export function fetchETHWithdrawalsFromEventLogs({
   address,
   fromBlock,
