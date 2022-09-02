32ec4f599ab2c24baab1a6ba89954bca533a28f9
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-09-02 11:10:29 +0200

    extract method for fetching eth withdrawals from subgraph

--- packages/token-bridge-sdk/src/util/__test__/fetchETHWithdrawals.test.ts
+++ packages/token-bridge-sdk/src/util/__test__/fetchETHWithdrawals.test.ts
@@ -1,12 +1,60 @@
 import { JsonRpcProvider } from '@ethersproject/providers'
+import 'cross-fetch/polyfill'
 
-import { fetchETHWithdrawalsFromEventLogs } from '../fetchETHWithdrawals'
+import {
+  fetchETHWithdrawalsFromSubgraph,
+  fetchETHWithdrawalsFromEventLogs
+} from '../fetchETHWithdrawals'
 
 const l2Provider = new JsonRpcProvider('https://rinkeby.arbitrum.io/rpc')
 
-function tx(txHash: string) {
-  return { transactionHash: txHash }
-}
+describe('fetchETHWithdrawalsFromSubgraph', () => {
+  it('correctly fetches no ETH withdrawals from subgraph', async () => {
+    const result = await fetchETHWithdrawalsFromSubgraph({
+      address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
+      fromBlock: 0,
+      toBlock: 11110000,
+      l1NetworkId: 4
+    })
+
+    expect(result.length).toEqual(0)
+  })
+
+  it('correctly fetches some ETH withdrawals from subgraph', async () => {
+    const result = await fetchETHWithdrawalsFromSubgraph({
+      address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
+      fromBlock: 11110000,
+      toBlock: 12055296,
+      l1NetworkId: 4
+    })
+
+    expect(result.length).toEqual(5)
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          l2TxHash:
+            '0xdc8f593a5c19f2133e59c6e332dee0f330d977ffa11387eee2cfc55cee1722e7'
+        }),
+        expect.objectContaining({
+          l2TxHash:
+            '0xb482555a63edc154035834a7380ae3787c0126730228d4e1dce97520f25ed0f7'
+        }),
+        expect.objectContaining({
+          l2TxHash:
+            '0xa639f70688a095ce3134c4768de5ef0e2fbad804ca4bdee8dc56131b4500bcb9'
+        }),
+        expect.objectContaining({
+          l2TxHash:
+            '0xc3636c940f800ccc14e83a35448cc62ab9c5bd9bcdb25cc451769df9bd0260e0'
+        }),
+        expect.objectContaining({
+          l2TxHash:
+            '0x821b91067d86d4081639183e29d00f1e553370f979514337f1aa2b867250a269'
+        })
+      ])
+    )
+  })
+})
 
 describe('fetchETHWithdrawalsFromEventLogs', () => {
   it('correctly fetches no ETH withdrawals from event logs', async () => {
@@ -31,31 +79,26 @@ describe('fetchETHWithdrawalsFromEventLogs', () => {
     expect(result.length).toEqual(5)
     expect(result).toEqual(
       expect.arrayContaining([
-        expect.objectContaining(
-          tx(
+        expect.objectContaining({
+          transactionHash:
             '0xdc8f593a5c19f2133e59c6e332dee0f330d977ffa11387eee2cfc55cee1722e7'
-          )
-        ),
-        expect.objectContaining(
-          tx(
+        }),
+        expect.objectContaining({
+          transactionHash:
             '0xb482555a63edc154035834a7380ae3787c0126730228d4e1dce97520f25ed0f7'
-          )
-        ),
-        expect.objectContaining(
-          tx(
+        }),
+        expect.objectContaining({
+          transactionHash:
             '0xa639f70688a095ce3134c4768de5ef0e2fbad804ca4bdee8dc56131b4500bcb9'
-          )
-        ),
-        expect.objectContaining(
-          tx(
+        }),
+        expect.objectContaining({
+          transactionHash:
             '0xc3636c940f800ccc14e83a35448cc62ab9c5bd9bcdb25cc451769df9bd0260e0'
-          )
-        ),
-        expect.objectContaining(
-          tx(
+        }),
+        expect.objectContaining({
+          transactionHash:
             '0x821b91067d86d4081639183e29d00f1e553370f979514337f1aa2b867250a269'
-          )
-        )
+        })
       ])
     )
   })
--- packages/token-bridge-sdk/src/util/fetchETHWithdrawals.ts
+++ packages/token-bridge-sdk/src/util/fetchETHWithdrawals.ts
@@ -1,7 +1,28 @@
 import { Provider, BlockTag } from '@ethersproject/providers'
 import { L2ToL1MessageReader } from '@arbitrum/sdk'
 
-export async function fetchETHWithdrawalsFromEventLogs({
+import { getETHWithdrawals as graph_getETHWithdrawals } from './graph'
+
+export function fetchETHWithdrawalsFromSubgraph({
+  address,
+  fromBlock,
+  toBlock,
+  l1NetworkId
+}: {
+  address: string
+  fromBlock: number
+  toBlock: number
+  l1NetworkId: number
+}) {
+  return graph_getETHWithdrawals(
+    address,
+    fromBlock,
+    toBlock,
+    String(l1NetworkId)
+  )
+}
+
+export function fetchETHWithdrawalsFromEventLogs({
   address,
   fromBlock,
   toBlock,
