17aa39243bde389e03a5e8aaa2e3e9c433a57c79
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-09-02 11:42:19 +0200

    moar tests

--- packages/token-bridge-sdk/src/util/__test__/fetchETHWithdrawals.test.ts
+++ packages/token-bridge-sdk/src/util/__test__/fetchETHWithdrawals.test.ts
@@ -9,7 +9,7 @@ import {
 const l2Provider = new JsonRpcProvider('https://rinkeby.arbitrum.io/rpc')
 
 describe('fetchETHWithdrawalsFromSubgraph', () => {
-  it('correctly fetches no ETH withdrawals from subgraph', async () => {
+  it('fetches no ETH withdrawals from subgraph pre-nitro', async () => {
     const result = await fetchETHWithdrawalsFromSubgraph({
       address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
       fromBlock: 0,
@@ -20,7 +20,7 @@ describe('fetchETHWithdrawalsFromSubgraph', () => {
     expect(result.length).toEqual(0)
   })
 
-  it('correctly fetches some ETH withdrawals from subgraph', async () => {
+  it('fetches some ETH withdrawals from subgraph pre-nitro', async () => {
     const result = await fetchETHWithdrawalsFromSubgraph({
       address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
       fromBlock: 11110000,
@@ -54,10 +54,33 @@ describe('fetchETHWithdrawalsFromSubgraph', () => {
       ])
     )
   })
+
+  it('fetches some ETH withdrawals from subgraph pre-nitro excluding post-nitro', async () => {
+    const result = await fetchETHWithdrawalsFromSubgraph({
+      address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
+      fromBlock: 13744993,
+      toBlock: 13956985,
+      l1NetworkId: 4
+    })
+
+    expect(result.length).toEqual(2)
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          l2TxHash:
+            '0x72ba5c0babbdea27a4b405b041b307b8a9bd420ec8f716071136b51fc660ad2c'
+        }),
+        expect.objectContaining({
+          l2TxHash:
+            '0x9dcdafe1f4cc1b5cf3b568ed3a4b6529dcbde5c71dbe2b6ded624e3b78056b76'
+        })
+      ])
+    )
+  })
 })
 
 describe('fetchETHWithdrawalsFromEventLogs', () => {
-  it('correctly fetches no ETH withdrawals from event logs', async () => {
+  it('fetches no ETH withdrawals from event logs pre-nitro', async () => {
     const result = await fetchETHWithdrawalsFromEventLogs({
       address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
       fromBlock: 0,
@@ -68,7 +91,7 @@ describe('fetchETHWithdrawalsFromEventLogs', () => {
     expect(result.length).toEqual(0)
   })
 
-  it('correctly fetches some ETH withdrawals from event logs', async () => {
+  it('fetches some ETH withdrawals from event logs pre-nitro', async () => {
     const result = await fetchETHWithdrawalsFromEventLogs({
       address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
       fromBlock: 11110000,
@@ -98,6 +121,41 @@ describe('fetchETHWithdrawalsFromEventLogs', () => {
         expect.objectContaining({
           transactionHash:
             '0x821b91067d86d4081639183e29d00f1e553370f979514337f1aa2b867250a269'
+        })
+      ])
+    )
+  })
+
+  it('fetches some ETH withdrawals from event logs pre-nitro and post-nitro', async () => {
+    const result = await fetchETHWithdrawalsFromEventLogs({
+      address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
+      fromBlock: 13744993,
+      toBlock: 13956985,
+      l2Provider
+    })
+
+    expect(result.length).toEqual(5)
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          transactionHash:
+            '0x72ba5c0babbdea27a4b405b041b307b8a9bd420ec8f716071136b51fc660ad2c'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0x9dcdafe1f4cc1b5cf3b568ed3a4b6529dcbde5c71dbe2b6ded624e3b78056b76'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0xa0f9390177977fc0abf839c36808e853e1213e7583b48d2d48341fa41d054732'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0xbeba9bc68ad640f6b946d3cd157dd51b3f272072c6a31234e5706e00c1bdb40b'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0x050fed85e2b48d4937663a9597d60a97929150072e3e9403b1f07dbdf080a9af'
         })
       ])
     )
