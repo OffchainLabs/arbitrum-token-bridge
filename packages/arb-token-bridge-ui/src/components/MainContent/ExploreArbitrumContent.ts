0dadc232851393b40fe9e8daaf69eeac8bbe2567
Fionna Chan <fionnacst@gmail.com>
2022-09-07 11:55:41 +0000

    chore: compress images

--- packages/arb-token-bridge-ui/src/components/MainContent/ExploreArbitrumContent.ts
+++ packages/arb-token-bridge-ui/src/components/MainContent/ExploreArbitrumContent.ts
@@ -4,54 +4,54 @@ export const ExploreArbitrumContent = {
       name: 'Uniswap',
       description: 'Swap in and out of your favorite tokens on Arbitrum',
       href: 'https://uniswap.org',
-      imageSrc: '/images/explore-arbitrum/defi/uniswap.png'
+      imageSrc: '/images/explore-arbitrum/defi/uniswap.jpg'
     },
     {
       name: 'SushiSwap',
       description: 'Swap in and out of your favorite tokens on Arbitrum',
       href: 'https://sushi.com',
-      imageSrc: '/images/explore-arbitrum/defi/sushiswap.png'
+      imageSrc: '/images/explore-arbitrum/defi/sushiswap.jpg'
     },
     {
       name: 'Curve',
       description:
         'Swap in and out of your favorite stablecoins & tokens on Arbitrum; provide liquidity and earn yields',
       href: 'https://arbitrum.curve.fi',
-      imageSrc: '/images/explore-arbitrum/defi/curve.png'
+      imageSrc: '/images/explore-arbitrum/defi/curve.jpg'
     },
     {
       name: 'Aave',
       description: 'Lend and borrow against your crypto',
       href: 'https://app.aave.com/?marketName=proto_arbitrum_v3',
-      imageSrc: '/images/explore-arbitrum/defi/aave.png'
+      imageSrc: '/images/explore-arbitrum/defi/aave.jpg'
     },
     {
       name: 'GMX',
       description:
         'Go long or short on your crypto with GMX’s perpetual exchange',
       href: 'https://gmx.io',
-      imageSrc: '/images/explore-arbitrum/defi/gmx.png'
+      imageSrc: '/images/explore-arbitrum/defi/gmx.jpg'
     },
     {
       name: 'Dopex',
       description:
         'Bet on market trends using Dopex’s custom options vaults on Arbitrum',
       href: 'https://www.dopex.io',
-      imageSrc: '/images/explore-arbitrum/defi/dopex.png'
+      imageSrc: '/images/explore-arbitrum/defi/dopex.jpg'
     },
     {
       name: 'TreasureDAO',
       description:
         'Engage your inner gamer with Treasure’s decentralized video game console and ecosystem of metaverses connected by $MAGIC',
       href: 'https://marketplace.treasure.lol',
-      imageSrc: '/images/explore-arbitrum/defi/treasuredao.png'
+      imageSrc: '/images/explore-arbitrum/defi/treasuredao.jpg'
     },
     {
       name: 'Tracer DAO',
       description:
         'Go long or short on your crypto with Tracer’s perpetual tokens',
       href: 'https://tracer.finance',
-      imageSrc: '/images/explore-arbitrum/defi/tracerdao.png'
+      imageSrc: '/images/explore-arbitrum/defi/tracerdao.jpg'
     },
     {
       name: 'Balancer',
@@ -65,14 +65,14 @@ export const ExploreArbitrumContent = {
       description:
         'Bet on market trends using Jones DAO’s options vaults on Arbitrum',
       href: 'https://jonesdao.io/vaults',
-      imageSrc: '/images/explore-arbitrum/defi/jonesdao.png'
+      imageSrc: '/images/explore-arbitrum/defi/jonesdao.jpg'
     },
     {
       name: '1inch',
       description:
         'Swap in and out of your favorite tokens on Arbitrum;  minimize slippage with 1inch’s swapping platform aggregator',
       href: 'https://app.1inch.io/#/42161',
-      imageSrc: '/images/explore-arbitrum/defi/1inch.png'
+      imageSrc: '/images/explore-arbitrum/defi/1inch.jpg'
     },
     {
       name: 'Beefy Finance',
@@ -86,7 +86,7 @@ export const ExploreArbitrumContent = {
       description:
         'Optimize your yields by auto-compounding your crypto assets for higher APYs',
       href: 'https://yearn.finance',
-      imageSrc: '/images/explore-arbitrum/defi/yearn-finance.png'
+      imageSrc: '/images/explore-arbitrum/defi/yearn-finance.jpg'
     },
     {
       name: 'KyberSwap',
@@ -108,34 +108,34 @@ export const ExploreArbitrumContent = {
       description:
         'Collect progressively-evolving NFTs that live in the smolverse; community-driven and built on good vibes',
       href: 'https://www.smolverse.lol',
-      imageSrc: '/images/explore-arbitrum/nft/smol-brains.png'
+      imageSrc: '/images/explore-arbitrum/nft/smol-brains.jpg'
     },
     {
       name: 'Battlefly',
       description: 'Collect the NFTs behind the famous PvP/P2E GameFi project',
       href: 'https://battlefly.game',
-      imageSrc: '/images/explore-arbitrum/nft/battlefly.png'
+      imageSrc: '/images/explore-arbitrum/nft/battlefly.jpg'
     },
     {
       name: 'Bridgeworld',
       description:
         'Play a game of strategic commerce, trade, and social coordination',
       href: 'https://bridgeworld.treasure.lol',
-      imageSrc: '/images/explore-arbitrum/nft/bridgeworld.png'
+      imageSrc: '/images/explore-arbitrum/nft/bridgeworld.jpg'
     },
     {
       name: 'Diamond Pepes',
       description:
         'Collect one of 2222 Diamond Pepes; pepes were forged from the diamond hands within the Dopex community',
       href: 'https://app.dopex.io/nfts/diamondpepes',
-      imageSrc: '/images/explore-arbitrum/nft/diamond-pepes.png'
+      imageSrc: '/images/explore-arbitrum/nft/diamond-pepes.jpg'
     },
     {
       name: 'GMX Blueberry Club',
       description:
         'Collect one of 10,000 Blueberry NFTs dedicated to GMX, the biggest perpetual protocol on Arbitrum',
       href: 'https://blueberry.club',
-      imageSrc: '/images/explore-arbitrum/nft/gmx-blueberry-club.png'
+      imageSrc: '/images/explore-arbitrum/nft/gmx-blueberry-club.jpg'
     },
     {
       name: 'Toadstoolz',
@@ -149,20 +149,20 @@ export const ExploreArbitrumContent = {
       description:
         'Collect one of 6,666 Smol Bodies, NFTs that inhabit gyms and stake plates to get swol',
       href: 'https://www.smolverse.lol',
-      imageSrc: '/images/explore-arbitrum/nft/smol-bodies.png'
+      imageSrc: '/images/explore-arbitrum/nft/smol-bodies.jpg'
     },
     {
       name: 'Tales of Elleria',
       description:
         'Send your heroes on assignments and quests to obtain Medals ($MEDALS) and Ellerium ($ELM) used solely in game',
       href: 'https://www.talesofelleria.com',
-      imageSrc: '/images/explore-arbitrum/nft/tales-of-elleria.png'
+      imageSrc: '/images/explore-arbitrum/nft/tales-of-elleria.jpg'
     },
     {
       name: 'RandomWalkNFT',
       description: 'Collect beautiful, generative art on Arbitrum',
       href: 'https://www.randomwalknft.com',
-      imageSrc: '/images/explore-arbitrum/nft/randomwalknft.png'
+      imageSrc: '/images/explore-arbitrum/nft/randomwalknft.jpg'
     },
     {
       name: 'City Clash',
@@ -189,28 +189,28 @@ export const ExploreArbitrumContent = {
       description:
         'Collect one of 8055 donkeys trying to find their way back home on Arbitrum',
       href: 'https://www.thelostdonkeys.com',
-      imageSrc: '/images/explore-arbitrum/nft/the-lost-donkeys.png'
+      imageSrc: '/images/explore-arbitrum/nft/the-lost-donkeys.jpg'
     },
     {
       name: 'Realm',
       description:
         'Collect NFTs to participate in Realm’s world-building and exploration-based decentralized game',
       href: 'https://www.rlm.land',
-      imageSrc: '/images/explore-arbitrum/nft/realm.png'
+      imageSrc: '/images/explore-arbitrum/nft/realm.jpg'
     },
     {
       name: 'SmithyDAO',
       description:
         'Join the world of Smithonia; home of Smithonian Weapons and interoperable NFTs',
       href: 'https://smithydao.lol',
-      imageSrc: '/images/explore-arbitrum/nft/smithydao.png'
+      imageSrc: '/images/explore-arbitrum/nft/smithydao.jpg'
     },
     {
       name: 'Mithical',
       description:
         'Collect from 1000 unique Samurais spawned in the mithical realm of Yamata; as uncharted lands are explored, new collections are unlocked',
       href: 'https://www.mithical.io',
-      imageSrc: '/images/explore-arbitrum/nft/mithical.png'
+      imageSrc: '/images/explore-arbitrum/nft/mithical.jpg'
     },
     {
       name: 'Farmland',
