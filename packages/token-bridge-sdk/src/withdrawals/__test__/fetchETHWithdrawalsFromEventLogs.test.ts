6317801ad03d5af879b2ffd7f6e138056232040c
Bartek <bartlomiejkramek@outlook.com>
2022-09-23 11:17:36 +0200

    revert tests

--- packages/token-bridge-sdk/src/withdrawals/__test__/fetchETHWithdrawalsFromEventLogs.test.ts
+++ packages/token-bridge-sdk/src/withdrawals/__test__/fetchETHWithdrawalsFromEventLogs.test.ts
@@ -5,7 +5,53 @@ import { fetchETHWithdrawalsFromEventLogs } from '../fetchETHWithdrawalsFromEven
 const l2Provider = new JsonRpcProvider('https://rinkeby.arbitrum.io/rpc')
 
 describe('fetchETHWithdrawalsFromEventLogs', () => {
-  it('fetches some ETH withdrawals from event logs', async () => {
+  it('fetches no ETH withdrawals from event logs pre-nitro', async () => {
+    const result = await fetchETHWithdrawalsFromEventLogs({
+      address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
+      fromBlock: 0,
+      toBlock: 11110000,
+      l2Provider
+    })
+
+    expect(result).toHaveLength(0)
+  })
+
+  it('fetches some ETH withdrawals from event logs pre-nitro', async () => {
+    const result = await fetchETHWithdrawalsFromEventLogs({
+      address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
+      fromBlock: 11110000,
+      toBlock: 12055296,
+      l2Provider
+    })
+
+    expect(result).toHaveLength(5)
+    expect(result).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          transactionHash:
+            '0xdc8f593a5c19f2133e59c6e332dee0f330d977ffa11387eee2cfc55cee1722e7'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0xb482555a63edc154035834a7380ae3787c0126730228d4e1dce97520f25ed0f7'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0xa639f70688a095ce3134c4768de5ef0e2fbad804ca4bdee8dc56131b4500bcb9'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0xc3636c940f800ccc14e83a35448cc62ab9c5bd9bcdb25cc451769df9bd0260e0'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0x821b91067d86d4081639183e29d00f1e553370f979514337f1aa2b867250a269'
+        })
+      ])
+    )
+  })
+
+  it('fetches some ETH withdrawals from event logs pre-nitro and post-nitro', async () => {
     const result = await fetchETHWithdrawalsFromEventLogs({
       address: '0x41C966f99De0cA6F6531fbcAc9Db7eaBDF119744',
       fromBlock: 13744993,
@@ -13,9 +59,17 @@ describe('fetchETHWithdrawalsFromEventLogs', () => {
       l2Provider
     })
 
-    expect(result).toHaveLength(3)
+    expect(result).toHaveLength(5)
     expect(result).toEqual(
       expect.arrayContaining([
+        expect.objectContaining({
+          transactionHash:
+            '0x72ba5c0babbdea27a4b405b041b307b8a9bd420ec8f716071136b51fc660ad2c'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0x9dcdafe1f4cc1b5cf3b568ed3a4b6529dcbde5c71dbe2b6ded624e3b78056b76'
+        }),
         expect.objectContaining({
           transactionHash:
             '0xa0f9390177977fc0abf839c36808e853e1213e7583b48d2d48341fa41d054732'
