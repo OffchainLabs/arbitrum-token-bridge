ed13c6ce7e34cca0926fa559ee2b8ffd29d29a28
Christophe Deveaux <contact@christophedeveaux.com>
2022-10-28 14:12:23 +0200

    fix: token import dialog not loading when no lists selected (#477)
    * fix: Fix token import dialog not loading when no lists are selected
    
    * Remove leftover console.log
    
    * Update packages/arb-token-bridge-ui/src/components/TransferPanel/TokenButton.tsx
    
    Co-authored-by: Dragiša Spasojević <dragisaspasojevic@gmail.com>
    
    * Fix issues on Goerli where bridgeTokens is never initialized
    
    * Fix useMemo dependency array
    
    * remove goerli special case
    
    * Add support for L2 address query param for token import dialog
    
    * Add hook to fetch token data from L1/2 address
    
    * Simplify hook to only return L1 address
    
    * Remove unecessary SWR options
    
    * Fix PR comment
    
    * Use SWR immutable to avoid refetching
    
    Co-authored-by: Dragiša Spasojević <dragisaspasojevic@gmail.com>

---
+++ packages/arb-token-bridge-ui/src/hooks/useERC20L1Address.ts
@@ -0,0 +1,39 @@
+import { Provider } from '@ethersproject/providers'
+import useSWRImmutable from 'swr/immutable'
+import { getL1ERC20Address } from 'token-bridge-sdk'
+
+/**
+ * Returns L1 address
+ *
+ * @param eitherL1OrL2Address string Token address (on L1 or L2)
+ * @param provider Provider
+ * @returns
+ */
+const useERC20L1Address = ({
+  eitherL1OrL2Address,
+  l2Provider
+}: {
+  eitherL1OrL2Address: string
+  l2Provider: Provider
+}) => {
+  const { data = null, isValidating } = useSWRImmutable(
+    ['useERC20L1Address', eitherL1OrL2Address],
+    async () => {
+      const address =
+        (await getL1ERC20Address({
+          erc20L2Address: eitherL1OrL2Address,
+          l2Provider
+        })) ?? eitherL1OrL2Address
+      return address.toLowerCase()
+    },
+    {
+      shouldRetryOnError: true,
+      errorRetryCount: 2,
+      errorRetryInterval: 1_000
+    }
+  )
+
+  return { data, isLoading: isValidating }
+}
+
+export { useERC20L1Address }
