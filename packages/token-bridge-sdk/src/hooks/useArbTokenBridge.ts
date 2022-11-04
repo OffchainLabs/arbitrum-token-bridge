eda43971c1a76df13dbe8a35ae28c46a563f4333
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-11-04 12:23:17 +0100

    update values

--- packages/token-bridge-sdk/src/hooks/useArbTokenBridge.ts
+++ packages/token-bridge-sdk/src/hooks/useArbTokenBridge.ts
@@ -545,9 +545,13 @@ export const useArbTokenBridge = (
 
     const estimatedL2SubmissionCost =
       await inbox.calculateRetryableSubmissionFee(
-        // Actual data length was 704 but we added some padding
+        // Values set by looking at a couple of L1 gateways
         //
-        // https://etherscan.io/tx/0x5c0ab94413217d54641ba5faa0c614c6dd5f97efcc7a6ca25df9c376738dfa34
+        // L1 LPT Gateway: 324
+        // L1 DAI Gateway: 324
+        // L1 Standard Gateway (APE): 740
+        // L1 Custom Gateway (USDT): 324
+        // L1 WETH Gateway (USDT): 324
         BigNumber.from(1_000),
         // We do the same percent increase in the SDK
         //
@@ -555,14 +559,17 @@ export const useArbTokenBridge = (
         l1BaseFee.add(l1BaseFee.mul(BigNumber.from(3)))
       )
 
-    // The values set by looking at a couple of different ERC-20 deposits
     return {
+      // Values set by looking at a couple of different ERC-20 deposits
+      //
       // https://etherscan.io/tx/0x5c0ab94413217d54641ba5faa0c614c6dd5f97efcc7a6ca25df9c376738dfa34
       // https://etherscan.io/tx/0x0049a5a171b891c5826ba47e77871fa6bae6eb57fcaf474a97d62ab07a815a2c
       // https://etherscan.io/tx/0xb11bffdfbe4bc6fb4328c390d4cdf73bc863dbaaef057afb59cd83dfd6dc210c
       // https://etherscan.io/tx/0x194ab69d3d2b5730b37e8bad1473f8bc54ded7a2ad3708d131ef13c09168d67e
       // https://etherscan.io/tx/0xc4789d3f13e0efb011dfa88eef89b4b715d8c32366977eae2d3b85f13b3aa6c5
       estimatedL1Gas: BigNumber.from(240_000),
+      // Values set by looking at a couple of different ERC-20 deposits
+      //
       // https://arbiscan.io/tx/0x483206b0ed4e8a23b14de070f6c552120d0b9bc6ed028f4feae33c4ca832f2bc
       // https://arbiscan.io/tx/0xd2ba11ebc51f546abc2ddda715507948d097e5707fd1dc37c239cc4cf28cc6ed
       // https://arbiscan.io/tx/0xb341745b6f4a34ee539c628dcf177fc98b658e494c7f8d21da872e69d5173596
