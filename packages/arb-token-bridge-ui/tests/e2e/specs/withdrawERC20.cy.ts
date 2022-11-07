c2506a7a0bc14c0012100835cf9c56e3b4affb16
Christophe Deveaux <contact@christophedeveaux.com>
2022-11-07 11:49:45 +0100

    fix: add short format for large numbers (#471)
    * fix: Add short format for large number
    
    * Add comma separator for thousands
    
    * Add enum for decimals formatting, simplify function arguments
    
    * Add test for really low balance
    
    * Merge formatNumber and formatBigNumber
    
    * Make formatNumber and formatAmount accepting number and BigNumber
    
    * Format
    
    * Add missing import
    
    * Add comments, update tests
    
    * replace formatNumber usage by formatAmount
    
    * Move symbol to the format function
    
    * Remove default parameter for formatNumber, remove implicit default parameter
    
    * Allow not passing options params to formatAmount
    
    * Update e2e tests
    
    Co-authored-by: Fionna Chan <fionnacst@gmail.com>

--- packages/arb-token-bridge-ui/tests/e2e/specs/withdrawERC20.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/withdrawERC20.cy.ts
@@ -2,7 +2,7 @@
  * When user wants to bridge ETH from L1 to L2
  */
 
-import { formatBigNumber } from '../../../src/util/NumberUtils'
+import { formatAmount } from '../../../src/util/NumberUtils'
 import { resetSeenTimeStampCache } from '../../support/commands'
 import {
   arbitrumGoerliRPC,
@@ -35,7 +35,7 @@ describe('Withdraw ERC20 Token', () => {
     // log in to metamask before withdrawal
     before(() => {
       getInitialERC20Balance(ERC20TokenAddressL2, arbitrumGoerliRPC).then(
-        val => (l2ERC20bal = formatBigNumber(val, 18, 5))
+        val => (l2ERC20bal = formatAmount(val, { symbol: 'LINK' }))
       )
 
       // login to L2 chain for Arb Goerli network
@@ -92,7 +92,7 @@ describe('Withdraw ERC20 Token', () => {
     })
 
     it('should show ERC20 balance correctly', () => {
-      cy.findByText(`Balance: ${l2ERC20bal} LINK`).should('be.visible')
+      cy.findByText(`Balance: ${l2ERC20bal}`).should('be.visible')
     })
 
     context("bridge amount is lower than user's L2 ERC20 balance value", () => {
