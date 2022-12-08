886aae88c17cee9c1d5e22f7f4152f88bfedfbc2
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2022-12-08 18:29:02 +0100

    chore: add GRT to list of tokens requiring approval on L2

--- packages/arb-token-bridge-ui/src/util/L2ApprovalUtils.ts
+++ packages/arb-token-bridge-ui/src/util/L2ApprovalUtils.ts
@@ -12,6 +12,11 @@ const L2ApproveTokens: { [chainId: number]: RequireL2ApproveToken[] } = {
       symbol: 'LPT',
       l1Address: '0x58b6A8A3302369DAEc383334672404Ee733aB239',
       l2Address: '0x289ba1701C2F088cf0faf8B3705246331cB8A839'
+    },
+    {
+      symbol: 'GRT',
+      l1Address: '0xc944e90c64b2c07662a292be6244bdf05cda44a7',
+      l2Address: '0x9623063377AD1B27544C965cCd7342f7EA7e88C7'
     }
   ],
   [ChainId.ArbitrumGoerli]: [
