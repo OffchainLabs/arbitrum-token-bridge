20ef10a54227a423a947b7eafb65e8507ef859f5
Bartek <bartlomiejkramek@outlook.com>
2023-01-03 16:20:15 +0100

    remove t&c test

--- packages/arb-token-bridge-ui/tests/support/commands.ts
+++ packages/arb-token-bridge-ui/tests/support/commands.ts
@@ -73,7 +73,7 @@ export const saveAppState = () => {
 
 export const connectToApp = () => {
   // initial modal prompts which come in the web-app
-  cy.findByText('Agree to terms').should('be.visible').click()
+  // cy.findByText('Agree to terms').should('be.visible').click()
   cy.findByText('MetaMask').should('be.visible')
   cy.findByText('Connect to your MetaMask Wallet').click()
 }
