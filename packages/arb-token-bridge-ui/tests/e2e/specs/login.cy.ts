7ff02911bc5f1e94824265edaec64be86f354e1f
Bartek <bartlomiejkramek@outlook.com>
2023-01-13 18:43:00 +0100

    clean up & fix tests

--- packages/arb-token-bridge-ui/tests/e2e/specs/login.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/login.cy.ts
@@ -26,102 +26,16 @@ describe('Login Account', () => {
     )
   })
 
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
   after(() => {
     // after all assertions are executed, logout and reset the account
     cy.logout()
   })
 
-  // it('just visits home page', () => {
-  //   cy.visit('/')
-  // })
-
-  it('T&C visible', () => {
-    cy.clearCookies()
-    cy.visit('/')
-    cy.clearCookies()
-    cy.findByText('Agree to terms').should('be.visible')
-  })
-
-  it('Header item defined', () => {
-    cy.visit('/')
-    cy.findByText('Get Help').should('not.be.undefined')
-  })
-
-  it('This should fail (non existing text)', () => {
-    cy.visit('/')
-    cy.findByText('I dont exist on this page gmskgmk').should(
-      'not.be.undefined'
-    )
-  })
-
-  it('This should also fail (wrong url)', () => {
-    cy.visit('/invalidurl')
-    cy.findByAltText('MetaMask').should('not.be.undefined')
-  })
-
-  it('Has loader on the page', () => {
-    cy.visit('/')
-    cy.find(`[aria-label="audio-loading"]`).should('be.visible')
-  })
-
-  it('Header item visible', () => {
-    cy.visit('/')
-    cy.findByText('Get Help').should('be.visible')
-  })
-
-  it('Connect Wallet button visible', () => {
-    cy.visit('/')
-    cy.findByText('Connect Wallet').should('be.visible')
-  })
-
-  it('Connect Wallet button in the main content visible', () => {
-    cy.visit('/')
-    cy.find('.rounded-lg').findByText('Connect Wallet').should('be.visible')
-  })
-
-  it('Clicks connect wallet and displays MetaMask popup', () => {
-    cy.visit('/')
-    cy.findByText('Connect Wallet').click()
-    cy.isMetamaskWindowActive().should('be.true')
-  })
-
-  it('MetaMask image is defined', () => {
-    cy.visit('/')
-    cy.findByAltText('MetaMask').should('not.be.undefined')
-  })
-
-  it('WalletConnect image is defined', () => {
-    cy.visit('/')
-    cy.findByAltText('WalletConnect').should('not.be.undefined')
-  })
-
-  it('MetaMask image is visible', () => {
-    cy.visit('/')
-    cy.findByAltText('MetaMask').should('be.visible')
-  })
-
-  it('WalletConnect image is visible', () => {
-    cy.visit('/')
-    cy.findByAltText('WalletConnect').should('be.visible')
-  })
-
-  it('MetaMask window shown', () => {
-    cy.visit('/')
-    cy.isMetamaskWindowActive().should('be.true')
-  })
-
-  it('Footer moon defined', () => {
-    cy.visit('/')
-    cy.findByAltText('Moon').should('not.be.undefined')
-  })
-
-  it('Footer moon visible', () => {
-    cy.visit('/')
-    cy.findByAltText('Moon').should('be.visible')
-  })
-
   it('should show connect wallet if not logged in', () => {
-    cy.visit('/')
     cy.findByText('Agree to terms').should('be.visible').click()
     cy.findByText('MetaMask').should('be.visible')
     cy.findByText('Connect to your MetaMask Wallet').should('be.visible')
