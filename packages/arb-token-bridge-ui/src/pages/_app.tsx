2d89feb77e9dcb3fde908991af9713a7096a2307
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-01-26 12:54:19 +0100

    move init stuff to _app.tsx

--- packages/arb-token-bridge-ui/src/pages/_app.tsx
+++ packages/arb-token-bridge-ui/src/pages/_app.tsx
@@ -1,7 +1,44 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+import 'tippy.js/dist/tippy.css'
+import 'tippy.js/themes/light.css'
+
+import Package from '../../package.json'
+import { registerLocalNetwork } from '../util/networks'
 
 import '../styles/tailwind.css'
+
+if (process.env.NODE_ENV === 'development') {
+  registerLocalNetwork()
+}
+
+dayjs.extend(relativeTime)
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  release: Package.version,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 0.15,
+  beforeSend: event => {
+    if (event.message) {
+      if (
+        // Ignore events related to failed `eth_gasPrice` calls
+        event.message.match(/eth_gasPrice/i) ||
+        // Ignore events related to failed `eth_getBalance` calls
+        event.message.match(/eth_getBalance/i)
+      ) {
+        return null
+      }
+    }
+
+    return event
+  }
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
