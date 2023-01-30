053f0c017f921bde3e4fb2d94d06edc9db5d2f23
Dewansh <dewanshparashar@yahoo.in>
2023-01-30 21:46:07 +0530

    Dev : removed the filter for ETH or ERC20 in table search

--- packages/arb-token-bridge-ui/src/components/TransactionsTable/TransactionsTable.tsx
+++ packages/arb-token-bridge-ui/src/components/TransactionsTable/TransactionsTable.tsx
@@ -177,13 +177,16 @@ export function TransactionsTable({
 
   return (
     <>
+      {/* top search and pagination bar */}
       <div className="sticky top-0 left-0 flex w-auto flex-nowrap items-center justify-between gap-4 rounded-tr-lg bg-white p-3 text-sm">
         {/* Deposit type dropdown */}
 
-        <DepositTypeDropdown
-          value={pageParams!.type}
-          setValue={changeType}
-        ></DepositTypeDropdown>
+        {false && (
+          <DepositTypeDropdown
+            value={pageParams!.type}
+            setValue={changeType}
+          ></DepositTypeDropdown>
+        )}
 
         {/* Search bar */}
         <div className="relative flex h-full w-full grow items-center rounded border-2 bg-white px-2">
