fd65d044bff7af57df3b8fd3a61d696ff6425b3b
Bartek <bartlomiejkramek@outlook.com>
2023-02-09 12:57:51 +0100

    refactor: make `loadEnvironmentVariableWithFallback` always return a string (#567)
    Co-authored-by: Dewansh <dparashar@offchainlabs.com>

--- packages/arb-token-bridge-ui/src/util/index.ts
+++ packages/arb-token-bridge-ui/src/util/index.ts
@@ -32,10 +32,21 @@ export const loadEnvironmentVariableWithFallback = ({
   fallback
 }: {
   env?: string
-  fallback: string
-}) => {
-  if (typeof env === 'undefined' || env.trim().length === 0) {
-    return fallback
+  fallback?: string
+}): string => {
+  const isValidEnv = (value?: string) => {
+    return typeof value === 'string' && value.trim().length !== 0
   }
-  return env
+  if (isValidEnv(env)) {
+    return String(env)
+  }
+  if (isValidEnv(fallback)) {
+    return String(fallback)
+  }
+  throw new Error(
+    `
+      Neither the provided value or its fallback are a valid environment variable.
+      Expected one of them to be a non-empty string but received env: '${env}', fallback: '${fallback}'.
+    `
+  )
 }
