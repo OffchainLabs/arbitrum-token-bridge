d3b8094842b01f566d31da692be4bf1f8118d513
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-02-21 11:06:12 +0100

    add token deposit test

--- packages/arb-token-bridge-ui/tests/e2e/specs/readClassicDeposits.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/readClassicDeposits.cy.ts
@@ -16,7 +16,6 @@ function mockClassicDepositTransaction(
   const dateYearAgo = new Date(dateNow.setFullYear(dateNow.getFullYear() - 1))
 
   return {
-    ...params,
     type: 'deposit-l1',
     status: 'success',
     assetName: 'ETH',
@@ -31,7 +30,8 @@ function mockClassicDepositTransaction(
       status: L1ToL2MessageStatus.NOT_YET_CREATED,
       retryableCreationTxID: undefined,
       l2TxID: undefined
-    }
+    },
+    ...params
   }
 }
 
@@ -53,8 +53,7 @@ describe('Read classic deposit messages', () => {
     cy.saveAppState()
   })
 
-  // Happy Path
-  context('User has a classic ETH deposit transaction', () => {
+  context('User has classic ETH deposit transaction', () => {
     // log in to metamask
     before(() => {
       cy.login({ networkType: 'L1' })
@@ -65,13 +64,13 @@ describe('Read classic deposit messages', () => {
       cy.logout()
     })
 
-    it('can read message successfully', () => {
+    it('can read successful ETH deposit', () => {
       cy.setLocalStorage(
         'arbTransactions',
         JSON.stringify([
           mockClassicDepositTransaction({
-            txID: '0x410301bba1987cd02c8c68c60fe7e56a3c5842857062b157c08edf9f3ac7526a',
-            value: '0.05'
+            txID: '0x00000a813d47f2c478dcc3298d5361cb3aed817648f25cace6d0c1a59d2b8309',
+            value: '0.024'
           })
         ])
       )
@@ -81,9 +80,64 @@ describe('Read classic deposit messages', () => {
         cy.openAccountPopover()
 
         const l1TxHash =
-          '0x410301bba1987cd02c8c68c60fe7e56a3c5842857062b157c08edf9f3ac7526a'
+          '0x00000a813d47f2c478dcc3298d5361cb3aed817648f25cace6d0c1a59d2b8309'
         const l2TxHash =
-          '0xa8af89040713368f6ef848e1ac45e7162210211f5e3399b3d274684703181873'
+          '0xd3ff2a70a115411e1ae4917351dca49281368684394d0dcac136fa08d9d9b436'
+
+        cy.findByLabelText(/l1 transaction status/i).should(
+          'contain',
+          'Success'
+        )
+        cy.findByLabelText(/l2 transaction status/i).should(
+          'contain',
+          'Success'
+        )
+
+        cy.findByLabelText(/l1 transaction link/i).should(
+          'contain',
+          `L1: ${shortenTxHash(l1TxHash)}`
+        )
+
+        cy.findByLabelText(/l2 transaction link/i).should(
+          'contain',
+          `L2: ${shortenTxHash(l2TxHash)}`
+        )
+      })
+    })
+  })
+
+  context('User has classic ERC-20 deposit transaction', () => {
+    // log in to metamask
+    before(() => {
+      cy.login({ networkType: 'L1' })
+    })
+
+    after(() => {
+      // after all assertions are executed, logout and reset the account
+      cy.logout()
+    })
+
+    it('can read successful ERC-20 deposit', () => {
+      cy.setLocalStorage(
+        'arbTransactions',
+        JSON.stringify([
+          mockClassicDepositTransaction({
+            txID: '0x000153c231eb9fd3690b5e818fb671bdd09d678fe46b16b8f694f3beb9cf6db1',
+            value: '10,000',
+            assetType: AssetType.ERC20,
+            assetName: 'DAI'
+          })
+        ])
+      )
+
+      cy.changeMetamaskNetwork('mainnet').then(() => {
+        cy.closeLowBalanceDialog()
+        cy.openAccountPopover()
+
+        const l1TxHash =
+          '0x000153c231eb9fd3690b5e818fb671bdd09d678fe46b16b8f694f3beb9cf6db1'
+        const l2TxHash =
+          '0x6cecd3bfc3ec73181c4ac0253d3f51e5aa8d26157ca7439ff9ab465de14a436f'
 
         cy.findByLabelText(/l1 transaction status/i).should(
           'contain',
