7bc1475e6af9b14728d590ffc9f818ba0e60f5d7
Bartek <bartlomiejkramek@outlook.com>
2023-02-22 17:21:28 +0100

    add support file

---
+++ packages/arb-token-bridge-ui/tests/e2e/support.ts
@@ -0,0 +1,11 @@
+import '../support/commands'
+import '@synthetixio/synpress/support'
+import 'cypress-localstorage-commands'
+
+Cypress.Keyboard.defaults({
+  keystrokeDelay: 50
+})
+
+before(() => {
+  cy.setupMetamask(Cypress.env('PRIVATE_KEY'))
+})
