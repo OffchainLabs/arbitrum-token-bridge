e6eca0adca80fd424bd881485fb2842e3413ae81
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-02-22 10:13:13 +0100

    add content

--- packages/arb-token-bridge-ui/src/components/App/BlockedDialog.tsx
+++ packages/arb-token-bridge-ui/src/components/App/BlockedDialog.tsx
@@ -1,19 +1,35 @@
 import { useRef } from 'react'
-import { Dialog as HeadlessUIDialog } from '@headlessui/react'
+import { ExclamationCircleIcon } from '@heroicons/react/outline'
 
 import { Dialog, UseDialogProps } from '../common/Dialog'
+import { ExternalLink } from '../common/ExternalLink'
 
 export function BlockedDialog(props: UseDialogProps & { address: string }) {
   const confirmButtonRef = useRef(null)
 
   return (
-    <Dialog {...props} initialFocus={confirmButtonRef} isCustom>
-      <div className="px-8 py-4">
-        <HeadlessUIDialog.Title className="text-2xl font-medium">
-          This wallet address is blocked
-        </HeadlessUIDialog.Title>
-
-        {props.address}
+    <Dialog
+      {...props}
+      initialFocus={confirmButtonRef}
+      cancelButtonProps={{ className: 'hidden' }}
+      actionButtonProps={{ className: 'font-medium' }}
+      actionButtonTitle="Close"
+      title={
+        <div className="flex items-center space-x-2">
+          <ExclamationCircleIcon className="h-8 w-8" />
+          <span className="uppercase">This wallet address is blocked</span>
+        </div>
+      }
+    >
+      <div className="flex flex-col space-y-8 break-words">
+        <span className="text-gray-10">{props.address.toLowerCase()}</span>
+        <span>This address is affiliated with a blocked activity.</span>
+        <span>
+          If you think this was an error, you can request a review with{' '}
+          <ExternalLink href="mailto:support@arbitrum.io">
+            support@arbitrum.io
+          </ExternalLink>
+        </span>
       </div>
     </Dialog>
   )
