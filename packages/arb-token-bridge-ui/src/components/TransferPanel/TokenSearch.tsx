b7acecd61fa8eb4bb14a9c9b250702aba5548c3d
Dewansh <dewanshparashar@yahoo.in>
2023-02-27 19:28:46 +0530

    Dev : master pull, updated 1 file

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TokenSearch.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TokenSearch.tsx
@@ -644,7 +644,7 @@ export function TokenSearch({
         <div className="flex justify-end pt-6">
           {isFetchingTokenLists ? (
             <span className="flex flex-row items-center gap-2 text-sm font-normal text-gray-9">
-              <Loader color="rgb(40, 160, 240)" height={16} width={16} />
+              <Loader color="rgb(40, 160, 240)" size="small" />
               Fetching Tokens...
             </span>
           ) : (
