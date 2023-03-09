6eaf7113897ed0faeb87ab9084fdffe8ffdef5cb
Dewansh <dewanshparashar@yahoo.in>
2023-03-09 13:01:35 +0530

    Dev : added clickjacking preventive measures

--- packages/arb-token-bridge-ui/next.config.js
+++ packages/arb-token-bridge-ui/next.config.js
@@ -12,3 +12,28 @@ module.exports = {
   distDir: 'build',
   productionBrowserSourceMaps: true
 }
+
+// https://nextjs.org/docs/advanced-features/security-headers
+// https://cheatsheetseries.owasp.org/cheatsheets/Clickjacking_Defense_Cheat_Sheet.html
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "frame-ancestors 'self';"
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
+  }
+}
