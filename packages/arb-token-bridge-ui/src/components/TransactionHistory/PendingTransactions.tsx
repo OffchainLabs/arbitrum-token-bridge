15d235097988139a054c728247bcb3b284f2ec77
Dewansh <dewanshparashar@yahoo.in>
2023-03-20 16:35:21 +0530

    Dev : reverting the error on network tab

--- packages/arb-token-bridge-ui/src/components/TransactionHistory/PendingTransactions.tsx
+++ packages/arb-token-bridge-ui/src/components/TransactionHistory/PendingTransactions.tsx
@@ -19,7 +19,9 @@ import { Loader } from '../common/atoms/Loader'
 
 const getOtherL2NetworkChainId = (chainId: number) => {
   if (!isNetwork(chainId).isArbitrumOne && !isNetwork(chainId).isArbitrumNova) {
-    console.warn(`[getOtherL2NetworkChainId] Unexpected chain id: ${chainId}`)
+    throw new Error(
+      `[getOtherL2NetworkChainId] Unexpected chain id: ${chainId}`
+    )
   }
   return isNetwork(chainId).isArbitrumOne
     ? ChainId.ArbitrumNova
