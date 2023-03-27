842990ef098bb856ce9fa5209f7e7b95262d62a0
Dewansh <dparashar@offchainlabs.com>
2023-03-27 17:37:35 +0530

    refactor: `Tab` component refactoring and cleanup (#628)

--- packages/arb-token-bridge-ui/tailwind.config.js
+++ packages/arb-token-bridge-ui/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('@headlessui/tailwindcss')],
   theme: {
     extend: {
       backgroundImage: {
