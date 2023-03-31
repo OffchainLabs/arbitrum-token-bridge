1ce31e3db4c818bf3cd01eab567b18bc56382993
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-03-31 22:37:06 +0200

    fix: set april fools theme live on 8am utc

--- packages/arb-token-bridge-ui/src/components/syncers/ThemeIncluder.tsx
+++ packages/arb-token-bridge-ui/src/components/syncers/ThemeIncluder.tsx
@@ -2,8 +2,8 @@ import dayjs from 'dayjs'
 import { useEffect, useState } from 'react'
 import { useTheme } from 'src/hooks/useTheme'
 
-const aprilFools = '2023-04-01'
-export const isAprilFools = dayjs().isSame(aprilFools, 'day')
+const aprilFools = '2023-04-01T08:00:00Z' // 8am UTC
+export const isAprilFools = dayjs().isAfter(aprilFools)
 export const isAfterAprilFools = dayjs().isAfter(aprilFools, 'day')
 export const classicThemeKey = 'arbitrum-classic-theme'
 
