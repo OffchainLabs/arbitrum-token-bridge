461f3dddbd8fa40a07ff5bcf44c7d49bad31e1a3
Dewansh <dewanshparashar@yahoo.in>
2023-04-04 23:05:57 +0530

    Dev : update comment

--- packages/arb-token-bridge-ui/jest.config.js
+++ packages/arb-token-bridge-ui/jest.config.js
@@ -6,8 +6,11 @@ const createJestConfig = nextJest({
 
 const customJestConfig = {
   testEnvironment: 'jsdom',
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  setupFiles: ['<rootDir>/setupTests.ts']
+  setupFiles: ['<rootDir>/setupTests.ts'],
+
+  // https://jestjs.io/docs/configuration#testmatch-arraystring
+  // removed ["**/__tests__/**/*.[jt]s?(x)"] from jest's default testMatch to ignore helpers.ts present in __test__ folders
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
