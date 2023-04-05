31914b7e17c631d8c10b27edcb362c7a01c06123
Fionna Chan <fionnacst@gmail.com>
2023-04-05 12:26:54 +0100

    remove svgr

--- packages/arb-token-bridge-ui/next.config.js
+++ packages/arb-token-bridge-ui/next.config.js
@@ -5,17 +5,6 @@
  **/
 
 module.exports = {
-  webpack(config) {
-    if (process.env.NODE_ENV === 'test') {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack']
-      })
-    }
-
-    return config
-  },
   distDir: 'build',
   productionBrowserSourceMaps: true
 }
