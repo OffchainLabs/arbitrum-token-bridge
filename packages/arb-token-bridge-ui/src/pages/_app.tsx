407fcc13b86dba78e114f646b6e7204d92d1caab
Bartek <bartlomiejkramek@outlook.com>
2023-04-13 21:43:26 +0200

    initial posthog

--- packages/arb-token-bridge-ui/src/pages/_app.tsx
+++ packages/arb-token-bridge-ui/src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import * as Sentry from '@sentry/react'
 import { BrowserTracing } from '@sentry/tracing'
+import posthog from 'posthog-js'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
@@ -49,7 +51,32 @@ Sentry.init({
   }
 })
 
+if (
+  typeof window !== 'undefined' &&
+  typeof process.env.NEXT_PUBLIC_POSTHOG_KEY === 'string'
+) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: 'https://app.posthog.com',
+    loaded: posthog => {
+      if (process.env.NODE_ENV !== 'production') {
+        // when in dev, you can see data that would be sent in prod (in devtools)
+        posthog.debug()
+      }
+    },
+    // store data in temporary memory that expires with each session
+    persistence: 'memory',
+    // by default posthog autocaptures (sends) events such as onClick, etc
+    // we set up our own events instead
+    autocapture: false
+  })
+}
+
 export default function App({ Component, pageProps }: AppProps) {
+  // because autocapture is disabled, we need to send pageview manually
+  useEffect(() => {
+    posthog.capture('$pageview')
+  }, [])
+
   return (
     <>
       <Head>
