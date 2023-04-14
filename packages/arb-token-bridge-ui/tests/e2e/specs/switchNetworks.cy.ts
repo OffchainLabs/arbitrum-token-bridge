20facd587ec284ad6f9f8945ef399e5f3c898d52
Fionna Chan <fionnacst@gmail.com>
2023-04-14 15:09:28 +0100

    change waiting time and run only that test on ci

--- packages/arb-token-bridge-ui/tests/e2e/specs/switchNetworks.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/switchNetworks.cy.ts
@@ -92,7 +92,7 @@ describe('Switch Networks', () => {
         })
         // Arbitrary waiting time has to be added to ensure Transfer Panel is loaded
         // .waitUntil did not work as it causes the whole process including login to loop
-        cy.wait(3000)
+        cy.wait(6000)
           .findByRole('button', { name: /From: Ethereum/i })
           .should('be.visible')
 
