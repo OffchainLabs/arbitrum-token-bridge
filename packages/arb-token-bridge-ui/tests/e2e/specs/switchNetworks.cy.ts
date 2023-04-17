45f1d7698753a82de6831793f14d85f31ef2b6cf
Bartek <bartlomiejkramek@outlook.com>
2023-04-17 14:13:22 +0200

    fix e2e

--- packages/arb-token-bridge-ui/tests/e2e/specs/switchNetworks.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/switchNetworks.cy.ts
@@ -18,6 +18,18 @@ describe('Switch Networks', () => {
           networkName: 'mainnet',
           shouldChangeNetwork: true
         })
+        cy.waitUntil(
+          () =>
+            cy
+              .findByRole('button', { name: /From: Mainnet/i })
+              .should('be.visible')
+              .should('be.visible'),
+          {
+            errorMsg: "Can't find /From: Mainnet/i",
+            timeout: 10000,
+            interval: 500
+          }
+        )
         cy.findByRole('button', { name: /Selected Network : /i })
           .should('be.visible')
           .click({ scrollBehavior: false })
@@ -51,7 +63,22 @@ describe('Switch Networks', () => {
       // })
 
       it('should change network to Arbitrum Nova successfully', () => {
-        cy.login({ networkType: 'L1', shouldChangeNetwork: true })
+        cy.login({
+          networkType: 'L1',
+          networkName: 'mainnet'
+        })
+        cy.waitUntil(
+          () =>
+            cy
+              .findByRole('button', { name: /From: Mainnet/i })
+              .should('be.visible')
+              .should('be.visible'),
+          {
+            errorMsg: "Can't find /From: Mainnet/i",
+            timeout: 10000,
+            interval: 500
+          }
+        )
         cy.findByRole('button', { name: /Selected Network : /i })
           .should('be.visible')
           .click({ scrollBehavior: false })
@@ -69,62 +96,65 @@ describe('Switch Networks', () => {
 
       it('should change network to Ethereum mainnet successfully', () => {
         cy.login({ networkType: 'L1' })
-        cy.findByRole('button', { name: /Selected Network : /i })
-          .should('be.visible')
-          .click({ scrollBehavior: false })
+        cy.waitUntil(
+          () =>
+            cy
+              .findByRole('button', { name: /From: Arbitrum Nova/i })
+              .should('be.visible'),
+          {
+            errorMsg: "Can't find /From: Arbitrum Nova/i",
+            timeout: 10000,
+            interval: 500
+          }
+        ).then(() => {
+          cy.findByRole('button', { name: /Selected Network : /i })
+            .should('be.visible')
+            .click({ scrollBehavior: false })
 
-        cy.findByRole('button', { name: /Switch to Mainnet/i }).click({
-          scrollBehavior: false
-        })
+          cy.findByRole('button', { name: /Switch to Mainnet/i }).click({
+            scrollBehavior: false
+          })
 
-        cy.allowMetamaskToSwitchNetwork().then(() => {
-          cy.findByRole('button', {
-            name: /Selected Network : Mainnet/i
-          }).should('be.visible')
+          cy.allowMetamaskToSwitchNetwork().then(() => {
+            cy.findByRole('button', {
+              name: /Selected Network : Mainnet/i
+            }).should('be.visible')
+          })
         })
       })
     })
 
     context('Test Networks list in Wrong Network UI', () => {
       it('should show wrong network UI', () => {
-        cy.login({
-          networkType: 'L1'
-        })
-        // Arbitrary waiting time has to be added to ensure Transfer Panel is loaded
-        // .waitUntil did not work as it causes the whole process including login to loop
-        cy.wait(30000)
-          .findByRole('button', { name: /From: Ethereum/i })
-          .should('be.visible')
-          // .waitUntil(
-          //   () => {
-          //     cy.findByRole('button', { name: /From: Ethereum/i }).should(
-          //       'be.visible'
-          //     )
-          //   },
-          //   {
-          //     errorMsg: "Can't find /From: Ethereum/i",
-          //     timeout: 2000, // waits up to 2000 ms, default to 5000
-          //     interval: 500 // performs the check every 500 ms, default to 200
-          //   }
-          // )
-          .changeMetamaskNetwork('Sepolia test network')
-          .then(() =>
-            cy
-              .findByText(/Oops! You’re connected to the wrong network/i)
-              .should('be.visible')
-          )
+        cy.login({ networkType: 'L1' })
+        cy.changeMetamaskNetwork('Sepolia test network').then(() => {
+          cy.waitUntil(
+            () =>
+              cy
+                .findByText(/Oops! You’re connected to the wrong network/i)
+                .should('be.visible'),
+            {
+              errorMsg:
+                "Can't find /Oops! You’re connected to the wrong network/i",
+              timeout: 10000,
+              interval: 500
+            }
+          ).then(() => {
+            context('Allow Network change from wrong network UI list', () => {
+              cy.findByRole('button', {
+                name: /Switch to Arbitrum Goerli/i
+              })
+                .should('be.visible')
+                .click({
+                  scrollBehavior: false
+                })
 
-        context('Allow Network change from wrong network UI list', () => {
-          cy.findByRole('button', { name: /Switch to Arbitrum Goerli/i })
-            .should('be.visible')
-            .click({
-              scrollBehavior: false
+              cy.allowMetamaskToAddAndSwitchNetwork().then(() => {
+                cy.findByRole('button', {
+                  name: /Selected Network : Arbitrum Goerli/i
+                }).should('be.visible')
+              })
             })
-
-          cy.allowMetamaskToAddAndSwitchNetwork().then(() => {
-            cy.findByRole('button', {
-              name: /Selected Network : Arbitrum Goerli/i
-            }).should('be.visible')
           })
         })
       })
