0312efece30fef131df5c98f3d0c6d083b7e836a
Fionna Chan <fionnacst@gmail.com>
2023-05-23 11:55:32 +0100

    update hook type

--- packages/arb-token-bridge-ui/src/hooks/__tests__/utils.ts
+++ packages/arb-token-bridge-ui/src/hooks/__tests__/utils.ts
@@ -1,16 +1,6 @@
 import { RenderHookResult, act, renderHook } from '@testing-library/react'
-import { BigNumber } from 'ethers'
-import { KeyedMutator } from 'swr'
 
-import { Erc20Balances, UseBalanceProps, useBalance } from '../useBalance'
-
-type RenderUseBalanceHookResultResultType = {
-  eth: readonly [BigNumber | null, KeyedMutator<BigNumber>]
-  erc20: readonly [
-    Erc20Balances | null,
-    (addresses: string[]) => Promise<Erc20Balances | undefined>
-  ]
-}
+import { UseBalanceProps, useBalance } from '../useBalance'
 
 // this method is suggested by https://stackoverflow.com/a/73476929/5143717
 export const renderHookAsync = async ({
@@ -21,7 +11,7 @@ export const renderHookAsync = async ({
   wrapper: React.ComponentType
 }) => {
   let hook:
-    | RenderHookResult<RenderUseBalanceHookResultResultType, UseBalanceProps>
+    | RenderHookResult<ReturnType<typeof useBalance>, UseBalanceProps>
     | undefined
 
   await act(async () => {
