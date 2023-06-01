6466613ff5862282d165748b75c50c47b5e83239
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-01 15:12:16 +0200

    remove stuff

--- packages/arb-token-bridge-ui/src/hooks/useIsConnectedWithSmartContractWallet.ts
+++ packages/arb-token-bridge-ui/src/hooks/useIsConnectedWithSmartContractWallet.ts
@@ -1,13 +1,11 @@
 import { useState, useEffect } from 'react'
-import { useAccount, useProvider, useSignMessage } from 'wagmi'
+import { useAccount, useProvider } from 'wagmi'
 
 import { addressIsSmartContract } from '../util/AddressUtils'
-import { isUserRejectedError } from '../util/isUserRejectedError'
 
 export function useIsConnectedWithSmartContractWallet() {
   const provider = useProvider()
   const { address } = useAccount()
-  const { signMessageAsync } = useSignMessage()
 
   const [result, setResult] = useState<boolean | undefined>(undefined)
 
@@ -18,26 +16,13 @@ export function useIsConnectedWithSmartContractWallet() {
         return
       }
 
-      const nonce = await provider.getTransactionCount(address)
-      const isMaybeUndeployedSmartContractWallet = nonce === 0
+      // TODO: Try to detect counterfactual/just-in-time deployed smart contract wallets
 
-      if (isMaybeUndeployedSmartContractWallet) {
-        try {
-          await signMessageAsync({ message: 'TODO: What message?' })
-        } catch (error) {
-          if (!isUserRejectedError(error)) {
-            setResult(true)
-            return
-          }
-        }
-      }
-
-      // Check if the address is a smart contract
       setResult(await addressIsSmartContract(address, provider))
     }
 
     update()
-  }, [address, provider, signMessageAsync])
+  }, [address, provider])
 
   return result
 }
