8fd7e47a20965afa95837cc1f88cf6c7a2acf28f
Bartek <bartlomiejkramek@outlook.com>
2023-06-12 14:31:23 +0200

    remove extension from isAddress import

--- packages/arb-token-bridge-ui/src/components/TransferPanel/AdvancedSettings.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/AdvancedSettings.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { isAddress } from 'ethers/lib/utils.js'
+import { isAddress } from 'ethers/lib/utils'
 import { Provider } from '@ethersproject/providers'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline'
 
