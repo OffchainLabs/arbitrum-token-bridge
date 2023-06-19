9411c31999fb093b1d26a565dc33322ff3603328
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-19 14:23:25 +0200

    temp

--- packages/arb-token-bridge-ui/src/hooks/useNetworks.ts
+++ packages/arb-token-bridge-ui/src/hooks/useNetworks.ts
@@ -1,5 +1,6 @@
 import { Chain } from 'wagmi'
 import { StaticJsonRpcProvider } from '@ethersproject/providers'
+import { create } from 'zustand'
 
 import { useArbQueryParams } from './useArbQueryParams'
 import {
@@ -9,6 +10,7 @@ import {
 } from '../types/ChainQueryParam'
 import { rpcURLs } from '../util/networks'
 import { getPartnerChainsForChain } from '../util/wagmi/getPartnerChainsForChain'
+import { chains } from '../constants'
 
 function getPartnerChainsQueryParams(
   chainQueryParam: ChainQueryParam
@@ -66,30 +68,71 @@ export function sanitizeQueryParams({
   return { from: from!, to: to! }
 }
 
-export type UseNetworksResult = {
-  from: Chain
-  fromProvider: StaticJsonRpcProvider
-  to: Chain
-  toProvider: StaticJsonRpcProvider
-}
+type UseNetworksSetStateProps = { from: Chain; to: Chain }
+type UseNetworksSetState = (props: UseNetworksSetStateProps) => void
 
-export function useNetworks(): UseNetworksResult {
+export type UseNetworksState = [
+  {
+    from: Chain
+    fromProvider: StaticJsonRpcProvider
+    to: Chain
+    toProvider: StaticJsonRpcProvider
+  },
+  UseNetworksSetState
+]
+
+const useNetworksState = create<UseNetworksState>(set => [
+  {
+    from: chains.mainnet,
+    fromProvider: getProviderForChain(chains.mainnet),
+    to: chains.arbitrumOne,
+    toProvider: getProviderForChain(chains.arbitrumOne)
+  },
+  (props: UseNetworksSetStateProps) => {
+    set(state => {
+      const [, setNetworks] = state
+
+      // Update the state but keep the second element (setter) intact
+      return [
+        {
+          from: props.from,
+          fromProvider: getProviderForChain(props.from),
+          to: props.to,
+          toProvider: getProviderForChain(props.to)
+        },
+        setNetworks
+      ]
+    })
+  }
+])
+
+export function useNetworks(): UseNetworksState {
+  const [state, setState] = useNetworksState()
   const [{ from, to }, setQueryParams] = useArbQueryParams()
   const { from: validFrom, to: validTo } = sanitizeQueryParams({ from, to })
 
+  console.log({ from, to })
+
   if (from !== validFrom || to !== validTo) {
+    // The return values of the hook will always be the sanitized values (including the first render)
+    const fromChain = getChainForChainQueryParam(validFrom)
+    const toChain = getChainForChainQueryParam(validTo)
+
     // On the first render, update query params with the sanitized values
     setQueryParams({ from: validFrom, to: validTo })
+    setState({ from: fromChain, to: toChain })
+
+    return [
+      {
+        from: fromChain,
+        fromProvider: getProviderForChain(fromChain),
+        to: toChain,
+        toProvider: getProviderForChain(toChain)
+      },
+      setState
+    ]
   }
 
-  // The return values of the hook will always be the sanitized values (including the first render)
-  const fromChain = getChainForChainQueryParam(validFrom)
-  const toChain = getChainForChainQueryParam(validTo)
-
-  return {
-    from: fromChain,
-    fromProvider: getProviderForChain(fromChain),
-    to: toChain,
-    toProvider: getProviderForChain(toChain)
-  }
+  // Everything is in sync, so we can just return values from the store
+  return [state, setState]
 }
