cc9c89940ae878c554ef7777387179fef0e4bb58
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-06-19 14:59:46 +0200

    remove loader

--- packages/arb-token-bridge-ui/src/pages/index.tsx
+++ packages/arb-token-bridge-ui/src/pages/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react'
 import dynamic from 'next/dynamic'
 
-import { AppConnectionFallbackContainer } from '../components/App/AppConnectionFallbackContainer'
-import { Loader } from '../components/common/atoms/Loader'
-
 const App = dynamic(() => import('../components/App/App'), {
-  ssr: false,
-  loading: () => (
-    <AppConnectionFallbackContainer>
-      <div className="fixed inset-0 m-auto h-[44px] w-[44px]">
-        <Loader size="large" color="white" />
-      </div>
-    </AppConnectionFallbackContainer>
-  )
+  ssr: false
 })
 
 export default function Index() {
