aa029b008f483ac6d109cdb76c9e1fade10d3421
Fionna Chan <fionnacst@gmail.com>
2023-06-26 12:34:03 +0100

    do not propagate when clicked external link

--- packages/arb-token-bridge-ui/src/components/common/ExternalLink.tsx
+++ packages/arb-token-bridge-ui/src/components/common/ExternalLink.tsx
@@ -1,9 +1,22 @@
 export function ExternalLink({
   children,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onClick = () => {},
   ...props
 }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+  function onClickHandler(event: React.MouseEvent<HTMLAnchorElement>) {
+    event.stopPropagation()
+    event.nativeEvent.stopImmediatePropagation()
+    onClick(event)
+  }
+
   return (
-    <a target="_blank" rel="noopener noreferrer" {...props}>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      onClick={onClickHandler}
+      {...props}
+    >
       {children}
     </a>
   )
