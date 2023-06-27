c8f91153eff94085e0bdfeccc58bf551c3577fda
Fionna Chan <fionnacst@gmail.com>
2023-06-27 14:39:39 +0100

    revert external link changes

--- packages/arb-token-bridge-ui/src/components/common/ExternalLink.tsx
+++ packages/arb-token-bridge-ui/src/components/common/ExternalLink.tsx
@@ -1,11 +1,9 @@
 export function ExternalLink({
   children,
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  onClick = () => {},
   ...props
 }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
   return (
-    <a target="_blank" rel="noopener noreferrer" onClick={onClick} {...props}>
+    <a target="_blank" rel="noopener noreferrer" {...props}>
       {children}
     </a>
   )
