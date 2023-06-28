b5c598fbdb9ac6402c7c34439c3052910965ee3a
spsjvc <dragisaspasojevic@gmail.com>
2023-06-28 14:36:26 +0200

    feat: migrate to WalletConnect v2 (#1017)

--- packages/arb-token-bridge-ui/src/components/App/App.tsx
+++ packages/arb-token-bridge-ui/src/components/App/App.tsx
@@ -50,7 +50,7 @@ import { NetworkSelectionContainer } from '../common/NetworkSelectionContainer'
 import { TOS_LOCALSTORAGE_KEY } from '../../constants'
 import { AppConnectionFallbackContainer } from './AppConnectionFallbackContainer'
 import FixingSpaceship from '@/images/arbinaut-fixing-spaceship.webp'
-import { appInfo, chains, wagmiClient } from '../../util/wagmi/setup'
+import { getProps } from '../../util/wagmi/setup'
 import { useAccountIsBlocked } from '../../hooks/useAccountIsBlocked'
 
 declare global {
@@ -311,6 +311,24 @@ function ConnectionFallback(props: FallbackProps): JSX.Element {
   }
 }
 
+// We're doing this as a workaround so users can select their preferred chain on WalletConnect.
+//
+// https://github.com/orgs/WalletConnect/discussions/2733
+// https://github.com/wagmi-dev/references/blob/main/packages/connectors/src/walletConnect.ts#L114
+const searchParams = new URLSearchParams(window.location.search)
+const targetChainKey = searchParams.get('walletConnectChain')
+
+const { wagmiConfigProps, rainbowKitProviderProps } = getProps(targetChainKey)
+
+// Clear cache for everything related to WalletConnect v2.
+//
+// TODO: Remove this once the fix for the infinite loop / memory leak is identified.
+Object.keys(localStorage).forEach(key => {
+  if (key === 'wagmi.requestedChains' || key.startsWith('wc@2')) {
+    localStorage.removeItem(key)
+  }
+})
+
 export default function App() {
   const [overmind] = useState<Overmind<typeof config>>(createOvermind(config))
 
@@ -337,11 +355,10 @@ export default function App() {
   return (
     <Provider value={overmind}>
       <ArbQueryParamProvider>
-        <WagmiConfig client={wagmiClient}>
+        <WagmiConfig {...wagmiConfigProps}>
           <RainbowKitProvider
-            chains={chains}
             theme={rainbowkitTheme}
-            appInfo={appInfo}
+            {...rainbowKitProviderProps}
           >
             <WelcomeDialog {...welcomeDialogProps} onClose={onClose} />
             <NetworkReady>
--- packages/arb-token-bridge-ui/src/util/wagmi/setup.ts
+++ packages/arb-token-bridge-ui/src/util/wagmi/setup.ts
@@ -10,6 +10,7 @@ import {
   localL2Network as arbitrumLocal
 } from './wagmiAdditionalNetworks'
 import { isTestingEnvironment } from '../CommonUtils'
+import { ChainId } from '../networks'
 
 const chainList = isTestingEnvironment
   ? [
@@ -26,29 +27,102 @@ const chainList = isTestingEnvironment
     ]
   : [mainnet, arbitrum, arbitrumNova, goerli, arbitrumGoerli]
 
-const { chains, provider } = configureChains(chainList, [publicProvider()])
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!
 
 const appInfo = {
-  appName: 'Bridge to Arbitrum'
+  appName: 'Bridge to Arbitrum',
+  projectId
 }
 
-const { wallets } = getDefaultWallets({
-  ...appInfo,
-  chains
-})
+enum TargetChainKey {
+  Mainnet = 'mainnet',
+  ArbitrumOne = 'arbitrum-one',
+  ArbitrumNova = 'arbitrum-nova',
+  Goerli = 'goerli',
+  ArbitrumGoerli = 'arbitrum-goerli'
+}
 
-const connectors = connectorsForWallets([
-  ...wallets,
-  {
-    groupName: 'More',
-    wallets: [trustWallet({ chains }), ledgerWallet({ chains })]
+function sanitizeTargetChainKey(targetChainKey: string | null): TargetChainKey {
+  // Default to Ethereum Mainnet if nothing passed in
+  if (targetChainKey === null) {
+    return TargetChainKey.Mainnet
   }
-])
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider
-})
+  // Default to Ethereum Mainnet if invalid
+  if (!(Object.values(TargetChainKey) as string[]).includes(targetChainKey)) {
+    return TargetChainKey.Mainnet
+  }
 
-export { chains, provider, appInfo, wagmiClient }
+  return targetChainKey as TargetChainKey
+}
+
+function getChainId(targetChainKey: TargetChainKey): number {
+  switch (targetChainKey) {
+    case TargetChainKey.Mainnet:
+      return ChainId.Mainnet
+
+    case TargetChainKey.ArbitrumOne:
+      return ChainId.ArbitrumOne
+
+    case TargetChainKey.ArbitrumNova:
+      return ChainId.ArbitrumNova
+
+    case TargetChainKey.Goerli:
+      return ChainId.Goerli
+
+    case TargetChainKey.ArbitrumGoerli:
+      return ChainId.ArbitrumGoerli
+  }
+}
+
+function getChains(targetChainKey: TargetChainKey) {
+  const targetChainId = getChainId(targetChainKey)
+
+  // Doing `Array.filter` instead of `Array.find` in case it's empty, just in case.
+  const target = chainList.filter(chain => chain.id === targetChainId)
+  const others = chainList.filter(chain => chain.id !== targetChainId)
+
+  return [...target, ...others]
+}
+
+export function getProps(targetChainKey: string | null) {
+  const { chains, provider } = configureChains(
+    // Wagmi selects the first chain as the one to target in WalletConnect, so it has to be the first in the array.
+    //
+    // https://github.com/wagmi-dev/references/blob/main/packages/connectors/src/walletConnect.ts#L114
+    getChains(sanitizeTargetChainKey(targetChainKey)),
+    [publicProvider()]
+  )
+
+  const { wallets } = getDefaultWallets({
+    ...appInfo,
+    chains
+  })
+
+  const connectors = connectorsForWallets([
+    ...wallets,
+    {
+      groupName: 'More',
+      wallets: [
+        trustWallet({ chains, projectId }),
+        ledgerWallet({ chains, projectId })
+      ]
+    }
+  ])
+
+  const client = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+  })
+
+  return {
+    rainbowKitProviderProps: {
+      appInfo,
+      chains
+    },
+    wagmiConfigProps: {
+      client
+    }
+  }
+}
