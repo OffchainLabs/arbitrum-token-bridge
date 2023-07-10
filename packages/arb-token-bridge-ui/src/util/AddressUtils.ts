9926881887a5c35cfae5db90e490adf10d31249e
Bartek <bartlomiejkramek@outlook.com>
2023-07-10 16:31:00 +0200

    add try catch

--- packages/arb-token-bridge-ui/src/util/AddressUtils.ts
+++ packages/arb-token-bridge-ui/src/util/AddressUtils.ts
@@ -16,12 +16,17 @@ export async function addressIsSmartContract(
 export async function addressIsDenylisted(address: string) {
   // The denylist consists of an array of addresses from Mainnet, Arbitrum One and Goerli.
   // We do not separate them as it's unlikely for anyone to have a wallet address matching our contract addresses.
-  const denylistResponse = await fetch(
-    `${getAPIBaseUrl()}/api/denylist?address=${address}`,
-    {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' }
-    }
-  )
-  return (await denylistResponse.json()).data as boolean
+  try {
+    const denylistResponse = await fetch(
+      `${getAPIBaseUrl()}/api/denylist?address=${address}`,
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
+    return (await denylistResponse.json()).data as boolean
+  } catch (error) {
+    console.error(error)
+    return false
+  }
 }
