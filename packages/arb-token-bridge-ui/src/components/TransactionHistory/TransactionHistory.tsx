a95fed74913e59e1b230ed79a8f0c6030f9a8f11
Bartek <bartlomiejkramek@outlook.com>
2023-08-01 16:04:11 +0200

    change word

--- packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionHistory.tsx
+++ packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionHistory.tsx
@@ -91,7 +91,7 @@ export const TransactionHistory = ({
   }
 
   useEffect(() => {
-    // this function runs every time the network card is changed, and here it is also triggered when the page loads
+    // this function runs every time the network tab is changed, and here it is also triggered when the page loads
     // it sets the tab to 0 (deposits), which is the default tab
     handleSentOrReceivedTxForSCW(0)
   }, [isSmartContractWallet, chain])
