5997df5da1f30f3b0ae48a87c7c50b4c080cb88a
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-08-07 19:08:36 +0200

    export

--- packages/arb-token-bridge-ui/src/util/checkFeatureFlag.ts
+++ packages/arb-token-bridge-ui/src/util/checkFeatureFlag.ts
@@ -1,9 +1,9 @@
-type Feature = 'example' | 'other'
-
 const searchParams = new URLSearchParams(window.location.search)
 // Using "_" as a delimiter as it's the default in use-query-params's DelimitedArrayParam
 // https://github.com/pbeshai/use-query-params/tree/master/packages/use-query-params#readme
 const featureFlags = (searchParams.get('ff') ?? '').split('_')
+
+export type Feature = 'example' | 'other'
 
 export function checkFeatureFlag(feature: Feature) {
   return featureFlags.includes(feature)
