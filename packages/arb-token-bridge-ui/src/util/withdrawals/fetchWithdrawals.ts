22f3fdc3c3f9ce36433215f517a0a5e59896c873
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-08-18 11:08:53 +0200

    oops

--- packages/arb-token-bridge-ui/src/util/withdrawals/fetchWithdrawals.ts
+++ packages/arb-token-bridge-ui/src/util/withdrawals/fetchWithdrawals.ts
@@ -86,7 +86,7 @@ export const fetchWithdrawals = async ({
       // todo: update when eth deposits to custom destination address are enabled (requires https://github.com/OffchainLabs/arbitrum-sdk/issues/325)
       //
       // currently, we can only do eth deposits to the same address, which is why we set `toAddress` to be `sender`
-      toAddress: sender,
+      fromBlock: toBlock + 1,
       toBlock: 'latest',
       l2Provider: l2Provider
     }),
