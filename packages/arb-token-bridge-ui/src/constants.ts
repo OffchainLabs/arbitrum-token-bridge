5f2993492800519f66006ceb6be86520dd28ba1b
Fionna Chan <fionnachan@users.noreply.github.com>
2023-08-24 17:30:52 +0100

    update article links

--- packages/arb-token-bridge-ui/src/constants.ts
+++ packages/arb-token-bridge-ui/src/constants.ts
@@ -7,3 +7,15 @@ export const GET_HELP_LINK =
 
 export const USDC_LEARN_MORE_LINK =
   'https://developer.arbitrum.io/bridge-tokens/concepts/usdc-concept'
+
+export const FAST_BRIDGE_ARTICLE_LINK =
+  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213771832987'
+
+export const TOKEN_APPROVAL_ARTICLE_LINK =
+  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213893952923'
+
+export const ETH_BALANCE_ARTICLE_LINK =
+  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213854684699'
+
+export const CONFIRMATION_PERIOD_ARTICLE_LINK =
+  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213843096091'
