b2d07f55563ed6061fa02278aa7f395aa02de3c6
fredlacs <32464905+fredlacs@users.noreply.github.com>
2023-08-29 07:37:08 -0300

    Update constants.ts

--- packages/arb-token-bridge-ui/src/constants.ts
+++ packages/arb-token-bridge-ui/src/constants.ts
@@ -3,7 +3,7 @@ export const TOS_VERSION = 1
 export const TOS_LOCALSTORAGE_KEY = 'arbitrum:bridge:tos-v' + TOS_VERSION
 
 export const GET_HELP_LINK =
-  'https://arbitrumfoundation.zendesk.com/hc/en-gb/requests/new?ticket_form_id=18155929976987&tf_18156987183899=You+can+find+your+Tx+Hash+in+your+profile+tab+on+the+bridge&tf_1900010553864=true&tf_description=1.+Describe+the+issue+in+detail%3A%3Cbr%3E%3Cbr%3E2.+Your+browser+%28Chrome%2C+Firefox%2C+Brave%2C+etc.%29%3Cbr%3E%3Cbr%3E3.+Are+you+moving+funds+to+Mainnet+or+Arbitrum%3F%3Cbr%3E%3Cbr%3E4.+%28optional%29+Steps+to+reproduce%3A%3Cbr%3E5.+%28optional%29+What+token+are+you+transferring%3A&tf_subject=Issue+with+bridging'
+  'https://arbitrumfoundation.zendesk.com/hc/en-gb/requests/new?ticket_form_id=18155929976987'
 
 export const USDC_LEARN_MORE_LINK =
   'https://developer.arbitrum.io/bridge-tokens/concepts/usdc-concept'
