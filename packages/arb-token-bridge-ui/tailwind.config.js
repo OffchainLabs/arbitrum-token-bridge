da283b35e8f8e41fea487b0a81d7841fa87873e6
Bartek <bartlomiejkramek@outlook.com>
2023-09-07 17:16:19 +0200

    remove truncate from config

--- packages/arb-token-bridge-ui/tailwind.config.js
+++ packages/arb-token-bridge-ui/tailwind.config.js
@@ -64,11 +64,6 @@ module.exports = {
         // shadow used for input fields across the app
         input:
           '0px 2px 2px rgba(33,37,41,0.06), 0px 0px 1px rgba(33,37,41,0.08)'
-      },
-      truncate: {
-        whiteSpace: 'noWrap',
-        overflow: 'hidden',
-        textOverflow: 'ellipsis'
       }
     }
   }
