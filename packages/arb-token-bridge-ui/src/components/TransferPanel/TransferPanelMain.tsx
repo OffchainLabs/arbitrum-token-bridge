a94227deaccd50ce36874b2ffa555b009d3ef972
mistersimon <779959+mistersimon@users.noreply.github.com>
2023-09-17 13:10:00 +0800

    always show balance below network on mobile

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMain.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMain.tsx
@@ -324,7 +324,7 @@ function NetworkListboxPlusBalancesContainer({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex flex-row flex-wrap items-center justify-between gap-1 gap-y-2.5 whitespace-nowrap">
+    <div className="flex flex-col items-start sm:flex-row sm:items-center justify-between gap-1 gap-y-2.5 whitespace-nowrap">
       {children}
     </div>
   )
