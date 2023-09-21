f1e7dc61aa8f1267a6df10bd0e0b7c7a46b0df13
Fionna Chan <fionnachan@users.noreply.github.com>
2023-09-21 15:13:06 +0100

    split urlQueryParam tests

--- packages/arb-token-bridge-ui/tests/e2e/specs/urlQueryParam.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/urlQueryParam.cy.ts
@@ -3,12 +3,7 @@
  */
 
 import { formatAmount } from '../../../src/util/NumberUtils'
-import {
-  getInitialETHBalance,
-  getNetworkNameForE2E
-} from '../../support/common'
-
-const walletConnectChain = getNetworkNameForE2E('L1')
+import { getInitialETHBalance } from '../../support/common'
 
 describe('User enters site with query params on URL', () => {
   let l1ETHbal: number
@@ -22,167 +17,188 @@ describe('User enters site with query params on URL', () => {
     ).then(val => (l1ETHbal = parseFloat(formatAmount(val, { decimals: 18 }))))
   })
 
-  it('should correctly populate amount input from query param', () => {
+  context('should correctly populate amount input from query param', () => {
     // only ETH is supported for now so by default the following tests are assumed to be ETH
-    context(
-      '?amount=max should set transfer panel amount to maximum amount possible based on balance',
-      () => {
-        cy.login({ networkType: 'L1', query: { amount: 'max' } })
+    it('?amount=max should set transfer panel amount to maximum amount possible based on balance', () => {
+      cy.login({ networkType: 'L1', query: { amount: 'max' } })
 
-        cy.findByPlaceholderText(/Enter amount/i)
-          .should('be.visible')
-          .should('not.have.text', 'max')
-          .should('not.have.text', 'MAX')
-          // it's very hard to get the max amount separately
-          // so this test only asserts the amount set for the input field is less than user's balance
-          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-          .then(() => {
-            cy.waitUntil(
-              () =>
-                cy
-                  .findByPlaceholderText(/Enter amount/i)
-                  .then($el => Number($el.val()) > 0),
-              // optional timeouts and error messages
-              {
-                errorMsg:
-                  'was expecting a numerical input value greater than 0',
-                timeout: 5000,
-                interval: 500
-              }
-            ).then(() => {
-              cy.findByPlaceholderText(/Enter amount/i)
-                .invoke('val')
-                .then(value => {
-                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-                })
-            })
+      cy.findByPlaceholderText(/Enter amount/i)
+        .should('be.visible')
+        .should('not.have.text', 'max')
+        .should('not.have.text', 'MAX')
+        // it's very hard to get the max amount separately
+        // so this test only asserts the amount set for the input field is less than user's balance
+        // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+        .then(() => {
+          cy.waitUntil(
+            () =>
+              cy
+                .findByPlaceholderText(/Enter amount/i)
+                .then($el => Number($el.val()) > 0),
+            // optional timeouts and error messages
+            {
+              errorMsg: 'was expecting a numerical input value greater than 0',
+              timeout: 5000,
+              interval: 500
+            }
+          ).then(() => {
+            cy.findByPlaceholderText(/Enter amount/i)
+              .invoke('val')
+              .then(value => {
+                cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+              })
           })
-      }
-    )
-    context(
-      '?amount=MAX should set transfer panel amount to maximum amount possible based on balance',
-      () => {
-        cy.visit('/', { qs: { amount: 'MAX', walletConnectChain } })
+        })
+    })
+  })
 
-        cy.findByPlaceholderText(/Enter amount/i)
-          .should('be.visible')
-          .should('not.have.text', 'max')
-          .should('not.have.text', 'MAX')
-          // it's very hard to get the max amount separately
-          // so this test only asserts the amount set for the input field is less than user's balance
-          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-          .then(() => {
-            cy.waitUntil(
-              () =>
-                cy
-                  .findByPlaceholderText(/Enter amount/i)
-                  .then($el => Number($el.val()) > 0),
-              // optional timeouts and error messages
-              {
-                errorMsg:
-                  'was expecting a numerical input value greater than 0',
-                timeout: 5000,
-                interval: 500
-              }
-            ).then(() => {
-              cy.findByPlaceholderText(/Enter amount/i)
-                .invoke('val')
-                .then(value => {
-                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-                })
-            })
-          })
-      }
-    )
-    context(
-      '?amount=MaX should set transfer panel amount to maximum amount possible based on balance',
-      () => {
-        cy.visit('/', { qs: { amount: 'MaX', walletConnectChain } })
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=MAX should set transfer panel amount to maximum amount possible based on balance', () => {
+      cy.login({ networkType: 'L1', query: { amount: 'MAX' } })
 
-        cy.findByPlaceholderText(/Enter amount/i)
-          .should('be.visible')
-          .should('not.have.text', 'max')
-          .should('not.have.text', 'MAX')
-          .should('not.have.text', 'MaX')
-          // it's very hard to get the max amount separately
-          // so this test only asserts the amount set for the input field is less than user's balance
-          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-          .then(() => {
-            cy.waitUntil(
-              () =>
-                cy
-                  .findByPlaceholderText(/Enter amount/i)
-                  .then($el => Number($el.val()) > 0),
-              // optional timeouts and error messages
-              {
-                errorMsg:
-                  'was expecting a numerical input value greater than 0',
-                timeout: 5000,
-                interval: 500
-              }
-            ).then(() => {
-              cy.findByPlaceholderText(/Enter amount/i)
-                .invoke('val')
-                .then(value => {
-                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-                })
-            })
+      cy.findByPlaceholderText(/Enter amount/i)
+        .should('be.visible')
+        .should('not.have.text', 'max')
+        .should('not.have.text', 'MAX')
+        // it's very hard to get the max amount separately
+        // so this test only asserts the amount set for the input field is less than user's balance
+        // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+        .then(() => {
+          cy.waitUntil(
+            () =>
+              cy
+                .findByPlaceholderText(/Enter amount/i)
+                .then($el => Number($el.val()) > 0),
+            // optional timeouts and error messages
+            {
+              errorMsg: 'was expecting a numerical input value greater than 0',
+              timeout: 5000,
+              interval: 500
+            }
+          ).then(() => {
+            cy.findByPlaceholderText(/Enter amount/i)
+              .invoke('val')
+              .then(value => {
+                cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+              })
           })
-      }
-    )
-    context('?amount=56 should set transfer panel amount to 56', () => {
-      cy.visit('/', { qs: { amount: '56', walletConnectChain } })
+        })
+    })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=MaX should set transfer panel amount to maximum amount possible based on balance', () => {
+      cy.login({ networkType: 'L1', query: { amount: 'MaX' } })
+
+      cy.findByPlaceholderText(/Enter amount/i)
+        .should('be.visible')
+        .should('not.have.text', 'max')
+        .should('not.have.text', 'MAX')
+        .should('not.have.text', 'MaX')
+        // it's very hard to get the max amount separately
+        // so this test only asserts the amount set for the input field is less than user's balance
+        // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+        .then(() => {
+          cy.waitUntil(
+            () =>
+              cy
+                .findByPlaceholderText(/Enter amount/i)
+                .then($el => Number($el.val()) > 0),
+            // optional timeouts and error messages
+            {
+              errorMsg: 'was expecting a numerical input value greater than 0',
+              timeout: 5000,
+              interval: 500
+            }
+          ).then(() => {
+            cy.findByPlaceholderText(/Enter amount/i)
+              .invoke('val')
+              .then(value => {
+                cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+              })
+          })
+        })
+    })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=56 should set transfer panel amount to 56', () => {
+      cy.login({ networkType: 'L1', query: { amount: '56' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '56')
     })
-    context('?amount=1.6678 should set transfer panel amount to 1.6678', () => {
-      cy.visit('/', { qs: { amount: '1.6678', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=1.6678 should set transfer panel amount to 1.6678', () => {
+      cy.login({ networkType: 'L1', query: { amount: '1.6678' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '1.6678')
     })
-    context('?amount=6 should set transfer panel amount to 6', () => {
-      cy.visit('/', { qs: { amount: '6', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=6 should set transfer panel amount to 6', () => {
+      cy.login({ networkType: 'L1', query: { amount: '6' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '6')
     })
-    context('?amount=0.123 should set transfer panel amount to 0.123', () => {
-      cy.visit('/', { qs: { amount: '0.123', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=0.123 should set transfer panel amount to 0.123', () => {
+      cy.login({ networkType: 'L1', query: { amount: '0.123' } })
 
       cy.url().should('include', 'amount=0.123')
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.123')
     })
-    context('?amount=-0.123 should set transfer panel amount to 0.123', () => {
-      cy.visit('/', { qs: { amount: '-0.123', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=-0.123 should set transfer panel amount to 0.123', () => {
+      cy.login({ networkType: 'L1', query: { amount: '-0.123' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.123')
     })
+  })
+
+  context('should correctly populate amount input from query param', () => {
     it('?amount=asdfs should not set transfer panel amount', () => {
-      cy.visit('/', { qs: { amount: 'asdfs', walletConnectChain } })
+      cy.login({ networkType: 'L1', query: { amount: 'asdfs' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
     })
-    context('?amount=0 should set transfer panel amount to 0', () => {
-      cy.visit('/', { qs: { amount: '0', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=0 should set transfer panel amount to 0', () => {
+      cy.login({ networkType: 'L1', query: { amount: '0' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0')
     })
-    context('?amount=0.0001 should set transfer panel amount to 0.0001', () => {
-      cy.visit('/', { qs: { amount: '0.0001', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=0.0001 should set transfer panel amount to 0.0001', () => {
+      cy.login({ networkType: 'L1', query: { amount: '0.0001' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.0001')
     })
-    context('?amount=123,3,43 should not set transfer panel amount', () => {
-      cy.visit('/', { qs: { amount: '123,3,43', walletConnectChain } })
+  })
+
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=123,3,43 should not set transfer panel amount', () => {
+      cy.login({ networkType: 'L1', query: { amount: '123,3,43' } })
 
       cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
     })
-    context(
-      '?amount=0, 123.222, 0.3 should not set transfer panel amount',
-      () => {
-        cy.visit('/', { qs: { amount: '0, 123.222, 0.3', walletConnectChain } })
+  })
 
-        cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
-      }
-    )
+  context('should correctly populate amount input from query param', () => {
+    it('?amount=0, 123.222, 0.3 should not set transfer panel amount', () => {
+      cy.login({ networkType: 'L1', query: { amount: '0, 123.222, 0.3' } })
+
+      cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
+    })
   })
 })
