262612bd5fd6ac52a90a34b652355e96207805fd
Christophe <contact@christophedeveaux.com>
2023-09-22 14:38:27 +0200

    Revert change in tab

--- packages/arb-token-bridge-ui/src/components/common/Tab.tsx
+++ packages/arb-token-bridge-ui/src/components/common/Tab.tsx
@@ -1,27 +1,19 @@
-import { twMerge } from 'tailwind-merge'
 import { Tab } from '@headlessui/react'
 import { forwardRef, PropsWithChildren } from 'react'
-import { Tooltip } from './Tooltip'
 
 export type TabButtonProps = PropsWithChildren<
   React.ButtonHTMLAttributes<HTMLButtonElement>
 >
 
-const TabButtonInner = forwardRef<HTMLButtonElement, TabButtonProps>(
+export const TabButton = forwardRef<HTMLButtonElement, TabButtonProps>(
   (props, ref) => {
     const tabButtonClassName =
-      'rounded-tl-lg rounded-tr-lg px-8 py-3 transition-colors duration-300 ui-selected:bg-white ui-not-selected:bg-ocl-blue ui-not-selected:text-white '
+      'rounded-tl-lg rounded-tr-lg px-8 py-3 transition-colors duration-300 ui-selected:bg-white ui-not-selected:bg-ocl-blue ui-not-selected:text-white ui-not-selected:hover:text-gray-4'
 
     return (
       <Tab
         ref={ref}
-        className={twMerge(
-          tabButtonClassName,
-          props.className,
-          props.disabled
-            ? 'disabled:opacity-50'
-            : 'ui-not-selected:hover:text-gray-4'
-        )}
+        className={`${tabButtonClassName} ${props.className ?? ''}`}
         {...props}
       >
         {props.children}
@@ -30,22 +22,4 @@ const TabButtonInner = forwardRef<HTMLButtonElement, TabButtonProps>(
   }
 )
 
-export const TabButton = forwardRef<HTMLButtonElement, TabButtonProps>(
-  (props, ref) => {
-    if (props.disabled) {
-      return (
-        <Tooltip
-          content={<span>[CCTP disabled text]</span>}
-          wrapperClassName="inline"
-        >
-          <TabButtonInner {...props} ref={ref} />
-        </Tooltip>
-      )
-    }
-
-    return <TabButtonInner {...props} ref={ref} />
-  }
-)
-
-TabButtonInner.displayName = 'TabButtonContent'
 TabButton.displayName = 'TabButton'
