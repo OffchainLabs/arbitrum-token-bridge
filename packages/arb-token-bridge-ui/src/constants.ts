174fa23704678405007331b7d924366f4dbd6c59
Bartek <bartlomiejkramek@outlook.com>
2023-09-26 11:01:29 +0200

    update foundation links

--- packages/arb-token-bridge-ui/src/constants.ts
+++ packages/arb-token-bridge-ui/src/constants.ts
@@ -3,23 +3,23 @@ export const TOS_VERSION = 1
 export const TOS_LOCALSTORAGE_KEY = 'arbitrum:bridge:tos-v' + TOS_VERSION
 
 export const GET_HELP_LINK =
-  'https://arbitrumfoundation.zendesk.com/hc/en-us/requests/new?ticket_form_id=18155929976987'
+  'https://support.arbitrum.io/hc/en-us/requests/new?ticket_form_id=18155929976987'
 
 export const DOCS_DOMAIN = 'https://docs.arbitrum.io'
 
 export const USDC_LEARN_MORE_LINK = `${DOCS_DOMAIN}/bridge-tokens/concepts/usdc-concept`
 
 export const FAST_BRIDGE_ARTICLE_LINK =
-  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213771832987'
+  'https://support.arbitrum.io/hc/en-us/articles/18213771832987'
 
 export const TOKEN_APPROVAL_ARTICLE_LINK =
-  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213893952923'
+  'https://support.arbitrum.io/hc/en-us/articles/18213893952923'
 
 export const ETH_BALANCE_ARTICLE_LINK =
-  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213854684699'
+  'https://support.arbitrum.io/hc/en-us/articles/18213854684699'
 
 export const CONFIRMATION_PERIOD_ARTICLE_LINK =
-  'https://arbitrumfoundation.zendesk.com/hc/en-us/articles/18213843096091'
+  'https://support.arbitrum.io/hc/en-us/articles/18213843096091'
 
 export const ORBIT_QUICKSTART_LINK =
   'https://docs.arbitrum.io/launch-orbit-chain/orbit-quickstart'
