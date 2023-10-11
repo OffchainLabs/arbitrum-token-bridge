dcf63eadb420077db7d8a5d41e9778f7c73c088d
dewanshparashar <dewanshparashar@yahoo.in>
2023-10-11 14:39:30 +0400

    Dev: remove error

--- packages/arb-token-bridge-ui/src/components/common/Header.tsx
+++ packages/arb-token-bridge-ui/src/components/common/Header.tsx
@@ -215,13 +215,7 @@ export function Header() {
     <header id="header" className={defaultHeaderClassName}>
       <div className="flex w-full max-w-[1440px] justify-between px-8">
         <div className="flex items-center lg:space-x-2 xl:space-x-12">
-          <a
-            href="/"
-            className="arb-hover flex shrink-0 flex-col items-center"
-            onClick={() => {
-              throw new Error('Testing sentry, ignore this error')
-            }}
-          >
+          <a href="/" className="arb-hover flex shrink-0 flex-col items-center">
             <HeaderImageElement
               src={HeaderLogoMainnetSVG}
               alt="Arbitrum logo"
