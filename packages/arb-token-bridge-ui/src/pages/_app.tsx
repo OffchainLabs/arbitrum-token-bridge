0daca15bcd0aafb2f5666acee6a6c52d51da9bb9
Fionna Chan <fionnachan@users.noreply.github.com>
2023-10-16 23:57:54 +0800

    move viewport

--- packages/arb-token-bridge-ui/src/pages/_app.tsx
+++ packages/arb-token-bridge-ui/src/pages/_app.tsx
@@ -78,6 +78,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{siteTitle}</title>
       </Head>
       <Layout>
