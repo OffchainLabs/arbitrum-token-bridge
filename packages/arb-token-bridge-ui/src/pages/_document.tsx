f880889883995b9afc6c7527e0caa6d0a8bb1658
Fionna Chan <fionnachan@users.noreply.github.com>
2023-10-16 22:52:44 +0800

    extract site domain

--- packages/arb-token-bridge-ui/src/pages/_document.tsx
+++ packages/arb-token-bridge-ui/src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export const siteTitle = 'Bridge to Arbitrum'
+const siteDomain = 'https://bridge.arbitrum.io'
 const siteDescription =
   'Built to scale Ethereum, Arbitrum brings you 10x lower costs while inheriting Ethereum’s security model. Arbitrum is a Layer 2 Optimistic Rollup.'
 
@@ -16,25 +17,19 @@ export default function Document() {
         <meta name="theme-color" content="#000000" />
         <meta name="description" content={siteDescription} />
         {/* <!-- Facebook Meta Tags --> */}
-        <meta property="og:url" content="https://bridge.arbitrum.io" />
+        <meta property="og:url" content={siteDomain} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={siteTitle} />
         <meta property="og:description" content={siteDescription} />
-        <meta
-          property="og:image"
-          content="https://bridge.arbitrum.io/og-image.jpg"
-        />
+        <meta property="og:image" content={`${siteDomain}/og-image.jpg`} />
 
         {/* <!-- Twitter Meta Tags --> */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="bridge.arbitrum.io" />
-        <meta property="twitter:url" content="https://bridge.arbitrum.io" />
+        <meta property="twitter:url" content={siteDomain} />
         <meta name="twitter:title" content={siteTitle} />
         <meta name="twitter:description" content={siteDescription} />
-        <meta
-          name="twitter:image"
-          content="https://bridge.arbitrum.io/og-image.jpg"
-        />
+        <meta name="twitter:image" content={`${siteDomain}/og-image.jpg`} />
       </Head>
       <body>
         <Main />
