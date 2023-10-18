2995aa724ff3812d8d5816805ec8441c97f26a74
gzeon <im@gzeon.dev>
2023-10-18 23:26:50 +0800

    fix: some more hardcoded addresses

--- packages/arb-token-bridge-ui/tests/support/common.ts
+++ packages/arb-token-bridge-ui/tests/support/common.ts
@@ -38,7 +38,7 @@ export const getL1NetworkConfig = (): NetworkConfig => {
     chainId: '1337',
     symbol: 'ETH',
     isTestnet: true,
-    multiCall: '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48'
+    multiCall: '0xA39FFA43ebA037D67a0f4fe91956038ABA0CA386'
   }
 }
 
@@ -53,7 +53,7 @@ export const getL2NetworkConfig = (): NetworkConfig => {
   }
 }
 
-export const wethTokenAddressL1 = '0x408Da76E87511429485C32E4Ad647DD14823Fdc4'
+export const wethTokenAddressL1 = '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48'
 export const wethTokenAddressL2 = '0x408Da76E87511429485C32E4Ad647DD14823Fdc4'
 export const ERC20TokenName = 'IntArbTestToken'
 export const ERC20TokenSymbol = 'IARB'
