59c33c31c0f9ab3c75b91078a971635eee9098cc
Fionna Chan <fionnachan@users.noreply.github.com>
2023-10-19 00:12:04 +0800

    update hardcoded l1 weth gateway addr

--- packages/arb-token-bridge-ui/tests/support/common.ts
+++ packages/arb-token-bridge-ui/tests/support/common.ts
@@ -53,6 +53,7 @@ export const getL2NetworkConfig = (): NetworkConfig => {
   }
 }
 
+export const l1WethGateway = '0x408Da76E87511429485C32E4Ad647DD14823Fdc4'
 export const wethTokenAddressL1 = '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48'
 export const wethTokenAddressL2 = '0x408Da76E87511429485C32E4Ad647DD14823Fdc4'
 export const ERC20TokenName = 'IntArbTestToken'
