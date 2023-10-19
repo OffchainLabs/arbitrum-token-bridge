888352d2c01b52b60bcc1f152ae0f903a5e42255
Bartek <bartlomiejkramek@outlook.com>
2023-10-19 12:27:33 +0200

    fix: display sent and received txs in a single table (#1167)
    Co-authored-by: Fionna Chan <13184582+fionnachan@users.noreply.github.com>
    Co-authored-by: spsjvc <dragisaspasojevic@gmail.com>

--- packages/arb-token-bridge-ui/src/util/withdrawals/fetchETHWithdrawalsFromEventLogs.ts
+++ packages/arb-token-bridge-ui/src/util/withdrawals/fetchETHWithdrawalsFromEventLogs.ts
@@ -5,26 +5,30 @@ import { L2ToL1MessageReader } from '@arbitrum/sdk'
  * Fetches initiated ETH withdrawals from event logs in range of [fromBlock, toBlock].
  *
  * @param query Query params
- * @param query.toAddress Address that will receive the funds
+ * @param query.receiver Address that will receive the funds
  * @param query.fromBlock Start at this block number (including)
  * @param query.toBlock Stop at this block number (including)
  * @param query.l2Provider Provider for the L2 network
  */
 export function fetchETHWithdrawalsFromEventLogs({
-  toAddress,
+  receiver,
   fromBlock,
   toBlock,
   l2Provider
 }: {
-  toAddress?: string
+  receiver?: string
   fromBlock: BlockTag
   toBlock: BlockTag
   l2Provider: Provider
 }) {
+  if (typeof receiver === 'undefined') {
+    return []
+  }
+  // funds sent by this address
   return L2ToL1MessageReader.getL2ToL1Events(
     l2Provider,
     { fromBlock, toBlock },
     undefined,
-    toAddress
+    receiver
   )
 }
