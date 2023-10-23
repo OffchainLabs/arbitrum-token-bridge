cf81a2a5c4782ac9854f4aba502ea6f54fa3c522
Fionna Chan <fionnachan@users.noreply.github.com>
2023-10-23 13:40:53 +0100

    remove estimateGas call

--- packages/arb-token-bridge-ui/src/util/TokenWithdrawalUtils.ts
+++ packages/arb-token-bridge-ui/src/util/TokenWithdrawalUtils.ts
@@ -1,10 +1,8 @@
 import { Erc20Bridger } from '@arbitrum/sdk'
 import { Provider } from '@ethersproject/providers'
 import { BigNumber } from 'ethers'
-import {
-  ARB_RETRYABLE_TX_ADDRESS,
-  NODE_INTERFACE_ADDRESS
-} from '@arbitrum/sdk/dist/lib/dataEntities/constants'
+import * as Sentry from '@sentry/react'
+import { NODE_INTERFACE_ADDRESS } from '@arbitrum/sdk/dist/lib/dataEntities/constants'
 import { PublicClient, isHex } from 'viem'
 
 import { GasEstimates } from '../hooks/arbTokenBridge.types'
@@ -38,32 +36,33 @@ export async function withdrawTokenEstimateGas({
     ? withdrawalRequest.txRequest.data
     : '0x'
 
-  const gasComponents = await arbPublicClient.simulateContract({
-    address: NODE_INTERFACE_ADDRESS,
-    abi: nodeInterfaceABI,
-    functionName: 'gasEstimateL1Component',
-    args: [ARB_RETRYABLE_TX_ADDRESS, false, withdrawalRequestCalldata],
-    account: address
-  })
+  try {
+    const gasComponents = await arbPublicClient.simulateContract({
+      address: NODE_INTERFACE_ADDRESS,
+      abi: nodeInterfaceABI,
+      functionName: 'gasEstimateComponents',
+      args: [address, false, withdrawalRequestCalldata],
+      account: address
+    })
 
-  // This is the gas needed to execute the withdrawal on L1; not the batch posting fee
-  // 1st result in the array is `gasEstimateForL1` according to ABI
-  const estimatedL1Gas = BigNumber.from(gasComponents.result[0])
+    // This is the gas needed to execute the withdrawal on L1; not the batch posting fee
+    // 1st result in the array is `gasEstimateForL1` according to ABI
+    const estimatedL1Gas = BigNumber.from(gasComponents.result[1])
 
-  const estimatedTotalGas = BigNumber.from(
-    Math.ceil(
-      Number(
-        await arbPublicClient.estimateGas({
-          data: withdrawalRequestCalldata,
-          account: address,
-          to: address
-        })
-      ) * 1.3
-      // add 30% to the estimated total gas as buffer
+    // add 30% to the estimated total gas as buffer
+    const estimatedTotalGas = BigNumber.from(
+      Math.ceil(Number(gasComponents.result[0]) * 1.3)
     )
-  )
 
-  const estimatedL2Gas = estimatedTotalGas.sub(estimatedL1Gas)
+    const estimatedL2Gas = estimatedTotalGas.sub(estimatedL1Gas)
 
-  return { estimatedL1Gas, estimatedL2Gas }
+    return { estimatedL1Gas, estimatedL2Gas }
+  } catch (error) {
+    Sentry.captureException(error)
+    // figures based on gas estimation returned
+    return {
+      estimatedL1Gas: BigNumber.from(498_000),
+      estimatedL2Gas: BigNumber.from(150_000)
+    }
+  }
 }
