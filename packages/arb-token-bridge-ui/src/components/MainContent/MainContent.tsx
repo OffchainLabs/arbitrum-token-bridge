7d054020d0d7306a04e6d0e0bab70f064fb997ba
Bartek <bartlomiejkramek@outlook.com>
2023-11-14 13:55:39 +0100

    bump

--- packages/arb-token-bridge-ui/src/components/MainContent/MainContent.tsx
+++ packages/arb-token-bridge-ui/src/components/MainContent/MainContent.tsx
@@ -49,8 +49,8 @@ export function MainContent() {
         </AnimatePresence>
       </div>
       <SidePanel
-        heading="Transaction history"
         isOpen={isTransactionHistoryPanelVisible}
+        heading="Transaction History"
         onClose={closeTransactionHistoryPanel}
       >
         <NewTransactionHistory />
