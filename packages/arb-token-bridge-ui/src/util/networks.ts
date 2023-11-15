ecc5754db821afa18fe77a125daa4d60bcab32b6
Fionna Chan <fionnachan@users.noreply.github.com>
2023-11-15 11:11:21 +0000

    chore: remove rinkeby from codebase

--- packages/arb-token-bridge-ui/src/util/networks.ts
+++ packages/arb-token-bridge-ui/src/util/networks.ts
@@ -142,10 +142,6 @@ export enum ChainId {
   // L1
   Mainnet = 1,
   // L1 Testnets
-  /**
-   * Rinkeby is deprecated, but we are keeping it in order to detect it and point to Goerli instead.
-   */
-  Rinkeby = 4,
   Goerli = 5,
   Local = 1337,
   Sepolia = 11155111,
@@ -153,10 +149,6 @@ export enum ChainId {
   ArbitrumOne = 42161,
   ArbitrumNova = 42170,
   // L2 Testnets
-  /**
-   * Arbitrum Rinkeby is deprecated, but we are keeping it in order to detect it and point to Arbitrum Goerli instead.
-   */
-  ArbitrumRinkeby = 421611,
   ArbitrumGoerli = 421613,
   ArbitrumSepolia = 421614,
   ArbitrumLocal = 412346,
@@ -407,7 +399,6 @@ export function isNetwork(chainId: ChainId) {
 
   const isMainnet = chainId === ChainId.Mainnet
 
-  const isRinkeby = chainId === ChainId.Rinkeby
   const isGoerli = chainId === ChainId.Goerli
   const isSepolia = chainId === ChainId.Sepolia
   const isLocal = chainId === ChainId.Local
@@ -416,19 +407,17 @@ export function isNetwork(chainId: ChainId) {
   const isArbitrumNova = chainId === ChainId.ArbitrumNova
   const isArbitrumGoerli = chainId === ChainId.ArbitrumGoerli
   const isArbitrumSepolia = chainId === ChainId.ArbitrumSepolia
-  const isArbitrumRinkeby = chainId === ChainId.ArbitrumRinkeby
   const isArbitrumLocal = chainId === ChainId.ArbitrumLocal
 
   const isXaiTestnet = chainId === ChainId.XaiTestnet
   const isStylusTestnet = chainId === ChainId.StylusTestnet
 
-  const isEthereum = isMainnet || isRinkeby || isGoerli || isSepolia || isLocal
+  const isEthereum = isMainnet || isGoerli || isSepolia || isLocal
 
   const isArbitrum =
     isArbitrumOne ||
     isArbitrumNova ||
     isArbitrumGoerli ||
-    isArbitrumRinkeby ||
     isArbitrumLocal ||
     isArbitrumSepolia
 
@@ -436,11 +425,9 @@ export function isNetwork(chainId: ChainId) {
   const isCustomOrbitChain = customChainIds.includes(chainId)
 
   const isTestnet =
-    isRinkeby ||
     isGoerli ||
     isLocal ||
     isArbitrumGoerli ||
-    isArbitrumRinkeby ||
     isSepolia ||
     isArbitrumSepolia ||
     isXaiTestnet ||
@@ -463,7 +450,6 @@ export function isNetwork(chainId: ChainId) {
     isMainnet,
     isEthereum,
     // L1 Testnets
-    isRinkeby,
     isGoerli,
     isSepolia,
     // L2
@@ -471,7 +457,6 @@ export function isNetwork(chainId: ChainId) {
     isArbitrumOne,
     isArbitrumNova,
     // L2 Testnets
-    isArbitrumRinkeby,
     isArbitrumGoerli,
     isArbitrumSepolia,
     // Orbit chains
