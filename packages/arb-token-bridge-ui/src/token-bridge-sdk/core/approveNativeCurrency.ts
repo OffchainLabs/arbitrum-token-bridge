b7cd98c291a4ecb8c3ce5eb96bf09fc04c757c8f
dewanshparashar <dewanshparashar@yahoo.in>
2023-12-04 14:11:28 +0400

    Dev: removed unused logic for now, will come in next pr

--- packages/arb-token-bridge-ui/src/token-bridge-sdk/core/approveNativeCurrency.ts
+++ packages/arb-token-bridge-ui/src/token-bridge-sdk/core/approveNativeCurrency.ts
@@ -1,20 +1,7 @@
-import { EthBridger } from '@arbitrum/sdk'
 import { Provider } from '@ethersproject/providers'
 import { Signer } from 'ethers'
 
 export type ApproveNativeCurrencyProps = {
   signer: Signer
   destinationChainProvider: Provider
-}
-
-export async function approveNativeCurrency({
-  signer,
-  destinationChainProvider
-}: ApproveNativeCurrencyProps): Promise<void> {
-  const ethBridger = await EthBridger.fromProvider(destinationChainProvider)
-
-  const approveCustomFeeTokenTx = await ethBridger.approveFeeToken({
-    l1Signer: signer
-  })
-  await approveCustomFeeTokenTx.wait()
 }
