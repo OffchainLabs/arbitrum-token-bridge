b7a2e6d44c15f5572f9f18da7070fcc7f1e57085
Fionna Chan <fionnachan@users.noreply.github.com>
2023-12-05 11:50:01 +0000

    update eslint parser

--- packages/arb-token-bridge-ui/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const basePath = path.join(process.cwd(), '/../..')
-
-module.exports = {
-  extends: `${basePath}/.eslintrc.js`,
-  parserOptions: {
-    project: path.resolve('./tsconfig.json')
-  }
-}
