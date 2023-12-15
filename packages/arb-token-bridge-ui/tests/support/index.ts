b8b39fcd720a834f695811bfb66f57b0f5a9447e
Christophe <contact@christophedeveaux.com>
2023-12-15 14:14:42 +0100

    Remove log in support file

--- packages/arb-token-bridge-ui/tests/support/index.ts
+++ packages/arb-token-bridge-ui/tests/support/index.ts
@@ -12,9 +12,8 @@ Cypress.Keyboard.defaults({
   // tests are flaky in CI with low keystroke delay
   keystrokeDelay: 150
 })
-console.log('SUPPORT')
+
 before(() => {
-  console.log('before')
   // connect to goerli to avoid connecting to localhost twice and failing
   cy.setupMetamask(Cypress.env('PRIVATE_KEY'), 'goerli')
 
