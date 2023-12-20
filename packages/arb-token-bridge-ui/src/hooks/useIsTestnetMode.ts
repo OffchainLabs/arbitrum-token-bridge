5929463e045a345a1ff6f7434a3a26307393933d
Bartek <bartlomiejkramek@outlook.com>
2023-12-20 11:26:43 +0100

    default hook to false

--- packages/arb-token-bridge-ui/src/hooks/useIsTestnetMode.ts
+++ packages/arb-token-bridge-ui/src/hooks/useIsTestnetMode.ts
@@ -3,5 +3,9 @@ import { useLocalStorage } from 'react-use'
 const testnetModeLocalStorageKey = 'arbitrum:bridge:settings:testnetMode'
 
 export const useIsTestnetMode = () => {
-  return useLocalStorage<boolean>(testnetModeLocalStorageKey)
+  const [isTestnetMode = false, setIsTestnetMode] = useLocalStorage<boolean>(
+    testnetModeLocalStorageKey
+  )
+
+  return [isTestnetMode, setIsTestnetMode]
 }
