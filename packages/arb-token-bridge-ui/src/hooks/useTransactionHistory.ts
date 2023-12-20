2a68730aae4959b07b97ec34fc54b1491a53bb5b
Bartek <bartlomiejkramek@outlook.com>
2023-12-20 11:22:59 +0100

    revert not needed change

--- packages/arb-token-bridge-ui/src/hooks/useTransactionHistory.ts
+++ packages/arb-token-bridge-ui/src/hooks/useTransactionHistory.ts
@@ -67,8 +67,6 @@ export type Withdrawal =
 type DepositOrWithdrawal = Deposit | Withdrawal
 type Transfer = DepositOrWithdrawal | MergedTransaction
 
-type ChainPair = { parentChain: ChainId; chain: ChainId }
-
 function getStandardizedTimestampByTx(tx: Transfer) {
   if (isCctpTransfer(tx)) {
     return (tx.createdAt ?? 0) / 1_000
@@ -91,7 +89,7 @@ function sortByTimestampDescending(a: Transfer, b: Transfer) {
     : 1
 }
 
-const multiChainFetchList: ChainPair[] = [
+const multiChainFetchList: { parentChain: ChainId; chain: ChainId }[] = [
   {
     parentChain: ChainId.Ethereum,
     chain: ChainId.ArbitrumOne
