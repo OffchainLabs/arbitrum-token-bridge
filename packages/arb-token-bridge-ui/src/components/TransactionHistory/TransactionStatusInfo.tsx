85da781d7f23c86f1241e670e66d53003ac120b9
Bartek <bartlomiejkramek@outlook.com>
2023-12-20 15:06:41 +0100

    status ui

---
+++ packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionStatusInfo.tsx
@@ -0,0 +1,109 @@
+/*
+A small info banner that we show when the user has some pending claim withdrawals OR some retryables to redeem.
+Format: "You have [X] deposits to retry and [Y] withdrawals ready to claim. [CTA]"
+*/
+
+import { useAccount } from 'wagmi'
+import { useMemo } from 'react'
+import {
+  CheckCircleIcon,
+  InformationCircleIcon
+} from '@heroicons/react/24/outline'
+import { twMerge } from 'tailwind-merge'
+import { useNetworksAndSigners } from '../../hooks/useNetworksAndSigners'
+
+import { isDepositReadyToRedeem } from '../../state/app/utils'
+import { shouldTrackAnalytics, trackEvent } from '../../util/AnalyticsUtils'
+import { getNetworkName } from '../../util/networks'
+import { useAppContextActions } from '../App/AppContext'
+import { ExternalLink } from '../common/ExternalLink'
+import { useTransactionHistory } from '../../hooks/useTransactionHistory'
+import { isTxClaimable } from './helpers'
+
+export const TransactionStatusInfo = () => {
+  const { address } = useAccount()
+  const {
+    l2: { network: l2Network }
+  } = useNetworksAndSigners()
+  const l2NetworkName = getNetworkName(l2Network.id)
+  const { openTransactionHistoryPanel } = useAppContextActions()
+  const {
+    data: { transactions }
+  } = useTransactionHistory(address)
+
+  const { numTransactionsReadyToClaim, numRetryablesToRedeem } = useMemo(() => {
+    return transactions.reduce(
+      (acc, tx) => {
+        // standard bridge withdrawal or cctp deposit/withdrawal
+        if (isTxClaimable(tx)) {
+          acc.numTransactionsReadyToClaim += 1
+        }
+        // failed retryable
+        if (isDepositReadyToRedeem(tx)) {
+          acc.numRetryablesToRedeem += 1
+        }
+        return acc
+      },
+      { numTransactionsReadyToClaim: 0, numRetryablesToRedeem: 0 }
+    )
+  }, [transactions])
+
+  // don't show this banner if user doesn't have anything to claim or redeem
+  if (numTransactionsReadyToClaim === 0 && numRetryablesToRedeem === 0)
+    return null
+
+  return (
+    <div
+      className={twMerge(
+        'mx-0 flex cursor-pointer flex-wrap items-center gap-1  p-2 text-sm lg:flex-nowrap lg:rounded-md lg:text-base',
+        numRetryablesToRedeem
+          ? 'bg-brick text-brick-dark'
+          : 'bg-lime text-lime-dark'
+      )}
+      onClick={() => {
+        openTransactionHistoryPanel()
+        if (shouldTrackAnalytics(l2NetworkName)) {
+          trackEvent('Open Transaction History Click', {
+            pageElement: 'Tx Info Banner'
+          })
+        }
+      }}
+    >
+      <div className="inline">
+        {numRetryablesToRedeem ? (
+          <InformationCircleIcon className="-mt-1 mr-2 inline h-4 w-4" />
+        ) : (
+          <CheckCircleIcon className="-mt-1 mr-2 inline h-4 w-4" />
+        )}
+        You have{` `}
+        {/* deposits ready to retry */}
+        {numRetryablesToRedeem ? (
+          <span className="font-bold">
+            {`${numRetryablesToRedeem} ${
+              numRetryablesToRedeem > 1 ? 'deposits' : 'deposit'
+            } to retry`}
+          </span>
+        ) : null}
+        {/* and */}
+        {numRetryablesToRedeem && numTransactionsReadyToClaim ? (
+          <span>
+            {` `}and{` `}
+          </span>
+        ) : null}
+        {/* withdrawals ready to claim text */}
+        {numTransactionsReadyToClaim ? (
+          <span className="font-bold">
+            {`${numTransactionsReadyToClaim} ${
+              numTransactionsReadyToClaim > 1 ? 'transactions' : 'transaction'
+            } ready to claim`}
+          </span>
+        ) : null}
+        <span>.{` `}</span>
+        {/* open tx history panel cta */}
+        <ExternalLink className="arb-hover text-sm underline lg:text-base">
+          Open Transaction History panel.
+        </ExternalLink>
+      </div>
+    </div>
+  )
+}
