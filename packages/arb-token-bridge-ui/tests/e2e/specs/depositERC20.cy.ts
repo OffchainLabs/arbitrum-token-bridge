d950dd08fdc875a36b2c96fc1c7cb22f3041899c
Bartek <bartlomiejkramek@outlook.com>
2023-12-21 17:08:34 +0100

    feat: new tx history - e2e (#1372)

--- packages/arb-token-bridge-ui/tests/e2e/specs/depositERC20.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/depositERC20.cy.ts
@@ -117,10 +117,11 @@ describe('Deposit ERC20 Token', () => {
           .click()
           .then(() => {
             cy.confirmMetamaskTransaction().then(() => {
+              cy.findByText('~10 mins remaining').should('be.visible')
               cy.findByText(
-                `Moving ${formatAmount(ERC20AmountToSend, {
+                `${formatAmount(ERC20AmountToSend, {
                   symbol: 'WETH'
-                })} to Arbitrum`
+                })}`
               ).should('be.visible')
             })
           })
