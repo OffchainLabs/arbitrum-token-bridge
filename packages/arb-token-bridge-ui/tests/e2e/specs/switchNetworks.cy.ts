f37dcdb96c0fb10abac69a1dc102316ba9ee6e4b
Fionna Chan <fionnachan@users.noreply.github.com>
2024-01-11 10:59:13 +0000

    update switchNetworks.cy.ts

--- packages/arb-token-bridge-ui/tests/e2e/specs/switchNetworks.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/switchNetworks.cy.ts
@@ -1,7 +1,3 @@
-/**
- * When user wants to bridge ETH from L1 to L2
- */
-
 describe('Switch Networks', () => {
   context('User is on test network L1', () => {
     it('should show L1 and L2 chains correctly', () => {
@@ -11,36 +7,20 @@ describe('Switch Networks', () => {
     })
 
     context(
-      'User is connected to Ethereum and source chain is Ethereum',
+      'User is connected to Ethereum, source chain is Ethereum and destination chain is Arbitrum',
       () => {
-        it('should change network to Arbitrum One successfully', () => {
+        it('should switch "from: Ethereum" to "from: Arbitrum" successfully', () => {
           cy.login({
             networkType: 'L1'
           })
-          // cy.waitUntil(
-          //   () =>
+
           cy.findByRole('button', { name: /From: Ethereum/i }).should(
             'be.visible'
           )
-          //       ,
-          //   {
-          //     errorMsg: "Can't find 'From: Ethereum'",
-          //     timeout: 10000,
-          //     interval: 500
-          //   }
-          // )
-          // cy.waitUntil(
-          //   () =>
+
           cy.findByRole('button', { name: /Switch Networks/i })
             .should('be.visible')
             .click()
-          //       ,
-          //   {
-          //     errorMsg: "Can't find 'Switch Networks'",
-          //     timeout: 10000,
-          //     interval: 500
-          //   }
-          // )
 
           cy.findByRole('button', {
             name: /From: Arbitrum/i
