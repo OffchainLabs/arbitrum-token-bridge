45bfb3859d80628d8a04ce2cf92ae09ce1c1f5bf
Fionna Chan <fionnachan@users.noreply.github.com>
2024-01-11 15:06:34 +0000

    remove commented code

--- packages/arb-token-bridge-ui/src/components/TransferPanel/EstimatedGas.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/EstimatedGas.tsx
@@ -93,8 +93,6 @@ export function EstimatedGas({ chainType }: { chainType: 'parent' | 'child' }) {
   return (
     <div
       className={twMerge(
-        // 'grid items-center rounded-md bg-white/25 px-3 py-2 text-right text-xs font-light text-white',
-        // showPrice ? 'grid-cols-3' : 'grid-cols-2'
         'flex items-center justify-between rounded-md bg-white/25 px-3 py-2 text-right text-xs font-light text-white'
       )}
     >
