a6257b4fe35993fc1b763da9a98402bb1fbdf4bc
Christophe <contact@christophedeveaux.com>
2024-01-15 16:32:11 +0100

    Wait for query param to be set before running test

--- packages/arb-token-bridge-ui/tests/e2e/specs/urlQueryParam.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/urlQueryParam.cy.ts
@@ -15,6 +15,7 @@ describe('User enters site with query params on URL', () => {
       Cypress.env('ETH_RPC_URL'),
       Cypress.env('ADDRESS')
     ).then(val => (l1ETHbal = parseFloat(formatAmount(val, { decimals: 18 }))))
+    cy.login({ networkType: 'L1' })
   })
 
   it('should correctly populate amount input from query param', () => {
@@ -22,42 +23,42 @@ describe('User enters site with query params on URL', () => {
     context(
       '?amount=max should set transfer panel amount to maximum amount possible based on balance',
       () => {
-        cy.login({ networkType: 'L1' })
         cy.visit('/', {
           qs: {
             amount: 'max',
             sourceChain: 'custom-localhost',
             destinationChain: 'arbitrum-localhost'
           }
-        })
-        cy.findByPlaceholderText(/Enter amount/i)
-          .should('be.visible')
-          .should('not.have.text', 'max')
-          .should('not.have.text', 'MAX')
-          // it's very hard to get the max amount separately
-          // so this test only asserts the amount set for the input field is less than user's balance
-          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-          .then(() => {
-            cy.waitUntil(
-              () =>
-                cy
-                  .findByPlaceholderText(/Enter amount/i)
-                  .then($el => Number($el.val()) > 0),
-              // optional timeouts and error messages
-              {
-                errorMsg:
-                  'was expecting a numerical input value greater than 0',
-                timeout: 5000,
-                interval: 500
-              }
-            ).then(() => {
-              cy.findByPlaceholderText(/Enter amount/i)
-                .invoke('val')
-                .then(value => {
-                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-                })
+        }).then(() => {
+          cy.findByPlaceholderText(/Enter amount/i)
+            .should('be.visible')
+            .should('not.have.text', 'max')
+            .should('not.have.text', 'MAX')
+            // it's very hard to get the max amount separately
+            // so this test only asserts the amount set for the input field is less than user's balance
+            // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+            .then(() => {
+              cy.waitUntil(
+                () =>
+                  cy
+                    .findByPlaceholderText(/Enter amount/i)
+                    .then($el => Number($el.val()) > 0),
+                // optional timeouts and error messages
+                {
+                  errorMsg:
+                    'was expecting a numerical input value greater than 0',
+                  timeout: 5000,
+                  interval: 500
+                }
+              ).then(() => {
+                cy.findByPlaceholderText(/Enter amount/i)
+                  .invoke('val')
+                  .then(value => {
+                    cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+                  })
+              })
             })
-          })
+        })
       }
     )
     context(
