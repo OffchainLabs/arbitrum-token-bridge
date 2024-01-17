0b36f95be08092d7f02a7cf8130c41fb25864498
Fionna Chan <fionnachan@users.noreply.github.com>
2024-01-17 16:27:12 +0000

    wrap each login in a context

--- packages/arb-token-bridge-ui/tests/e2e/specs/urlQueryParam.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/urlQueryParam.cy.ts
@@ -18,252 +18,290 @@ describe('User enters site with query params on URL', () => {
   })
 
   // only ETH is supported for now so by default the following tests are assumed to be ETH
-  it('?amount=max should set transfer panel amount to maximum amount possible based on balance', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: 'max',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
-
-    cy.findByPlaceholderText(/Enter amount/i)
-      .should('be.visible')
-      .should('not.have.text', 'max')
-      .should('not.have.text', 'MAX')
-      // it's very hard to get the max amount separately
-      // so this test only asserts the amount set for the input field is less than user's balance
-      // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-      .then(() => {
-        cy.waitUntil(
-          () =>
-            cy
-              .findByPlaceholderText(/Enter amount/i)
-              .then($el => Number($el.val()) > 0),
-          // optional timeouts and error messages
-          {
-            errorMsg: 'was expecting a numerical input value greater than 0',
-            timeout: 5000,
-            interval: 500
+  context(
+    'set transfer panel amount to maximum amount possible based on balance',
+    () => {
+      it('?amount=max', () => {
+        cy.login({
+          networkType: 'L1',
+          query: {
+            amount: 'max',
+            sourceChain: 'custom-localhost',
+            destinationChain: 'arbitrum-localhost'
           }
-        ).then(() => {
-          cy.findByPlaceholderText(/Enter amount/i)
-            .invoke('val')
-            .then(value => {
-              cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-            })
         })
+
+        cy.findByPlaceholderText(/Enter amount/i)
+          .should('be.visible')
+          .should('not.have.text', 'max')
+          .should('not.have.text', 'MAX')
+          // it's very hard to get the max amount separately
+          // so this test only asserts the amount set for the input field is less than user's balance
+          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+          .then(() => {
+            cy.waitUntil(
+              () =>
+                cy
+                  .findByPlaceholderText(/Enter amount/i)
+                  .then($el => Number($el.val()) > 0),
+              // optional timeouts and error messages
+              {
+                errorMsg:
+                  'was expecting a numerical input value greater than 0',
+                timeout: 5000,
+                interval: 500
+              }
+            ).then(() => {
+              cy.findByPlaceholderText(/Enter amount/i)
+                .invoke('val')
+                .then(value => {
+                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+                })
+            })
+          })
       })
-  })
+    }
+  )
 
-  it('?amount=MAX should set transfer panel amount to maximum amount possible based on balance', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: 'MAX',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
-
-    cy.findByPlaceholderText(/Enter amount/i)
-      .should('be.visible')
-      .should('not.have.text', 'max')
-      .should('not.have.text', 'MAX')
-      // it's very hard to get the max amount separately
-      // so this test only asserts the amount set for the input field is less than user's balance
-      // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-      .then(() => {
-        cy.waitUntil(
-          () =>
-            cy
-              .findByPlaceholderText(/Enter amount/i)
-              .then($el => Number($el.val()) > 0),
-          // optional timeouts and error messages
-          {
-            errorMsg: 'was expecting a numerical input value greater than 0',
-            timeout: 5000,
-            interval: 500
+  context(
+    'set transfer panel amount to maximum amount possible based on balance',
+    () => {
+      it('?amount=MAX', () => {
+        cy.login({
+          networkType: 'L1',
+          query: {
+            amount: 'MAX',
+            sourceChain: 'custom-localhost',
+            destinationChain: 'arbitrum-localhost'
           }
-        ).then(() => {
-          cy.findByPlaceholderText(/Enter amount/i)
-            .invoke('val')
-            .then(value => {
-              cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-            })
         })
+
+        cy.findByPlaceholderText(/Enter amount/i)
+          .should('be.visible')
+          .should('not.have.text', 'max')
+          .should('not.have.text', 'MAX')
+          // it's very hard to get the max amount separately
+          // so this test only asserts the amount set for the input field is less than user's balance
+          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+          .then(() => {
+            cy.waitUntil(
+              () =>
+                cy
+                  .findByPlaceholderText(/Enter amount/i)
+                  .then($el => Number($el.val()) > 0),
+              // optional timeouts and error messages
+              {
+                errorMsg:
+                  'was expecting a numerical input value greater than 0',
+                timeout: 5000,
+                interval: 500
+              }
+            ).then(() => {
+              cy.findByPlaceholderText(/Enter amount/i)
+                .invoke('val')
+                .then(value => {
+                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+                })
+            })
+          })
       })
-  })
+    }
+  )
 
-  it('?amount=MaX should set transfer panel amount to maximum amount possible based on balance', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: 'MaX',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
-
-    cy.findByPlaceholderText(/Enter amount/i)
-      .should('be.visible')
-      .should('not.have.text', 'max')
-      .should('not.have.text', 'MAX')
-      .should('not.have.text', 'MaX')
-      // it's very hard to get the max amount separately
-      // so this test only asserts the amount set for the input field is less than user's balance
-      // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
-      .then(() => {
-        cy.waitUntil(
-          () =>
-            cy
-              .findByPlaceholderText(/Enter amount/i)
-              .then($el => Number($el.val()) > 0),
-          // optional timeouts and error messages
-          {
-            errorMsg: 'was expecting a numerical input value greater than 0',
-            timeout: 5000,
-            interval: 500
+  context(
+    'set transfer panel amount to maximum amount possible based on balance',
+    () => {
+      it('?amount=MaX', () => {
+        cy.login({
+          networkType: 'L1',
+          query: {
+            amount: 'MaX',
+            sourceChain: 'custom-localhost',
+            destinationChain: 'arbitrum-localhost'
           }
-        ).then(() => {
-          cy.findByPlaceholderText(/Enter amount/i)
-            .invoke('val')
-            .then(value => {
-              cy.wrap(Number(value)).should('be.lt', l1ETHbal)
-            })
         })
+
+        cy.findByPlaceholderText(/Enter amount/i)
+          .should('be.visible')
+          .should('not.have.text', 'max')
+          .should('not.have.text', 'MAX')
+          .should('not.have.text', 'MaX')
+          // it's very hard to get the max amount separately
+          // so this test only asserts the amount set for the input field is less than user's balance
+          // but not the exact MAX AMOUNT set by the `setMaxAmount` function in `TransferPanelMain.tsx`
+          .then(() => {
+            cy.waitUntil(
+              () =>
+                cy
+                  .findByPlaceholderText(/Enter amount/i)
+                  .then($el => Number($el.val()) > 0),
+              // optional timeouts and error messages
+              {
+                errorMsg:
+                  'was expecting a numerical input value greater than 0',
+                timeout: 5000,
+                interval: 500
+              }
+            ).then(() => {
+              cy.findByPlaceholderText(/Enter amount/i)
+                .invoke('val')
+                .then(value => {
+                  cy.wrap(Number(value)).should('be.lt', l1ETHbal)
+                })
+            })
+          })
       })
+    }
+  )
+
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=56', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '56',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
+
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '56')
+    })
   })
 
-  it('?amount=56 should set transfer panel amount to 56', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '56',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=1.6678', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '1.6678',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '56')
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '1.6678')
+    })
   })
 
-  it('?amount=1.6678 should set transfer panel amount to 1.6678', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '1.6678',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=6', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '6',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '1.6678')
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '6')
+    })
   })
 
-  it('?amount=6 should set transfer panel amount to 6', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '6',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=0.123', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '0.123',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '6')
+      cy.url().should('include', 'amount=0.123')
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.123')
+    })
   })
 
-  it('?amount=0.123 should set transfer panel amount to 0.123', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '0.123',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=-0.123', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '-0.123',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.url().should('include', 'amount=0.123')
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.123')
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.123')
+    })
   })
 
-  it('?amount=-0.123 should set transfer panel amount to 0.123', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '-0.123',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=0', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '0',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.123')
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0')
+    })
   })
 
-  it('?amount=asdfs should not set transfer panel amount', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: 'asdfs',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('set transfer panel amount to specific amount', () => {
+    it('?amount=0.0001', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '0.0001',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
+      cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.0001')
+    })
   })
 
-  it('?amount=0 should set transfer panel amount to 0', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '0',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('not set transfer panel amount to specific amount', () => {
+    it('?amount=asdfs', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: 'asdfs',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0')
+      cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
+    })
   })
 
-  it('?amount=0.0001 should set transfer panel amount to 0.0001', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '0.0001',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
+  context('not set transfer panel amount to specific amount', () => {
+    it('?amount=123,3,43', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '123,3,43',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
 
-    cy.findByPlaceholderText(/Enter amount/i).should('have.value', '0.0001')
+      cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
+    })
   })
 
-  it('?amount=123,3,43 should not set transfer panel amount', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '123,3,43',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
+  context('not set transfer panel amount to specific amount', () => {
+    it('?amount=0, 123.222, 0.3', () => {
+      cy.login({
+        networkType: 'L1',
+        query: {
+          amount: '0, 123.222, 0.3',
+          sourceChain: 'custom-localhost',
+          destinationChain: 'arbitrum-localhost'
+        }
+      })
+
+      cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
     })
-
-    cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
-  })
-
-  it('?amount=0, 123.222, 0.3 should not set transfer panel amount', () => {
-    cy.login({
-      networkType: 'L1',
-      query: {
-        amount: '0, 123.222, 0.3',
-        sourceChain: 'custom-localhost',
-        destinationChain: 'arbitrum-localhost'
-      }
-    })
-
-    cy.findByPlaceholderText(/Enter amount/i).should('be.empty')
   })
 })
