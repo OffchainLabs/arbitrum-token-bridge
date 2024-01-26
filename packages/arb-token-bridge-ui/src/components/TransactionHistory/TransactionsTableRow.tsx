872e9eded6b4ca3d8e94370c4ec6474398a264c0
Bartek <bartlomiejkramek@outlook.com>
2024-01-26 14:26:35 +0100

    fixes

--- packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionsTableRow.tsx
+++ packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionsTableRow.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import dayjs from 'dayjs'
 import {
