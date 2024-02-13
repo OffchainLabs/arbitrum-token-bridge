eef523b565944c03870495aea9706763f4d6327f
Christophe <contact@christophedeveaux.com>
2024-02-13 20:53:38 +0100

    Refactor useTokenLists

--- packages/arb-token-bridge-ui/src/hooks/useTokenLists.ts
+++ packages/arb-token-bridge-ui/src/hooks/useTokenLists.ts
@@ -1,61 +1,32 @@
 import useSWRImmutable from 'swr/immutable'
 import { SWRResponse } from 'swr'
-import {
-  BRIDGE_TOKEN_LISTS,
-  fetchTokenListFromURL,
-  TokenListWithId
-} from '../util/TokenListUtils'
-import { isNetwork } from '../util/networks'
+import { TokenListMap, TokensMap } from '../pages/api/tokenlist'
+import { useMemo } from 'react'
 
-export function fetchTokenLists(
-  forL2ChainId: number
-): Promise<TokenListWithId[]> {
-  return new Promise(resolve => {
-    const { isOrbitChain } = isNetwork(forL2ChainId)
-    const requestListArray = BRIDGE_TOKEN_LISTS.filter(
-      bridgeTokenList =>
-        bridgeTokenList.originChainID === forL2ChainId ||
-        // Always load the Arbitrum Token token list except from or to Orbit chain
-        (bridgeTokenList.isArbitrumTokenTokenList && !isOrbitChain)
-    )
-
-    Promise.all(
-      requestListArray.map(bridgeTokenList =>
-        fetchTokenListFromURL(bridgeTokenList.url)
-      )
-    ).then(responses => {
-      const tokenListsWithBridgeTokenListId = responses
-        .map(({ data, isValid }, index) => {
-          const bridgeTokenListId = requestListArray[index]?.id
-
-          if (typeof bridgeTokenListId === 'undefined') {
-            return { ...data, isValid }
-          }
-
-          return {
-            l2ChainId: forL2ChainId,
-            bridgeTokenListId,
-            isValid,
-            ...data
-          }
-        })
-        .filter(list => list?.isValid)
-
-      resolve(tokenListsWithBridgeTokenListId as TokenListWithId[])
-    })
-  })
-}
-
-export function useTokenLists(
-  forL2ChainId: number
-): SWRResponse<TokenListWithId[]> {
-  return useSWRImmutable(
-    ['useTokenLists', forL2ChainId],
-    ([, _forL2ChainId]) => fetchTokenLists(_forL2ChainId),
+const defaultEmpty = {}
+export function useTokenLists(sourceChainId: number): SWRResponse<TokensMap> {
+  const swrResponse = useSWRImmutable<TokenListMap>(
+    ['useTokenLists'],
+    async () => {
+      const response = await fetch('/api/tokenlist')
+      const data: { data: TokenListMap } = await response.json()
+      // TODO: Handle error
+      return data.data
+    },
     {
       shouldRetryOnError: true,
       errorRetryCount: 2,
       errorRetryInterval: 1_000
     }
   )
+
+  const data = useMemo(() => {
+    if (!swrResponse.data) return defaultEmpty
+    return swrResponse.data[sourceChainId] ?? defaultEmpty
+  }, [swrResponse.data, sourceChainId])
+
+  return {
+    ...swrResponse,
+    data
+  }
 }
