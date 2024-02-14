d2081df99d952443f8c318815c70d7d88e2c2009
Bartek <bartlomiejkramek@outlook.com>
2024-02-14 14:30:00 +0100

    fixes

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TokenInfo.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TokenInfo.tsx
@@ -8,10 +8,20 @@ import { getExplorerUrl } from '../../util/networks'
 import { useNetworks } from '../../hooks/useNetworks'
 import { shortenAddress } from '../../util/CommonUtils'
 
+type TokenInfoOptions = {
+  showTokenLogo?: boolean
+  shortenAddress?: boolean
+}
+
 export const TokenInfo = ({
-  token
+  token,
+  options = {
+    showTokenLogo: true,
+    shortenAddress: true
+  }
 }: {
   token: ERC20BridgeToken | null | undefined
+  options?: TokenInfoOptions
 }) => {
   const [networks] = useNetworks()
   const {
@@ -19,8 +29,12 @@ export const TokenInfo = ({
       arbTokenBridge: { bridgeTokens }
     }
   } = useAppState()
+  const tokenAddressLowercased = token?.address.toLocaleLowerCase()
 
   const tokenLogo = useMemo(() => {
+    if (!options.showTokenLogo) {
+      return undefined
+    }
     if (typeof bridgeTokens === 'undefined') {
       return undefined
     }
@@ -34,32 +48,35 @@ export const TokenInfo = ({
     }
 
     return undefined
-  }, [bridgeTokens, token])
+  }, [bridgeTokens, token, options.showTokenLogo])
 
   return (
     <div className="flex flex-row items-center space-x-3">
-      {tokenLogo ? (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src={tokenLogo} alt="Token logo" className="h-6 w-6" />
-      ) : (
-        <div className="flex h-6 w-6 items-center justify-center rounded-full border border-white/30 bg-gray-dark text-sm font-medium">
-          ?
-        </div>
-      )}
+      {options.showTokenLogo &&
+        (tokenLogo ? (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={tokenLogo} alt="Token logo" className="h-6 w-6" />
+        ) : (
+          <div className="flex h-6 w-6 items-center justify-center rounded-full border border-white/30 bg-gray-dark text-sm font-medium">
+            ?
+          </div>
+        ))}
 
       <div className="flex flex-col">
         <div className="flex items-center space-x-1">
           <span className="text-base">{token?.symbol}</span>
           <span className="text-xs text-white/70">{token?.name}</span>
         </div>
-        {token?.address && (
+        {tokenAddressLowercased && (
           <ExternalLink
-            href={`${getExplorerUrl(networks.sourceChain.id)}/token/${
-              token?.address
-            }`}
+            href={`${getExplorerUrl(
+              networks.sourceChain.id
+            )}/token/${tokenAddressLowercased}`}
             className="arb-hover text-xs underline"
           >
-            {shortenAddress(token.address.toLowerCase())}
+            {options.shortenAddress
+              ? shortenAddress(tokenAddressLowercased)
+              : tokenAddressLowercased}
           </ExternalLink>
         )}
       </div>
