6ab77cf3e5b9ef69ca6d5316d78b3e2228cf97cf
Fionna Chan <fionnachan@users.noreply.github.com>
2024-02-15 14:20:09 +0000

    update projects link

--- packages/arb-token-bridge-ui/src/components/Sidebar/SidebarMenu.tsx
+++ packages/arb-token-bridge-ui/src/components/Sidebar/SidebarMenu.tsx
@@ -201,7 +201,6 @@ export const SidebarMenu = ({
       iconSrc: IconProjects,
       isExternalLink: false,
       isExpandable: true,
-      link: '',
       onClick: () => {
         toggleActiveMenu('projects')
       },
