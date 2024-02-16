b43e440cc3ce0a555640d661258159b689e38ac8
Fionna Chan <fionnachan@users.noreply.github.com>
2024-02-16 13:14:18 +0000

    address review comments

--- packages/arb-token-bridge-ui/src/components/common/NetworkSelectionContainer.tsx
+++ packages/arb-token-bridge-ui/src/components/common/NetworkSelectionContainer.tsx
@@ -335,7 +335,7 @@ export const NetworkSelectionContainer = ({
           </Popover.Button>
 
           <Transition
-            className="fixed left-0 top-0 z-50 lg:absolute lg:left-auto lg:right-0 lg:top-[45px]"
+            className="fixed left-0 top-0 z-50 lg:absolute lg:left-auto lg:right-0 lg:top-[54px]"
             // we don't unmount on leave here because otherwise transition won't work with virtualized lists
             options={{ unmountOnLeave: false }}
           >
