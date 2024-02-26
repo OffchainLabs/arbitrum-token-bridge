f516c68bce1813f2e71753b0f855cf95784d19ed
dewanshparashar <dewanshparashar@yahoo.in>
2024-02-26 18:56:12 +0400

    Dev: remove extra feature

--- packages/arb-token-bridge-ui/tests/support/index.ts
+++ packages/arb-token-bridge-ui/tests/support/index.ts
@@ -15,20 +15,6 @@ Cypress.Keyboard.defaults({
 
 logCollector()
 
-// Hide fetch/XHR requests
-// https://stackoverflow.com/questions/71357705/hide-xhr-calls-on-cypress-test-runner
-const app = window.top
-if (!app?.document.head.querySelector('[data-hide-command-log-request]')) {
-  const style = app?.document.createElement('style')
-  if (style) {
-    style.innerHTML =
-      '.command-name-request, .command-name-xhr { display: none }'
-    style.setAttribute('data-hide-command-log-request', '')
-
-    app?.document.head.appendChild(style)
-  }
-}
-
 before(() => {
   // connect to goerli to avoid connecting to localhost twice and failing
   cy.setupMetamask(Cypress.env('PRIVATE_KEY'), 'goerli')
