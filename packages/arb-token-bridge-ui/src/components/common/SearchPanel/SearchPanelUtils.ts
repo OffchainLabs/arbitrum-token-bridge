658770d6a6a8d14afa543066024bf1fcbb866ede
Fionna Chan <fionnachan@users.noreply.github.com>
2024-03-01 16:47:08 +0000

    fix: sidebar height jump

--- packages/arb-token-bridge-ui/src/components/common/SearchPanel/SearchPanelUtils.ts
+++ packages/arb-token-bridge-ui/src/components/common/SearchPanel/SearchPanelUtils.ts
@@ -2,13 +2,13 @@ import { twMerge } from 'tailwind-merge'
 
 export const panelWrapperClassnames = twMerge(
   'h-screen w-full bg-bg-gray-1 text-white border-gray-dark border w-screen',
-  'lg:h-auto lg:w-auto lg:min-w-[448px] lg:gap-3 lg:rounded lg:shadow-modal'
+  'sm:h-auto sm:w-auto sm:min-w-[448px] sm:gap-3 sm:rounded sm:shadow-modal'
 )
 
 export function onPopoverClose() {
-  document.body.classList.remove('overflow-hidden', 'lg:overflow-auto')
+  document.body.classList.remove('overflow-hidden', 'sm:overflow-visible')
 }
 
 export function onPopoverButtonClick() {
-  document.body.classList.add('overflow-hidden', 'lg:overflow-auto')
+  document.body.classList.add('overflow-hidden', 'sm:overflow-visible')
 }
