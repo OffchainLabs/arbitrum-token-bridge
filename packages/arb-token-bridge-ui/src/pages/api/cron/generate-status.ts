59cd5c243f0c5bbd3d3356060318fbcd3e2c77f6
dewanshparashar <dewanshparashar@yahoo.in>
2024-03-28 16:37:02 +0400

    Dev: experimental caching

--- packages/arb-token-bridge-ui/src/pages/api/cron/generate-status.ts
+++ packages/arb-token-bridge-ui/src/pages/api/cron/generate-status.ts
@@ -1,13 +1,37 @@
-import fs from 'fs'
-import path from 'path'
 import axios from 'axios'
+import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
+
+type ArbitrumStatusResponse = {
+  meta: {
+    timestamp: string
+  }
+  content: {
+    components?: {
+      id: string
+      name: string
+      description: string
+      status:
+        | 'UNDERMAINTENANCE'
+        | 'OPERATIONAL'
+        | 'DEGRADEDPERFORMANCE'
+        | 'PARTIALOUTAGE'
+        | 'MAJOROUTAGE'
+    }[]
+  }
+}
 
 const STATUS_URL = 'https://status.arbitrum.io/v2/components.json'
 
+let cachedResult: ArbitrumStatusResponse | null = null
+const originalTime = dayjs()
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ data: boolean | undefined; message?: string }>
+  res: NextApiResponse<{
+    data: ArbitrumStatusResponse | undefined
+    message?: string
+  }>
 ) {
   // validate method
   if (req.method !== 'GET') {
@@ -17,25 +41,20 @@ export default async function handler(
     return
   }
 
+  const currentTime = dayjs()
+
+  if (cachedResult !== null && currentTime.diff(originalTime, 'minutes') < 5) {
+    res.status(200).json({ data: cachedResult })
+    return
+  }
+
   const _statusSummary = (await axios.get(STATUS_URL)).data
-  const resultJson =
-    JSON.stringify(
-      {
-        meta: {
-          timestamp: new Date().toISOString()
-        },
-        content: _statusSummary
-      },
-      null,
-      2
-    ) + '\n'
-
-  const filePath = path.join(
-    process.cwd(),
-    'public',
-    '__auto-generated-status.json'
-  )
-  fs.writeFileSync(filePath, resultJson)
-
-  res.status(200).json({ data: true })
+  const resultJson = {
+    meta: {
+      timestamp: new Date().toISOString()
+    },
+    content: _statusSummary
+  }
+  cachedResult = resultJson
+  res.status(200).json({ data: resultJson })
 }
