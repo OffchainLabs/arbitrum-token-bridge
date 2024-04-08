bdd89f33feeb1d6c848a418bace8300d4bdc103b
Doug <4741454+douglance@users.noreply.github.com>
2024-04-08 11:29:50 -0400

    revert next.config.js changes

--- packages/arb-token-bridge-ui/next.config.js
+++ packages/arb-token-bridge-ui/next.config.js
@@ -1,7 +1,5 @@
 // @ts-check type next.config.js
 
-const path = require('path')
-
 /**
  * @type {import('next').NextConfig}
  **/
@@ -9,14 +7,5 @@ const path = require('path')
 module.exports = {
   distDir: 'build',
   productionBrowserSourceMaps: true,
-  reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Resolve React to a single version if there's a dependency conflict
-    config.resolve.alias['react'] = path.resolve('../../node_modules/react')
-    config.resolve.alias['react-dom'] = path.resolve(
-      '../../node_modules/react-dom'
-    )
-
-    return config
-  }
+  reactStrictMode: true
 }
