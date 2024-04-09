9fd25050808eeb4bae0446307c09092635fbd616
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-04-09 10:31:02 +0200

    wip

--- packages/arb-token-bridge-ui/src/pages/api/ServerSubgraphUtils.ts
+++ packages/arb-token-bridge-ui/src/pages/api/ServerSubgraphUtils.ts
@@ -12,7 +12,97 @@ const graphNetworkEnabled = process.env.GRAPH_NETWORK_ENABLED === 'true'
  */
 const graphNetworkApiKey = process.env.GRAPH_NETWORK_API_KEY
 
-function createClient(uri: string) {
+type GraphNetworkSubgraphId =
+  | 'E6iPLnDGEgrcc4gu9uiHJxENSRAAzTvUJqQqJcHZqJT1'
+  | '9DgSggKVrvfi4vdyYTdmSBuPgDfm3D7zfLZ1qaQFjYYW'
+  | '4gSU1PTxjYPWk2TXPX2fusjuXrBFHC7kCZrbhrhaF9V5'
+  | '4Dp9ENSFDKfeBsmZeSyATKKrhxC2EKzbC3bZvTHpU1DB'
+  | 'F2N4nGH86Y5Bk2vPo15EVRSTz2wbtz7BGRe8DDJqMPG4'
+  | '6Xvyjk9r91N3DSRQP6UZ1Lkbou567hFxLSWt2Tsv5AWp'
+  | 'GF6Ez7sY2gef8EoXrR76X6iFa41wf38zh4TXZkDkL5Z9'
+  | '9eFk14Tms68qBN7YwL6kFuk9e2BVRqkX6gXyjzLR3tuj'
+  | 'AaUuKWWuQbCXbvRkXpVDEpw9B7oVicYrovNyMLPZtLPw'
+
+type GraphHostedServiceSubgraphName =
+  // CCTP Mainnet Subgraphs
+  | 'chrstph-dvx/cctp-mainnet'
+  | 'chrstph-dvx/cctp-arb-one'
+  // CCTP Testnet Subgraphs
+  | 'chrstph-dvx/cctp-sepolia'
+  | 'chrstph-dvx/cctp-arb-sepolia'
+  // L1 Mainnet Subgraphs
+  | 'gvladika/arb-bridge-eth-nitro'
+  | 'gvladika/arb-bridge-eth-nova'
+  // L1 Testnet Subgraphs
+  | 'fionnachan/arb-bridge-eth-sepolia'
+  // L2 Mainnet Subgraphs
+  | 'gvladika/layer2-token-gateway-arb1'
+  // L2 Testnet Subgraphs
+  | 'fionnachan/layer2-token-gateway-sepolia'
+
+type ReadableSubgraphIdentifier =
+  | 'cctp-mainnet'
+  | 'cctp-arbitrum-one'
+  | 'cctp-sepolia'
+  | 'cctp-arbitrum-sepolia'
+  | 'l1-arbitrum-one'
+  | 'l1-arbitrum-nova'
+  | 'l1-arbitrum-sepolia'
+  | 'l2-arbitrum-one'
+  | 'l2-arbitrum-sepolia'
+
+const subgraphs: Record<
+  ReadableSubgraphIdentifier,
+  {
+    graphNetworkSubgraphId: GraphNetworkSubgraphId
+    graphHostedServiceSubgraphName: GraphHostedServiceSubgraphName
+  }
+> = {
+  // CCTP Mainnet Subgraphs
+  'cctp-mainnet': {
+    graphNetworkSubgraphId: 'E6iPLnDGEgrcc4gu9uiHJxENSRAAzTvUJqQqJcHZqJT1',
+    graphHostedServiceSubgraphName: 'chrstph-dvx/cctp-mainnet'
+  },
+  'cctp-arbitrum-one': {
+    graphNetworkSubgraphId: '9DgSggKVrvfi4vdyYTdmSBuPgDfm3D7zfLZ1qaQFjYYW',
+    graphHostedServiceSubgraphName: 'chrstph-dvx/cctp-arb-one'
+  },
+  // CCTP Testnet Subgraphs
+  'cctp-sepolia': {
+    graphNetworkSubgraphId: '4gSU1PTxjYPWk2TXPX2fusjuXrBFHC7kCZrbhrhaF9V5',
+    graphHostedServiceSubgraphName: 'chrstph-dvx/cctp-sepolia'
+  },
+  'cctp-arbitrum-sepolia': {
+    graphNetworkSubgraphId: '4Dp9ENSFDKfeBsmZeSyATKKrhxC2EKzbC3bZvTHpU1DB',
+    graphHostedServiceSubgraphName: 'chrstph-dvx/cctp-arb-sepolia'
+  },
+  // L1 Mainnet Subgraphs
+  'l1-arbitrum-one': {
+    graphNetworkSubgraphId: 'F2N4nGH86Y5Bk2vPo15EVRSTz2wbtz7BGRe8DDJqMPG4',
+    graphHostedServiceSubgraphName: 'gvladika/arb-bridge-eth-nitro'
+  },
+  'l1-arbitrum-nova': {
+    graphNetworkSubgraphId: '6Xvyjk9r91N3DSRQP6UZ1Lkbou567hFxLSWt2Tsv5AWp',
+    graphHostedServiceSubgraphName: 'gvladika/arb-bridge-eth-nova'
+  },
+  // L1 Testnet Subgraphs
+  'l1-arbitrum-sepolia': {
+    graphNetworkSubgraphId: 'GF6Ez7sY2gef8EoXrR76X6iFa41wf38zh4TXZkDkL5Z9',
+    graphHostedServiceSubgraphName: 'fionnachan/arb-bridge-eth-sepolia'
+  },
+  // L2 Mainnet Subgraphs
+  'l2-arbitrum-one': {
+    graphNetworkSubgraphId: '9eFk14Tms68qBN7YwL6kFuk9e2BVRqkX6gXyjzLR3tuj',
+    graphHostedServiceSubgraphName: 'gvladika/layer2-token-gateway-arb1'
+  },
+  // L2 Testnet Subgraphs
+  'l2-arbitrum-sepolia': {
+    graphNetworkSubgraphId: 'AaUuKWWuQbCXbvRkXpVDEpw9B7oVicYrovNyMLPZtLPw',
+    graphHostedServiceSubgraphName: 'fionnachan/layer2-token-gateway-sepolia'
+  }
+}
+
+function createApolloClient(uri: string) {
   return new ApolloClient({
     link: new HttpLink({ uri, fetch }),
     cache: new InMemoryCache()
@@ -20,112 +110,86 @@ function createClient(uri: string) {
 }
 
 function createGraphNetworkClient(subgraphId: string) {
-  if (typeof graphNetworkApiKey === 'undefined') {
+  if (typeof graphNetworkApiKey === 'undefined' || graphNetworkApiKey === '') {
     throw new Error(
       `[createGraphNetworkClient] missing "GRAPH_NETWORK_API_KEY" env variable"`
     )
   }
 
-  return createClient(
+  return createApolloClient(
     `https://gateway-arbitrum.network.thegraph.com/api/${graphNetworkApiKey}/subgraphs/id/${subgraphId}`
   )
 }
 
 function createGraphHostedServiceClient(subgraphName: string) {
-  return createClient(`https://api.thegraph.com/subgraphs/name/${subgraphName}`)
+  return createApolloClient(
+    `https://api.thegraph.com/subgraphs/name/${subgraphName}`
+  )
 }
 
-// CCTP Subgraphs
+function createSubgraphClient(identifier: ReadableSubgraphIdentifier) {
+  const { graphNetworkSubgraphId, graphHostedServiceSubgraphName } =
+    subgraphs[identifier]
 
-const CctpSubgraphClient = {
-  // mainnet
-  Ethereum: graphNetworkEnabled
-    ? createGraphNetworkClient('E6iPLnDGEgrcc4gu9uiHJxENSRAAzTvUJqQqJcHZqJT1')
-    : createGraphHostedServiceClient('chrstph-dvx/cctp-mainnet'),
-  ArbitrumOne: graphNetworkEnabled
-    ? createGraphNetworkClient('9DgSggKVrvfi4vdyYTdmSBuPgDfm3D7zfLZ1qaQFjYYW')
-    : createGraphHostedServiceClient('chrstph-dvx/cctp-arb-one'),
-  // testnet
-  Sepolia: graphNetworkEnabled
-    ? createGraphNetworkClient('4gSU1PTxjYPWk2TXPX2fusjuXrBFHC7kCZrbhrhaF9V5')
-    : createGraphHostedServiceClient('chrstph-dvx/cctp-sepolia'),
-  ArbitrumSepolia: graphNetworkEnabled
-    ? createGraphNetworkClient('4Dp9ENSFDKfeBsmZeSyATKKrhxC2EKzbC3bZvTHpU1DB')
-    : createGraphHostedServiceClient('chrstph-dvx/cctp-arb-sepolia')
+  if (!graphNetworkEnabled) {
+    return createGraphHostedServiceClient(graphHostedServiceSubgraphName)
+  }
+
+  try {
+    return createGraphNetworkClient(graphNetworkSubgraphId)
+  } catch (err) {
+    console.warn(
+      `[createSubgraphClient] failed to create client for subgraph "${graphNetworkSubgraphId}" on the graph network`
+    )
+    console.warn(
+      `[createSubgraphClient] falling back to subgraph "${graphHostedServiceSubgraphName}" on the graph hosted service`
+    )
+    return createGraphHostedServiceClient(graphNetworkSubgraphId)
+  }
 }
 
 export function getCctpSubgraphClient(chainId: number) {
   switch (chainId) {
     case ChainId.Ethereum:
-      return CctpSubgraphClient.Ethereum
+      return createSubgraphClient('cctp-mainnet')
 
     case ChainId.ArbitrumOne:
-      return CctpSubgraphClient.ArbitrumOne
+      return createSubgraphClient('cctp-arbitrum-one')
 
     case ChainId.Sepolia:
-      return CctpSubgraphClient.Sepolia
+      return createSubgraphClient('cctp-sepolia')
 
     case ChainId.ArbitrumSepolia:
-      return CctpSubgraphClient.ArbitrumSepolia
+      return createSubgraphClient('cctp-arbitrum-sepolia')
 
     default:
       throw new Error(`[getCctpSubgraphClient] unsupported chain: ${chainId}`)
   }
 }
 
-// L1 Subgraphs
-
-const L1SubgraphClient = {
-  // mainnet
-  ArbitrumOne: graphNetworkEnabled
-    ? createGraphNetworkClient('F2N4nGH86Y5Bk2vPo15EVRSTz2wbtz7BGRe8DDJqMPG4')
-    : createGraphHostedServiceClient('gvladika/arb-bridge-eth-nitro'),
-  ArbitrumNova: graphNetworkEnabled
-    ? createGraphNetworkClient('6Xvyjk9r91N3DSRQP6UZ1Lkbou567hFxLSWt2Tsv5AWp')
-    : createGraphHostedServiceClient('gvladika/arb-bridge-eth-nova'),
-  // testnet
-  ArbitrumSepolia: graphNetworkEnabled
-    ? createGraphNetworkClient('GF6Ez7sY2gef8EoXrR76X6iFa41wf38zh4TXZkDkL5Z9')
-    : createGraphHostedServiceClient('fionnachan/arb-bridge-eth-sepolia')
-}
-
 export function getL1SubgraphClient(l2ChainId: number) {
   switch (l2ChainId) {
     case ChainId.ArbitrumOne:
-      return L1SubgraphClient.ArbitrumOne
+      return createSubgraphClient('l1-arbitrum-one')
 
     case ChainId.ArbitrumNova:
-      return L1SubgraphClient.ArbitrumNova
+      return createSubgraphClient('l1-arbitrum-nova')
 
     case ChainId.ArbitrumSepolia:
-      return L1SubgraphClient.ArbitrumSepolia
+      return createSubgraphClient('l1-arbitrum-sepolia')
 
     default:
       throw new Error(`[getL1SubgraphClient] unsupported chain: ${l2ChainId}`)
   }
 }
 
-// L2 Subgraphs
-
-const L2SubgraphClient = {
-  // mainnet
-  // note that arbitrum nova is not supported
-  ArbitrumOne: graphNetworkEnabled
-    ? createGraphNetworkClient('9eFk14Tms68qBN7YwL6kFuk9e2BVRqkX6gXyjzLR3tuj')
-    : createGraphHostedServiceClient('gvladika/layer2-token-gateway-arb1'),
-  // testnet
-  ArbitrumSepolia: graphNetworkEnabled
-    ? createGraphNetworkClient('AaUuKWWuQbCXbvRkXpVDEpw9B7oVicYrovNyMLPZtLPw')
-    : createGraphHostedServiceClient('fionnachan/layer2-token-gateway-sepolia')
-}
-
 export function getL2SubgraphClient(l2ChainId: number) {
   switch (l2ChainId) {
     case ChainId.ArbitrumOne:
-      return L2SubgraphClient.ArbitrumOne
+      return createSubgraphClient('l2-arbitrum-one')
 
     case ChainId.ArbitrumSepolia:
-      return L2SubgraphClient.ArbitrumSepolia
+      return createSubgraphClient('l2-arbitrum-sepolia')
 
     default:
       throw new Error(`[getL2SubgraphClient] unsupported chain: ${l2ChainId}`)
