cea6fea26e4e30d5a221ffbc4a96d98ebb619e82
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-04-24 16:34:42 +0200

    use ids everywhere

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanel.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanel.tsx
@@ -671,6 +671,7 @@ export function TransferPanel() {
       }
 
       const sourceChainId = latestNetworks.current.sourceChain.id
+      const destinationChainId = latestNetworks.current.destinationChain.id
       const connectedChainId = networks.sourceChain.id
       const sourceChainEqualsConnectedChain = sourceChainId === connectedChainId
 
@@ -697,9 +698,9 @@ export function TransferPanel() {
       const signer = isDepositMode ? l1Signer : l2Signer
 
       const bridgeTransferStarter = await BridgeTransferStarterFactory.create({
-        sourceChainProvider,
-        destinationChainProvider,
-        sourceChainErc20Address
+        sourceChainId,
+        sourceChainErc20Address,
+        destinationChainId
       })
 
       const { isNativeCurrencyTransfer, isWithdrawal } =
@@ -712,7 +713,7 @@ export function TransferPanel() {
       if (!signer) throw Error('Signer not connected!')
 
       if (isWithdrawal && selectedToken && !sourceChainErc20Address) {
-        /* 
+        /*
         just a fail-safe - since our types allow for an optional `selectedToken?.l2Address`, we can theoretically end up with a case
         where user is trying to make an ERC-20 withdrawal but passing `sourceChainErc20Address` as undefined, ending up with
         the SDK to initialize wrongly and make an ETH withdrawal instead. To summarize:
