691e9ba78bf0dad16abb0d44e84799c8b2ec67cf
dewanshparashar <dewanshparashar@yahoo.in>
2024-05-08 17:57:31 +0400

    Dev: teleporter disabled content

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TransferDisabledDialog.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferDisabledDialog.tsx
@@ -4,6 +4,8 @@ import { useActions, useAppState } from '../../state'
 import { Dialog } from '../common/Dialog'
 import { sanitizeTokenSymbol } from '../../util/TokenUtils'
 import { useNetworks } from '../../hooks/useNetworks'
+import { useTeleportMode } from '../../hooks/useTeleportMode'
+import { ExternalLink } from '../common/ExternalLink'
 
 type TransferDisabledDialogStore = {
   isOpen: boolean
@@ -34,6 +36,11 @@ export function TransferDisabledDialog() {
     chainId: networks.sourceChain.id
   })
 
+  const isTeleportMode = useTeleportMode({
+    sourceChainId: networks.sourceChain.id,
+    destinationChainId: networks.destinationChain.id
+  })
+
   const onClose = () => {
     setSelectedToken(null)
     closeTransferDisabledDialog()
@@ -49,16 +56,41 @@ export function TransferDisabledDialog() {
       onClose={onClose}
     >
       <div className="flex flex-col space-y-4 py-4">
-        <p>
-          Unfortunately, <span className="font-medium">{unsupportedToken}</span>{' '}
-          has a custom bridge solution that is incompatible with the canonical
-          Arbitrum bridge.
-        </p>
-        <p>
-          For more information please contact{' '}
-          <span className="font-medium">{unsupportedToken}</span>
-          &apos;s developer team directly or explore their docs.
-        </p>
+        {/* teleport transfer disabled content if token is not in the allowlist */}
+        {isTeleportMode ? (
+          <>
+            <p>
+              Unfortunately,{' '}
+              <span className="font-medium">{unsupportedToken}</span> is not
+              supported for LayerLeap transfers yet.
+            </p>
+            <p>
+              For more information please contact{' '}
+              <ExternalLink
+                href="https://discord.com/invite/ZpZuw7p"
+                className="underline"
+              >
+                Discord
+              </ExternalLink>{' '}
+              and reach out in #support for assistance..
+            </p>
+          </>
+        ) : (
+          // canonical transfer disabled content for all other cases
+          <>
+            <p>
+              Unfortunately,{' '}
+              <span className="font-medium">{unsupportedToken}</span> has a
+              custom bridge solution that is incompatible with the canonical
+              Arbitrum bridge.
+            </p>
+            <p>
+              For more information please contact{' '}
+              <span className="font-medium">{unsupportedToken}</span>
+              &apos;s developer team directly or explore their docs.
+            </p>
+          </>
+        )}
       </div>
     </Dialog>
   )
