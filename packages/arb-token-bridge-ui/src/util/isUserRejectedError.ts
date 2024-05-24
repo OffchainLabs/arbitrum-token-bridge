4d44c8adf0c8b1137b2b9af4c92531f8399646c2
Christophe <contact@christophedeveaux.com>
2024-05-24 12:16:54 +0000

    Fix: Ignore walletConnect user rejected error

--- packages/arb-token-bridge-ui/src/util/isUserRejectedError.ts
+++ packages/arb-token-bridge-ui/src/util/isUserRejectedError.ts
@@ -1,4 +1,8 @@
 function isUserRejectedError(error: any) {
-  return error?.code === 4001 || error?.code === 'ACTION_REJECTED'
+  return (
+    error?.code === 4001 ||
+    error?.code === 'ACTION_REJECTED' ||
+    /user rejected/i.test(error?.message) // WalletConnect
+  )
 }
 export { isUserRejectedError }
