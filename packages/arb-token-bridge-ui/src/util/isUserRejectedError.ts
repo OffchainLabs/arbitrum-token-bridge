0dda2b749f9422967b9e7bbbb335200e17485089
Christophe <contact@christophedeveaux.com>
2024-05-24 15:33:14 +0000

    Add user rejected error detection for Zerion

--- packages/arb-token-bridge-ui/src/util/isUserRejectedError.ts
+++ packages/arb-token-bridge-ui/src/util/isUserRejectedError.ts
@@ -4,7 +4,8 @@ function isUserRejectedError(error: any) {
   return (
     error?.code === 4001 ||
     error?.code === 'ACTION_REJECTED' ||
-    error instanceof UserRejectedRequestError
+    error instanceof UserRejectedRequestError ||
+    error.message.match(/User Cancelled/)
   )
 }
 export { isUserRejectedError }
