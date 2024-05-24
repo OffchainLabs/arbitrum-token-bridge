ed1e9a1c981065b715fbcae383a96b134bb07198
Christophe <contact@christophedeveaux.com>
2024-05-24 15:20:32 +0000

    Improve user rejected error detection

--- packages/arb-token-bridge-ui/src/util/isUserRejectedError.ts
+++ packages/arb-token-bridge-ui/src/util/isUserRejectedError.ts
@@ -1,8 +1,10 @@
+import { UserRejectedRequestError } from 'wagmi'
+
 function isUserRejectedError(error: any) {
   return (
     error?.code === 4001 ||
     error?.code === 'ACTION_REJECTED' ||
-    /user rejected/i.test(error?.message) // WalletConnect
+    error instanceof UserRejectedRequestError
   )
 }
 export { isUserRejectedError }
