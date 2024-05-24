418762f077c7c2c90645b96dd46c8103219c3aee
Bartek <bartlomiejkramek@outlook.com>
2024-05-24 13:12:55 +0200

    better error

--- packages/arb-token-bridge-ui/scripts/validateOrbitChains.ts
+++ packages/arb-token-bridge-ui/scripts/validateOrbitChains.ts
@@ -7,66 +7,72 @@ const zAddress = z
   .string()
   .refine(address => isAddress(address), 'Invalid address')
 
-const zIsTrue = z.boolean({ coerce: true })
+const zIsTrue = z.boolean().refine(bool => bool === true)
 
 function validateOrbitChain(chain: OrbitChainConfig) {
-  z.object({
-    chainID: z.number().nonnegative().int(),
-    confirmPeriodBlocks: z.number().nonnegative().int(),
-    ethBridge: z.object({
-      bridge: zAddress,
-      inbox: zAddress,
-      outbox: zAddress,
-      rollup: zAddress,
-      sequencerInbox: zAddress
-    }),
-    nativeToken: zAddress.optional(),
-    explorerUrl: z.string(),
-    rpcUrl: z.string(),
-    isArbitrum: zIsTrue,
-    isCustom: zIsTrue,
-    name: z.string(),
-    slug: z.string(),
-    partnerChainID: z.number().nonnegative().int(),
-    partnerChainIDs: z.array(z.number()),
-    retryableLifetimeSeconds: z.number().nonnegative().int(),
-    blockTime: z.number().nonnegative(),
-    tokenBridge: z.object({
-      l1CustomGateway: zAddress,
-      l1ERC20Gateway: zAddress,
-      l1GatewayRouter: zAddress,
-      l1MultiCall: zAddress,
-      l1ProxyAdmin: zAddress,
-      l1Weth: zAddress,
-      l1WethGateway: zAddress,
-      l2CustomGateway: zAddress,
-      l2ERC20Gateway: zAddress,
-      l2GatewayRouter: zAddress,
-      l2Multicall: zAddress,
-      l2ProxyAdmin: zAddress,
-      l2Weth: zAddress,
-      l2WethGateway: zAddress
-    }),
-    nitroGenesisBlock: z.number().nonnegative().int(),
-    nitroGenesisL1Block: z.number().nonnegative().int(),
-    depositTimeout: z.number().nonnegative().int(),
-    bridgeUiConfig: z.object({
-      color: z.string(),
-      network: z.object({
-        name: z.string(),
-        logo: z.string(),
-        description: z.string()
+  try {
+    z.object({
+      chainID: z.number().nonnegative().int(),
+      confirmPeriodBlocks: z.number().nonnegative().int(),
+      ethBridge: z.object({
+        bridge: zAddress,
+        inbox: zAddress,
+        outbox: zAddress,
+        rollup: zAddress,
+        sequencerInbox: zAddress
       }),
-      nativeTokenData: z
-        .object({
+      nativeToken: zAddress.optional(),
+      explorerUrl: z.string(),
+      rpcUrl: z.string(),
+      isArbitrum: zIsTrue,
+      isCustom: zIsTrue,
+      name: z.string(),
+      slug: z.string(),
+      partnerChainID: z.number().nonnegative().int(),
+      partnerChainIDs: z.array(z.number()),
+      retryableLifetimeSeconds: z.number().nonnegative().int(),
+      blockTime: z.number().nonnegative(),
+      tokenBridge: z.object({
+        l1CustomGateway: zAddress,
+        l1ERC20Gateway: zAddress,
+        l1GatewayRouter: zAddress,
+        l1MultiCall: zAddress,
+        l1ProxyAdmin: zAddress,
+        l1Weth: zAddress,
+        l1WethGateway: zAddress,
+        l2CustomGateway: zAddress,
+        l2ERC20Gateway: zAddress,
+        l2GatewayRouter: zAddress,
+        l2Multicall: zAddress,
+        l2ProxyAdmin: zAddress,
+        l2Weth: zAddress,
+        l2WethGateway: zAddress
+      }),
+      nitroGenesisBlock: z.number().nonnegative().int(),
+      nitroGenesisL1Block: z.number().nonnegative().int(),
+      depositTimeout: z.number().nonnegative().int(),
+      bridgeUiConfig: z.object({
+        color: z.string(),
+        network: z.object({
           name: z.string(),
-          symbol: z.string(),
-          decimals: z.number().nonnegative().int(),
-          logoUrl: z.string()
-        })
-        .optional()
-    })
-  }).parse(chain)
+          logo: z.string(),
+          description: z.string()
+        }),
+        nativeTokenData: z
+          .object({
+            name: z.string(),
+            symbol: z.string(),
+            decimals: z.number().nonnegative().int(),
+            logoUrl: z.string()
+          })
+          .optional()
+      })
+    }).parse(chain)
+  } catch (e) {
+    console.error(
+      `Error while validating Orbit chain: ${chain.name} (${chain.chainID}): ${e}`
+    )
+  }
 }
 
 function main() {
