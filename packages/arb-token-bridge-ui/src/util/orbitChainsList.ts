187f61384248d32d278e858252931cfcaa048445
Dewansh <dparashar@offchainlabs.com>
2024-06-25 18:17:27 +0400

    ci: monitor nova retryables (#1700)

--- packages/arb-token-bridge-ui/src/util/orbitChainsList.ts
+++ packages/arb-token-bridge-ui/src/util/orbitChainsList.ts
@@ -20,7 +20,9 @@ export type BridgeUiConfig = {
   nativeTokenData?: NativeCurrencyBase
 }
 
-type OrbitChainConfig = ChainWithRpcUrl & { bridgeUiConfig: BridgeUiConfig }
+export type OrbitChainConfig = ChainWithRpcUrl & {
+  bridgeUiConfig: BridgeUiConfig
+}
 
 export const orbitMainnets: {
   [key: number]: OrbitChainConfig
