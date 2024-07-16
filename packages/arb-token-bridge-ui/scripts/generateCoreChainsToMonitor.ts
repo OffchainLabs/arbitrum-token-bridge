ff102e826768a76cd509392fc7a6d29366265244
spsjvc <git@spsjvc.com>
2024-07-16 18:43:30 +0200

    refactor: use latest retryable monitoring

--- packages/arb-token-bridge-ui/scripts/generateCoreChainsToMonitor.ts
+++ packages/arb-token-bridge-ui/scripts/generateCoreChainsToMonitor.ts
@@ -1,17 +1,17 @@
 import fs from 'fs'
 import 'dotenv/config'
-import { getL2Network } from '@arbitrum/sdk'
+import { getArbitrumNetwork } from '@arbitrum/sdk'
 import { ChainId, rpcURLs } from '../src/util/networks'
 import { getChainToMonitor } from './utils'
 
 async function generateCoreChainsToMonitor() {
-  const novaChain = await getL2Network(ChainId.ArbitrumNova)
+  const novaChain = getArbitrumNetwork(ChainId.ArbitrumNova)
 
   // make the chain data compatible with that required by the retryable-monitoring script
   const coreChainsToMonitor = [novaChain].map(coreChain =>
     getChainToMonitor({
       chain: coreChain,
-      rpcUrl: process.env.NOVA_MONITOR_RPC_URL ?? rpcURLs[coreChain.chainID]
+      rpcUrl: process.env.NOVA_MONITOR_RPC_URL ?? rpcURLs[coreChain.chainId]
     })
   )
 
--- packages/arb-token-bridge-ui/scripts/generateOrbitChainsToMonitor.ts
+++ packages/arb-token-bridge-ui/scripts/generateOrbitChainsToMonitor.ts
@@ -3,7 +3,7 @@ import { getOrbitChains } from '../src/util/orbitChainsList'
 import { getChainToMonitor } from './utils'
 
 async function generateOrbitChainsToMonitor() {
-  const orbitChains = await getOrbitChains({ mainnet: true, testnet: false })
+  const orbitChains = getOrbitChains({ mainnet: true, testnet: false })
 
   // make the orbit chain data compatible with the orbit-data required by the retryable-monitoring script
   const orbitChainsToMonitor = orbitChains.map(orbitChain => {
