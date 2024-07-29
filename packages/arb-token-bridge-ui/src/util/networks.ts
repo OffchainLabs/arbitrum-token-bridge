fdadc6ff68a77bd1281b90c540452126461a1a32
dewanshparashar <dewanshparashar@yahoo.in>
2024-07-29 15:32:59 +0400

    Check assertion activity on L3

--- packages/arb-token-bridge-ui/src/util/networks.ts
+++ packages/arb-token-bridge-ui/src/util/networks.ts
@@ -270,7 +270,6 @@ const defaultL1Network: L1Network = {
   chainId: 1337
 }
 
-console.log('XXXXXXXX', process.env.NEXT_PUBLIC_IS_E2E_TEST)
 export const defaultL2Network: ArbitrumNetwork = {
   chainId: 412346,
   parentChainId: ChainId.Local,
@@ -313,7 +312,9 @@ export const defaultL3Network: ArbitrumNetwork = {
     bridge: '0xA584795e24628D9c067A6480b033C9E96281fcA3',
     inbox: '0xDcA690902d3154886Ec259308258D10EA5450996',
     outbox: '0xda243bD61B011024FC923164db75Dde198AC6175',
-    rollup: '0xf9B0F86aCc3e42B7DF373c9a8adb2803BF0a7662',
+    rollup: process.env.NEXT_PUBLIC_IS_E2E_TEST
+      ? '0xdeD540257498027B1De7DFD4fe6cc4CeC030F355'
+      : '0xf9B0F86aCc3e42B7DF373c9a8adb2803BF0a7662',
     sequencerInbox: '0x16c54EE2015CD824415c2077F4103f444E00A8cb'
   },
   isCustom: true,
