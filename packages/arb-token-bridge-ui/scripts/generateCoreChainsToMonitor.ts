cb7943a975d25b2a1c4e279f040853f7c4a6a2d2
dewanshparashar <dewanshparashar@yahoo.in>
2024-07-30 14:59:22 +0400

    Dev: conditionally monitor arbone too

--- packages/arb-token-bridge-ui/scripts/generateCoreChainsToMonitor.ts
+++ packages/arb-token-bridge-ui/scripts/generateCoreChainsToMonitor.ts
@@ -6,16 +6,20 @@ import { getChainToMonitor } from './utils'
 
 async function generateCoreChainsToMonitor() {
   const novaChain = getArbitrumNetwork(ChainId.ArbitrumNova)
+  const arbOneChain = getArbitrumNetwork(ChainId.ArbitrumOne)
 
-  // make the chain data compatible with that required by the retryable-monitoring script
-  const coreChainsToMonitor = [novaChain].map(coreChain =>
-    getChainToMonitor({
-      chain: coreChain,
-      rpcUrl: process.env.NOVA_MONITOR_RPC_URL ?? rpcURLs[coreChain.chainId]
-    })
-  )
+  // make the chain data compatible with that required by the monitoring script
+  const coreChainsToMonitor = (
+    process.env.BATCH_POSTER_MONITORING ? [arbOneChain, novaChain] : [novaChain]
+  ) // don't monitor ArbitrumOne for retryable-monitoring
+    .map(coreChain =>
+      getChainToMonitor({
+        chain: coreChain,
+        rpcUrl: process.env.NOVA_MONITOR_RPC_URL ?? rpcURLs[coreChain.chainId]
+      })
+    )
 
-  // write to orbit-chains.json, we will use this json as an input to the retryable-monitoring script
+  // write to orbit-chains.json, we will use this json as an input to the monitoring script
   const resultsJson = JSON.stringify(
     {
       childChains: coreChainsToMonitor
