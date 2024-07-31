2dea65b18b2193353c979bc38910a72287603d1a
Christophe <contact@christophedeveaux.com>
2024-07-31 12:07:27 +0000

    Fix regex

--- packages/arb-token-bridge-ui/tests/support/commands.ts
+++ packages/arb-token-bridge-ui/tests/support/commands.ts
@@ -352,7 +352,7 @@ export function findTransactionInTransactionHistory({
   amount: number
   duration?: string
 }) {
-  const rowId = `claimable-row-*-${amount}${symbol}`
+  const rowId = `claimable-row-[0-9xabcdef]-${amount}${symbol}`
   cy.findByTestId(rowId).as('row')
   if (duration) {
     cy.get('@row').findAllByText(duration).first().should('be.visible')
