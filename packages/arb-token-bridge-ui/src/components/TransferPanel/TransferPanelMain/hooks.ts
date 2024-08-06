c70da1082cdaa50f2326f1e92e7371584b37575f
Bartek <bartlomiejkramek@outlook.com>
2024-08-06 19:08:39 +0200

    fix

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMain/hooks.ts
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMain/hooks.ts
@@ -53,7 +53,6 @@ export function useUpdateUSDCTokenData() {
     isDestinationChainArbitrumOne,
     isDestinationChainArbitrumSepolia,
     selectedToken,
-    setSelectedToken,
     token
   ])
 }
