a09a468fb20685d41d614e8ea1cc7e14824cc956
Bartek <bartlomiejkramek@outlook.com>
2024-08-22 19:24:08 +0200

    feat: transaction history for batch transfers (#1851)
    Co-authored-by: Fionna Chan <13184582+fionnachan@users.noreply.github.com>

--- packages/arb-token-bridge-ui/src/state/app/state.ts
+++ packages/arb-token-bridge-ui/src/state/app/state.ts
@@ -53,6 +53,7 @@ export interface MergedTransaction {
   asset: string
   assetType: AssetType
   value: string | null
+  value2?: string
   uniqueId: BigNumber | null
   isWithdrawal: boolean
   blockNum: number | null
