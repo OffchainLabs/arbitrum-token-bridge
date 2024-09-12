a84b47ae969fc4599ea3b870002f778f965918d9
Christophe <contact@christophedeveaux.com>
2024-09-12 18:22:20 +0000

    test: Run cctp tests on approval

--- .github/workflows/formatSpecfiles.js
+++ .github/workflows/formatSpecfiles.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
-const specFiles = require('../../packages/arb-token-bridge-ui/tests/e2e/specfiles.json')
-const cctpFiles = require('../../packages/arb-token-bridge-ui/tests/e2e/cctp.json')
+const specFiles = require("../../packages/arb-token-bridge-ui/tests/e2e/specfiles.json");
+const cctpFiles = require("../../packages/arb-token-bridge-ui/tests/e2e/cctp.json");
 
 // For each test in specFiles, add an orbit test
-const tests = []
-specFiles.forEach(spec => {
-    tests.push({
-        ...spec,
-        type: 'regular',
-        orbitTest: '0',
-    })
-    tests.push({
-        ...spec,
-        type: 'regular',
-        orbitTest: '1',
-    })
-})
+const tests = [];
+specFiles.forEach((spec) => {
+  tests.push({
+    ...spec,
+    type: "regular",
+    orbitTest: "0",
+  });
+  tests.push({
+    ...spec,
+    type: "regular",
+    orbitTest: "1",
+  });
+});
 
-cctpFiles.forEach(spec => {
+const includeCctp = Boolean(process.argv[2] === "includeCctp" || false);
+if (includeCctp) {
+  cctpFiles.forEach((spec) => {
     tests.push({
-        ...spec,
-        type: 'cctp',
-        orbitTest: null,
-    })
-})
+      ...spec,
+      type: "cctp",
+      orbitTest: null,
+    });
+  });
+}
 
-console.log(JSON.stringify(tests))
+console.log(JSON.stringify(tests));
