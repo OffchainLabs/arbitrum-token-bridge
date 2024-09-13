33052cdcca4774937001f46f810d0aee7203dc6a
Christophe <contact@christophedeveaux.com>
2024-09-13 16:32:33 +0000

    Extract CCTP workflow

--- .github/workflows/formatSpecfiles.js
+++ .github/workflows/formatSpecfiles.js
@@ -2,30 +2,32 @@
 const specFiles = require("../../packages/arb-token-bridge-ui/tests/e2e/specfiles.json");
 const cctpFiles = require("../../packages/arb-token-bridge-ui/tests/e2e/cctp.json");
 
-// For each test in specFiles, add an orbit test
 const tests = [];
-specFiles.forEach((spec) => {
-  tests.push({
-    ...spec,
-    type: "regular",
-    orbitTest: "0",
-  });
-  tests.push({
-    ...spec,
-    type: "regular",
-    orbitTest: "1",
-  });
-});
 
-const includeCctp = Boolean(process.argv[2] === "includeCctp" || false);
-if (includeCctp) {
-  cctpFiles.forEach((spec) => {
-    tests.push({
-      ...spec,
-      type: "cctp",
-      orbitTest: null,
+const testType = process.argv[2];
+switch (testType) {
+  case "regular": {
+    specFiles.forEach((spec) => {
+      tests.push({
+        ...spec,
+        type: "regular",
+      });
+      tests.push({
+        ...spec,
+        type: "orbit",
+      });
     });
-  });
+    break;
+  }
+  case "cctp": {
+    cctpFiles.forEach((spec) => {
+      tests.push({
+        ...spec,
+        type: 'cctp',
+      })
+    })
+    break;
+  }
 }
 
 console.log(JSON.stringify(tests));
