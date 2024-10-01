733484eb77ea78782c1877cf5f7874017b16ee3b
Bartek <bartlomiejkramek@outlook.com>
2024-10-01 18:10:23 +0200

    update

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMain.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanelMain.tsx
@@ -27,6 +27,7 @@ import { useUpdateUSDCTokenData } from './TransferPanelMain/hooks'
 import { useBalances } from '../../hooks/useBalances'
 import { DestinationNetworkBox } from './TransferPanelMain/DestinationNetworkBox'
 import { SourceNetworkBox } from './TransferPanelMain/SourceNetworkBox'
+import { isExperimentalFeatureEnabled } from '../../util'
 
 export function SwitchNetworksButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
@@ -287,6 +288,11 @@ export function TransferPanelMain() {
       (isTokenSepoliaUSDC(selectedToken?.address) && isArbitrumSepolia))
 
   useEffect(() => {
+    if (isExperimentalFeatureEnabled('eth-custom-dest')) {
+      // do not reset destination address for this feature
+      // this will also be the default behavior when the feature is live - which means we will remove this hook
+      return
+    }
     // Different destination address only allowed for tokens
     if (!selectedToken) {
       setDestinationAddress(undefined)
