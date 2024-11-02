4ea0b58e88eef756de8ce47fabd78c890092e8a6
Doug <4741454+douglance@users.noreply.github.com>
2024-11-02 12:09:11 -0400

    update main

--- packages/scripts/src/addOrbitChain/index.ts
+++ packages/scripts/src/addOrbitChain/index.ts
@@ -6,9 +6,6 @@ import {
   handleImages,
   createAndValidateOrbitChain,
   updateAndValidateOrbitChainsList,
-  commitChangesAndCreatePR,
-  setOutputs,
-  runPrettier,
 } from "./transforms";
 
 /**
@@ -32,21 +29,7 @@ export async function addOrbitChain(targetJsonPath: string): Promise<void> {
       nativeTokenLogoPath
     );
 
-    const updatedOrbitChainsList = await updateAndValidateOrbitChainsList(
-      orbitChain,
-      targetJsonPath
-    );
-
-    await runPrettier(targetJsonPath);
-
-    await commitChangesAndCreatePR(
-      branchName,
-      targetJsonPath,
-      updatedOrbitChainsList,
-      orbitChain
-    );
-
-    setOutputs(branchName, orbitChain, targetJsonPath);
+    await updateAndValidateOrbitChainsList(orbitChain, targetJsonPath);
   } catch (error) {
     core.setFailed(`Error in addOrbitChain: ${error}`);
     throw error;
