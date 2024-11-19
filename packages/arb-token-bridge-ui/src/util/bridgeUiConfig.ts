731dc46c3cbd6d2a3c89a4d10a1d5d4b0b67be1c
Fionna <13184582+fionnachan@users.noreply.github.com>
2024-11-19 17:50:58 +0000

    update xmtp sepolia

--- packages/arb-token-bridge-ui/src/util/bridgeUiConfig.ts
+++ packages/arb-token-bridge-ui/src/util/bridgeUiConfig.ts
@@ -129,9 +129,13 @@ export function getBridgeUiConfigForChain(chainId: number): BridgeUiConfig {
 
       if (orbitChain) {
         const bridgeUiConfig = orbitChain.bridgeUiConfig
-        if (orbitChain.bridgeUiConfig.nativeTokenData) {
+        if (
+          orbitChain.bridgeUiConfig.nativeTokenData &&
+          orbitChain.bridgeUiConfig.nativeTokenData.decimals !== 18
+        ) {
           // we can do this because the native token's number of decimals is always 18 on its Orbit chain
           // and this number of decimals's main use is for the wallet to add the Orbit chain
+          //
           // in other cases when we need to know the number of decimals at the Parent chain,
           // we always call `useNativeCurrency` to fetch it using the parent chain provider
           // with the token address
