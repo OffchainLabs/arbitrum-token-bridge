26646377aec04580d683537382d6ce87d6ef0384
Fionna <13184582+fionnachan@users.noreply.github.com>
2024-11-19 18:55:07 +0000

    ux update

--- packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionHistorySearchBar.tsx
+++ packages/arb-token-bridge-ui/src/components/TransactionHistory/TransactionHistorySearchBar.tsx
@@ -3,9 +3,11 @@ import { isAddress } from 'ethers/lib/utils.js'
 import { Address, useAccount, useNetwork } from 'wagmi'
 
 import { Button } from '../common/Button'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { ExternalLink } from '../common/ExternalLink'
 import { getExplorerUrl } from '../../util/networks'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import { twMerge } from 'tailwind-merge'
 
 type TransactionHistoryAddressStore = {
   address: string
@@ -45,6 +47,7 @@ export const useTransactionHistoryAddressStore =
 export function TransactionHistorySearchBar() {
   const { address, setAddress, setSanitizedAddress, sanitizedAddress } =
     useTransactionHistoryAddressStore()
+  const [showAddressField, setShowAddressField] = useState(false)
   const { address: connectedAddress } = useAccount()
   const { chain } = useNetwork()
 
@@ -56,21 +59,6 @@ export function TransactionHistorySearchBar() {
 
   return (
     <div className="mb-4 flex flex-col gap-2">
-      <form className="flex flex-col overflow-hidden rounded border border-gray-dark">
-        <input
-          type="text"
-          value={address}
-          onChange={event => setAddress(event.target.value)}
-          inputMode="search"
-          placeholder="0xWalletAddress"
-          aria-label="Transaction history wallet address input"
-          className="w-full bg-white px-3 py-1 text-sm font-light placeholder:text-gray-400"
-          // stop password managers from autofilling
-          data-1p-ignore
-          data-lpignore="true"
-          data-form-type="other"
-        />
-      </form>
       <span className="text-sm text-white">
         Showing transactions for{' '}
         {chain ? (
@@ -84,6 +72,43 @@ export function TransactionHistorySearchBar() {
           sanitizedAddress
         )}
       </span>
+      <div className="flex flex-row items-center">
+        <Button
+          variant="secondary"
+          className={twMerge(
+            'h-[32px] w-min items-center px-3',
+            'no-select [&>div>span]:flex [&>div>span]:w-max [&>div>span]:flex-row [&>div>span]:items-center [&>div>span]:justify-center',
+            showAddressField ? 'hidden' : 'flex'
+          )}
+          truncate={false}
+          onClick={() => setShowAddressField(true)}
+        >
+          <MagnifyingGlassIcon className="mr-1 h-3 w-3" />
+          <span>Check another address</span>
+        </Button>
+        <form
+          className={twMerge(
+            'items-center justify-center overflow-hidden rounded border border-gray-dark bg-white/[0.15] text-white md:w-1/2',
+            showAddressField ? 'flex' : 'hidden'
+          )}
+          onSubmit={event => event.preventDefault()}
+        >
+          <MagnifyingGlassIcon className="ml-3 mr-1 h-3 w-3" />
+          <input
+            type="text"
+            value={address}
+            onChange={event => setAddress(event.target.value)}
+            inputMode="search"
+            placeholder="0xWalletAddress"
+            aria-label="Transaction history wallet address input"
+            className="w-full bg-transparent py-1 pl-1 pr-3 text-sm font-light placeholder:text-white/60"
+            // stop password managers from autofilling
+            data-1p-ignore
+            data-lpignore="true"
+            data-form-type="other"
+          />
+        </form>
+      </div>
     </div>
   )
 }
