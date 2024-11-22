89f0b8bbe1024f83733d11b9509243b4be31e397
Fionna <13184582+fionnachan@users.noreply.github.com>
2024-11-22 13:26:06 +0000

    review comment

--- packages/arb-token-bridge-ui/src/pages/_app.tsx
+++ packages/arb-token-bridge-ui/src/pages/_app.tsx
@@ -7,7 +7,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import advancedFormat from 'dayjs/plugin/advancedFormat'
 import timeZone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
-import { Chain } from 'wagmi'
+import type { Chain } from 'wagmi'
 
 import 'tippy.js/dist/tippy.css'
 import 'tippy.js/themes/light.css'
