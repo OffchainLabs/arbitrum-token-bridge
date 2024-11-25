9e0333c34a00e7e6c6257938cfb6094cec97b37e
Fionna Chan <13184582+fionnachan@users.noreply.github.com>
2024-11-25 12:44:43 +0000

    update comment with extra info
    Co-authored-by: spsjvc <git@spsjvc.com>

--- packages/arb-token-bridge-ui/src/pages/_app.tsx
+++ packages/arb-token-bridge-ui/src/pages/_app.tsx
@@ -157,7 +157,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
     destinationChainInfo = getChainForChainKeyQueryParam(destinationChainSlug)
   } catch (error) {
     // 1. slug misspelling can enter this flow
-    // 2. when user selects a custom orbit chain, it will also go to this flow
+    // 2. when user selects a custom orbit chain, it will also go to this flow (they are only available in local storage and not on the server)
     sourceChainInfo = getChainForChainKeyQueryParam('ethereum')
     destinationChainInfo = getChainForChainKeyQueryParam('arbitrum-one')
   }
