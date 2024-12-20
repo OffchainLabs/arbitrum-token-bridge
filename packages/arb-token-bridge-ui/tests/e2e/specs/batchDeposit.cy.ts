de5c44dd4d4f39a24ceb58583dfe2fb8fc9953fe
Bartek <bartlomiejkramek@outlook.com>
2024-12-20 12:44:47 +0100

    fix

--- packages/arb-token-bridge-ui/tests/e2e/specs/batchDeposit.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/batchDeposit.cy.ts
@@ -152,23 +152,29 @@ describe('Batch Deposit', () => {
 
     context('funds should reach destination account successfully', () => {
       // should have more funds on destination chain
-      cy.findByLabelText(`${ERC20TokenSymbol} balance amount on childChain`)
-        .invoke('text')
-        .then(parseFloat)
-        .should('be.gt', Number(parentErc20Balance))
-      cy.findByLabelText(`${nativeTokenSymbol} balance amount on childChain`)
-        .invoke('text')
-        .then(parseFloat)
-        .should(
-          'be.gt',
+      cy.findByLabelText(
+        `${ERC20TokenSymbol} balance amount on childChain`
+      ).should($el => {
+        const currentBalance = parseFloat($el.text())
+        expect(currentBalance).to.be.gt(Number(parentErc20Balance))
+      })
+
+      cy.findByLabelText(
+        `${nativeTokenSymbol} balance amount on childChain`
+      ).should($el => {
+        const currentBalance = parseFloat($el.text())
+        expect(currentBalance).to.be.gt(
           Number(parentNativeTokenBalance) + nativeCurrencyAmountToSend
         )
+      })
 
       // the balance on the source chain should not be the same as before
-      cy.findByLabelText(`${ERC20TokenSymbol} balance amount on parentChain`)
-        .invoke('text')
-        .then(parseFloat)
-        .should('be.lt', Number(parentErc20Balance))
+      cy.findByLabelText(
+        `${ERC20TokenSymbol} balance amount on parentChain`
+      ).should($el => {
+        const currentBalance = parseFloat($el.text())
+        expect(currentBalance).to.be.lt(Number(parentErc20Balance))
+      })
     })
 
     context('transfer panel amount should be reset', () => {
