45a25c9f4b3d48ad31fe870b6b2936f846f56c62
dewanshparashar <dewanshparashar@yahoo.in>
2025-01-13 16:51:42 +0530

    dev: working

--- packages/arb-token-bridge-ui/src/token-bridge-sdk/EnhancedProvider.ts
+++ packages/arb-token-bridge-ui/src/token-bridge-sdk/EnhancedProvider.ts
@@ -1,46 +1,100 @@
 import {
-  JsonRpcBatchProvider,
+  StaticJsonRpcProvider,
   TransactionReceipt
 } from '@ethersproject/providers'
+import { BigNumber } from 'ethers'
 
-class EnhancedProvider extends JsonRpcBatchProvider {
-  private chainId: number
+const cacheKey = `arbitrum:bridge:tx-receipts`
+const cacheLogs = false
 
-  constructor(url?: string, chainId?: number) {
-    super(url)
-
-    if (typeof chainId === 'undefined')
-      throw Error('could not initialize provider')
-
-    this.chainId = chainId
+/*
+ when BigNumbers are stored in localStorage, they get flattened to normal Objects and lose their `BigNumber` properties
+ these are utility functions to serialize and deserialize BigNumber data types, to and from `localStorage`
+ */
+BigNumber.prototype.toJSON = function toJSON() {
+  return {
+    _type: 'BigNumber',
+    _data: Object.assign({}, this)
   }
+}
+function getBigNumberFromJSON(_key: string, value: any) {
+  let result = value
+  if (
+    typeof value === 'object' &&
+    value !== null &&
+    value.hasOwnProperty('_type')
+  ) {
+    switch (value._type) {
+      case 'BigNumber':
+        result = BigNumber.from(value._data)
+    }
+  }
+  return result
+}
 
+function getTxReceiptFromCache(
+  chainId: number,
+  txHash: string
+): TransactionReceipt | undefined {
+  if (typeof localStorage === 'undefined') return undefined
+
+  const cachedReceipts = localStorage.getItem(cacheKey)
+
+  const allReceipts = cachedReceipts
+    ? JSON.parse(cachedReceipts, getBigNumberFromJSON)
+    : {}
+
+  return allReceipts[chainId]
+    ? (allReceipts[chainId][txHash] as TransactionReceipt)
+    : undefined
+}
+
+function addTxReceiptToCache(chainId: number, txReceipt: TransactionReceipt) {
+  if (typeof localStorage === 'undefined') return
+
+  const cachedReceipts = localStorage.getItem(cacheKey)
+
+  const allReceipts = cachedReceipts
+    ? JSON.parse(cachedReceipts, getBigNumberFromJSON)
+    : {}
+
+  localStorage.setItem(
+    cacheKey,
+    JSON.stringify({
+      ...allReceipts,
+      [chainId]: {
+        ...(allReceipts[chainId] ?? {}),
+        [txReceipt.transactionHash]: txReceipt
+      }
+    })
+  )
+}
+
+class EnhancedProvider extends StaticJsonRpcProvider {
   async getTransactionReceipt(
     transactionHash: string | Promise<string>
   ): Promise<TransactionReceipt> {
     const hash = await transactionHash
-    const cacheKey = `arbitrum:bridge:tx-receipts:${this.chainId}`
 
-    // Retrieve the cached receipts for the specific chainId
-    const cachedReceipts = localStorage.getItem(cacheKey)
-    const receiptsMap = cachedReceipts ? JSON.parse(cachedReceipts) : {}
-
+    // Retrieve the cached receipts for the specific
+    const cachedReceipt = getTxReceiptFromCache(this.network.chainId, hash)
     // Check if the receipt is already cached
-    if (receiptsMap[hash]) {
-      console.log(`Cache hit for transaction: ${hash}`)
-      return receiptsMap[hash] as TransactionReceipt
+    if (cachedReceipt) {
+      if (cacheLogs) console.log(`Cache hit for transaction: ${hash}`)
+
+      return cachedReceipt
     }
 
-    console.log(`Cache miss for transaction: ${hash}`)
+    if (cacheLogs) console.log(`Cache miss for transaction: ${hash}`)
 
     // Call the original method to fetch the receipt
     const receipt = await super.getTransactionReceipt(hash)
 
     // Cache the receipt if it exists
     if (receipt) {
-      receiptsMap[hash] = receipt
-      localStorage.setItem(cacheKey, JSON.stringify(receiptsMap))
-      console.log(`Added transaction to cache: ${hash}`)
+      addTxReceiptToCache(this.network.chainId, receipt)
+
+      if (cacheLogs) console.log(`Added transaction to cache: ${hash}`)
     }
 
     return receipt
