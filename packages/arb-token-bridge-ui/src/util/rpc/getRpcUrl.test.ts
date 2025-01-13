dbf96ce3624306e0cc1eabb06d535ed00136ce1c
spsjvc <git@spsjvc.com>
2025-01-13 12:14:22 +0100

    fix tests

--- packages/arb-token-bridge-ui/src/util/rpc/getRpcUrl.test.ts
+++ packages/arb-token-bridge-ui/src/util/rpc/getRpcUrl.test.ts
@@ -1,21 +1,26 @@
-import { ChainId } from '../../types/ChainId'
 import { getRpcUrl } from './getRpcUrl'
-import { getInfuraRpcUrl, InfuraSupportedChainId } from './infura'
+import { ChainId } from '../../types/ChainId'
 
-it('(infura) returns rpc for ethereum mainnet', () => {
-  expect(getRpcUrl(1, 'infura', '123456')).toEqual(
-    'https://mainnet.infura.io/v3/123456'
+it('returns correct rpc for ethereum mainnet (infura)', () => {
+  expect(getRpcUrl(ChainId.Ethereum, 'infura', 'infura-key')).toEqual(
+    'https://mainnet.infura.io/v3/infura-key'
   )
 })
 
-it('(infura) returns public rpc for arbitrum nova', () => {
-  expect(getRpcUrl(ChainId.ArbitrumNova, 'infura', '654321')).toEqual(
+it('returns correct rpc for arbitrum nova (infura)', () => {
+  expect(getRpcUrl(ChainId.ArbitrumNova, 'infura', 'infura-key')).toEqual(
     'https://nova.arbitrum.io/rpc'
   )
 })
 
-it('(alchemy) returns rpc for ethereum mainnet', () => {
-  expect(getRpcUrl(1, 'alchemy', '123456')).toEqual(
-    'https://eth-mainnet.g.alchemy.com/v2/123456'
+it('returns correct rpc for ethereum mainnet (alchemy)', () => {
+  expect(getRpcUrl(ChainId.Ethereum, 'alchemy', 'alchemy-key')).toEqual(
+    'https://eth-mainnet.g.alchemy.com/v2/alchemy-key'
+  )
+})
+
+it('returns correct rpc for arbitrum nova (alchemy)', () => {
+  expect(getRpcUrl(ChainId.ArbitrumNova, 'alchemy', 'alchemy-key')).toEqual(
+    'https://arbnova-mainnet.g.alchemy.com/v2/alchemy-key'
   )
 })
