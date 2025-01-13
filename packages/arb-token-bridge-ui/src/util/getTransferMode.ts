dc209eae52b8b211868b01e8718c4fb4c00328ae
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-01-13 18:24:15 +0000

    feat: add getTransferMode and tests

---
+++ packages/arb-token-bridge-ui/src/util/getTransferMode.ts
@@ -0,0 +1,45 @@
+import { getArbitrumNetwork } from '@arbitrum/sdk'
+import { isValidTeleportChainPair } from '../token-bridge-sdk/teleport'
+import { isDepositMode } from './isDepositMode'
+import { getDestinationChainIds } from './networks'
+
+type TransferMode = 'deposit' | 'withdrawal' | 'teleport'
+
+/**
+ * determines if the UI is in deposit mode or withdrawal mode or teleport mode
+ */
+export function getTransferMode({
+  sourceChainId,
+  destinationChainId
+}: {
+  sourceChainId: number
+  destinationChainId: number
+}): TransferMode | undefined {
+  const validDestinationChains = getDestinationChainIds(sourceChainId)
+
+  if (!validDestinationChains.includes(destinationChainId)) {
+    throw new Error('Unsupported source and destination chain pair.')
+  }
+
+  const isDeposit = isDepositMode({ sourceChainId, destinationChainId })
+
+  if (isDeposit) {
+    return 'deposit'
+  }
+
+  const isTeleport = isValidTeleportChainPair({
+    sourceChainId,
+    destinationChainId
+  })
+
+  if (isTeleport) {
+    return 'teleport'
+  }
+
+  const isWithdrawal =
+    getArbitrumNetwork(sourceChainId).parentChainId === destinationChainId
+
+  if (isWithdrawal) {
+    return 'withdrawal'
+  }
+}
