270fb862a35e397f1ec97cf2c23cdfd91ad7941e
dewanshparashar <dewanshparashar@yahoo.in>
2025-01-16 13:40:46 +0530

    dev: flatted data structure

--- packages/arb-token-bridge-ui/src/token-bridge-sdk/EnhancedProvider.ts
+++ packages/arb-token-bridge-ui/src/token-bridge-sdk/EnhancedProvider.ts
@@ -5,11 +5,13 @@ import {
 import { BigNumber } from 'ethers'
 import { ChainId } from '../types/ChainId'
 
-type CachedReceiptsPerChain = Record<string, TransactionReceipt>
-type CachedReceipts = Record<string, CachedReceiptsPerChain>
+type CachedReceipts = Record<string, TransactionReceipt>
 
 const cacheKey = `arbitrum:bridge:tx-receipts-cache`
-const enableCaching = true // switch to turn off tx caching altogether (in case of emergency/hotfix)
+const enableCaching = true
+
+const getCacheKey = (chainId: number | string, txHash: string) =>
+  `${chainId}:${txHash}`.toLowerCase()
 
 /**
  * Encodes BigNumber properties in cached receipts for localStorage.
@@ -18,52 +20,38 @@ const enableCaching = true // switch to turn off tx caching altogether (in case 
  */
 const encodeBigNumbers = (cachedReceipts: CachedReceipts) => {
   const encodedCachedReceipts: Record<string, any> = {}
-  Object.keys(cachedReceipts).forEach((chainId: string) => {
-    encodedCachedReceipts[chainId] = {}
-    const receipts = cachedReceipts[chainId] as CachedReceiptsPerChain
-    Object.keys(receipts).forEach((txHash: string) => {
-      const receipt = receipts[txHash]
-      const encodedReceipt: Record<string, any> = { ...receipt }
 
-      Object.keys(encodedReceipt).forEach(key => {
-        if (encodedReceipt[key]?._isBigNumber) {
-          encodedReceipt[key] = {
-            _type: 'BigNumber',
-            _data: encodedReceipt[key].toString()
-          }
+  Object.entries(cachedReceipts).forEach(([key, receipt]) => {
+    const encodedReceipt: Record<string, any> = { ...receipt }
+
+    Object.keys(encodedReceipt).forEach(field => {
+      if (encodedReceipt[field]?._isBigNumber) {
+        encodedReceipt[field] = {
+          _type: 'BigNumber',
+          _data: encodedReceipt[field].toString()
         }
-      })
-
-      encodedCachedReceipts[chainId][txHash] = encodedReceipt
+      }
     })
+
+    encodedCachedReceipts[key] = encodedReceipt
   })
+
   return encodedCachedReceipts
 }
 
 const decodeBigNumbers = (cacheFromLocalStorage: Record<string, any>) => {
-  let decodedCachedReceipts = {} as CachedReceipts
+  const decodedCachedReceipts: CachedReceipts = {}
 
-  Object.keys(cacheFromLocalStorage).forEach((chainId: string) => {
-    decodedCachedReceipts[chainId] = {} as CachedReceiptsPerChain
+  Object.entries(cacheFromLocalStorage).forEach(([key, receipt]) => {
+    const decodedReceipt = { ...receipt }
 
-    Object.keys(cacheFromLocalStorage[chainId]).forEach((txHash: string) => {
-      const receipt = cacheFromLocalStorage[chainId][txHash]
-      const decodedReceipt = { ...receipt }
-
-      Object.keys(decodedReceipt).forEach(key => {
-        if (decodedReceipt[key] && decodedReceipt[key]._type === 'BigNumber') {
-          decodedReceipt[key] = BigNumber.from(decodedReceipt[key]._data)
-        }
-      })
-
-      decodedCachedReceipts = {
-        ...decodedCachedReceipts,
-        [chainId]: {
-          ...decodedCachedReceipts[chainId],
-          [txHash]: decodedReceipt
-        }
+    Object.keys(decodedReceipt).forEach(field => {
+      if (decodedReceipt[field]?._type === 'BigNumber') {
+        decodedReceipt[field] = BigNumber.from(decodedReceipt[field]._data)
       }
     })
+
+    decodedCachedReceipts[key] = decodedReceipt as TransactionReceipt
   })
 
   return decodedCachedReceipts
@@ -98,41 +86,30 @@ const shouldCacheTxReceipt = (
 }
 
 function getTxReceiptFromCache(chainId: number, txHash: string) {
-  if (!enableCaching) return undefined
-
-  if (typeof localStorage === 'undefined') return undefined
+  if (!enableCaching || typeof localStorage === 'undefined') return undefined
 
   const cachedReceipts = localStorage.getItem(cacheKey)
+  if (!cachedReceipts) return undefined
 
-  const allReceipts = cachedReceipts
-    ? decodeBigNumbers(JSON.parse(cachedReceipts))
-    : {}
-
-  const cachedReceiptsPerChain = allReceipts[chainId]
-
-  return cachedReceiptsPerChain?.[txHash]
-    ? cachedReceiptsPerChain[txHash]
-    : undefined
+  const allReceipts = decodeBigNumbers(JSON.parse(cachedReceipts))
+  return allReceipts[getCacheKey(chainId, txHash)]
 }
 
 function addTxReceiptToCache(chainId: number, txReceipt: TransactionReceipt) {
   if (typeof localStorage === 'undefined') return
 
   const cachedReceipts = localStorage.getItem(cacheKey)
-
   const allReceipts = cachedReceipts
     ? decodeBigNumbers(JSON.parse(cachedReceipts))
     : {}
 
+  const key = getCacheKey(chainId, txReceipt.transactionHash)
   localStorage.setItem(
     cacheKey,
     JSON.stringify(
       encodeBigNumbers({
         ...allReceipts,
-        [chainId]: {
-          ...(allReceipts[chainId] ?? {}),
-          [txReceipt.transactionHash]: txReceipt
-        }
+        [key]: txReceipt
       })
     )
   )
@@ -149,7 +126,7 @@ class EnhancedProvider extends StaticJsonRpcProvider {
     const cachedReceipt = getTxReceiptFromCache(chainId, hash)
     if (cachedReceipt) return cachedReceipt
 
-    // Fetch the receipt using the original method
+    // Else, fetch the receipt using the original method
     const receipt = await super.getTransactionReceipt(hash)
 
     // Cache the receipt if it meets the criteria
