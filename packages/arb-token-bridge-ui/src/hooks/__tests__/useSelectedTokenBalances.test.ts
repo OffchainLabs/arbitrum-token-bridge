30b910ea1ee299ca3de92c99e4c92673e452f01e
Bartek <bartlomiejkramek@outlook.com>
2025-01-21 19:18:37 +0100

    fix

--- packages/arb-token-bridge-ui/src/hooks/__tests__/useSelectedTokenBalances.test.ts
+++ packages/arb-token-bridge-ui/src/hooks/__tests__/useSelectedTokenBalances.test.ts
@@ -18,15 +18,17 @@ jest.mock('../useBalances', () => ({
 }))
 
 jest.mock('../useSelectedToken', () => ({
-  useSelectedToken: jest.fn().mockReturnValue({
-    type: 'ERC20',
-    decimals: 18,
-    name: 'random',
-    symbol: 'RAND',
-    address: '0x123',
-    l2Address: '0x234',
-    listIds: new Set('1')
-  })
+  useSelectedToken: jest.fn().mockReturnValue([
+    {
+      type: 'ERC20',
+      decimals: 18,
+      name: 'random',
+      symbol: 'RAND',
+      address: '0x123',
+      l2Address: '0x234',
+      listIds: new Set('1')
+    }
+  ])
 }))
 
 describe('useSelectedTokenBalances', () => {
