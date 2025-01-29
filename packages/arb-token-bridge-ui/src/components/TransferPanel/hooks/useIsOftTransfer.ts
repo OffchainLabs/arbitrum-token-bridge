823baefee4cde63e166fb87121245d9a996e7bc8
dewanshparashar <dewanshparashar@yahoo.in>
2025-01-29 12:58:49 +0400

    dev: fixes

--- packages/arb-token-bridge-ui/src/components/TransferPanel/hooks/useIsOftTransfer.ts
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/hooks/useIsOftTransfer.ts
@@ -2,7 +2,6 @@ import { useNetworks } from '../../../hooks/useNetworks'
 import { useNetworksRelationship } from '../../../hooks/useNetworksRelationship'
 import { useAppState } from '../../../state'
 import { useMemo } from 'react'
-import { getProviderForChainId } from '../../../token-bridge-sdk/utils'
 import { isLayerZeroToken } from '../../../token-bridge-sdk/oftUtils'
 
 export const useIsOftTransfer = function () {
@@ -10,29 +9,24 @@ export const useIsOftTransfer = function () {
     app: { selectedToken }
   } = useAppState()
   const [networks] = useNetworks()
-  const { isDepositMode, isTeleportMode } = useNetworksRelationship(networks)
+  const { isTeleportMode } = useNetworksRelationship(networks)
 
   return useMemo(async () => {
     if (!selectedToken) {
       return false
     }
 
-    // OFT transfers are not compatible with teleport mode
-    if (isTeleportMode) {
-      return false
-    }
-
     try {
-      const sourceChainProvider = getProviderForChainId(networks.sourceChain.id)
+      // Only allow OFT transfers if:
+      // 1. We have a valid token selected
+      // 2. Token is an OFT
+      // 3. We're in deposit or withdrawal mode (not teleport)
+
       const isOft = await isLayerZeroToken(
         selectedToken.address,
         networks.sourceChain.id
       )
 
-      // Only allow OFT transfers if:
-      // 1. Token is an OFT
-      // 2. We're in deposit or withdrawal mode (not teleport)
-      // 3. We have a valid token selected
       return isOft && !isTeleportMode && !!selectedToken
     } catch (error) {
       console.warn('Error checking if token is OFT:', error)
