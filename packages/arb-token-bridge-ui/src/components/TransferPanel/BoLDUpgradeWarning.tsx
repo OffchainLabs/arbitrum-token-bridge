81e34454de8b48fe24860c18b107f7a3223d02cd
dewanshparashar <dewanshparashar@yahoo.in>
2025-02-03 13:42:00 +0400

    dev: eslint fix

--- packages/arb-token-bridge-ui/src/components/TransferPanel/BoLDUpgradeWarning.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/BoLDUpgradeWarning.tsx
@@ -18,33 +18,35 @@ export const BoLDUpgradeWarning = () => {
       <p>
         Withdrawals initiated between{' '}
         <span className="font-bold">Feb 5 09:00 ET and Feb 12 09:00 ET</span> on
-        Arbitrum&apos;s canonical bridge (the page you&apos;'re on right now) will be
-        delayed by an <span className="underline">additional</span> 6.4 days.{' '}
+        Arbitrum&apos;s canonical bridge (the page you&apos;'re on right now)
+        will be delayed by an <span className="underline">additional</span> 6.4
+        days.{' '}
       </p>
 
       <p>This is happening because BoLD is getting an essential update.</p>
 
-    <div className="mt-2 flex w-full flex-col gap-2 rounded border border-white/30 bg-orange-dark/60 p-2 text-sm font-light text-white">
-      <p>It&apos;s super annoying, but it&apos;s worth it, trust us.</p>
+      <div className="mt-2 flex w-full flex-col gap-2 rounded border border-white/30 bg-orange-dark/60 p-2 text-sm font-light text-white">
+        <p>It&apos;s super annoying, but it&apos;s worth it, trust us.</p>
 
-      <p>
-        To avoid this additional delay of nearly a week, you can either:
-        <ul>
-          <li>Use a third-party fast bridge</li>
-          <li>Do your withdrawal before Feb 5 or after Feb 12</li>
-        </ul>
-      </p>
+        <p>
+          To avoid this additional delay of nearly a week, you can either:
+          <ul>
+            <li>Use a third-party fast bridge</li>
+            <li>Do your withdrawal before Feb 5 or after Feb 12</li>
+          </ul>
+        </p>
 
-      <p>
-        Read more about the upgrade{' '}
-        <ExternalLink
-          className="underline"
-          href="https://docs.arbitrum.io/how-arbitrum-works/bold/gentle-introduction#wen-mainnet"
-        >
-          here
-        </ExternalLink>
-        .
-      </p>
+        <p>
+          Read more about the upgrade{' '}
+          <ExternalLink
+            className="underline"
+            href="https://docs.arbitrum.io/how-arbitrum-works/bold/gentle-introduction#wen-mainnet"
+          >
+            here
+          </ExternalLink>
+          .
+        </p>
+      </div>
     </div>
   )
 }
