99f64aaffef0188be66893ded48dde45b8533f1d
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-02-12 14:04:00 +0000

    fix prettier config file

---
+++ packages/arb-token-bridge-ui/prettier.config.cjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+
+  // Plugins
+  plugins: ['prettier-plugin-tailwindcss']
+}
