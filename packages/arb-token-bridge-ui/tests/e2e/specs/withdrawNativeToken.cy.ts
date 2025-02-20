d2c62e1fc09210eea9490fb7a1412cecdd135847
dewanshparashar <dewanshparashar@yahoo.in>
2025-02-20 15:17:08 +0400

    dev: fix e2e

--- packages/arb-token-bridge-ui/tests/e2e/specs/withdrawNativeToken.cy.ts
+++ packages/arb-token-bridge-ui/tests/e2e/specs/withdrawNativeToken.cy.ts
@@ -90,7 +90,7 @@ describe('Withdraw native token', () => {
           cy.confirmMetamaskTransaction()
 
           cy.findTransactionInTransactionHistory({
-            duration: 'an hour',
+            duration: 'a few seconds',
             amount: ETHToWithdraw,
             symbol: nativeTokenSymbol
           })
@@ -180,7 +180,7 @@ describe('Withdraw native token', () => {
       }
 
       cy.findTransactionInTransactionHistory({
-        duration: 'an hour',
+        duration: 'a few seconds',
         ...txData
       })
       cy.openTransactionDetails(txData)
