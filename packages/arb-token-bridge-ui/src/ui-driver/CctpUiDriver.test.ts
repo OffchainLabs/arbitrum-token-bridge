64f4a82ac7408c4593eb6c17bdc71aef0ae297f7
spsjvc <git@spsjvc.com>
2025-02-25 20:07:24 +0100

    progress

--- packages/arb-token-bridge-ui/src/ui-driver/CctpUiDriver.test.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/CctpUiDriver.test.ts
@@ -5,7 +5,11 @@ import {
   UiDriverStepDialog
 } from './CctpUiDriver'
 
-it('deposit: user rejects 1st dialog', async () => {
+it(`
+  isDepositMode=true
+
+  * dialog(cctp_withdrawal) -> user rejects
+`, async () => {
   const steps = CctpUiDriver.createSteps({
     isDepositMode: true
   } as UiDriverContext)
@@ -19,16 +23,75 @@ it('deposit: user rejects 1st dialog', async () => {
   expect(step2).toBeUndefined()
 })
 
-it('withdrawal: user rejects 1st dialog', async () => {
+it(`
+  isDepositMode=false,isSmartContractWallet=false
+
+  * dialog(cctp_withdrawal) -> user rejects
+`, async () => {
   const steps = CctpUiDriver.createSteps({
-    isDepositMode: false
-  } as UiDriverContext)
+    isDepositMode: false,
+    isSmartContractWallet: false
+  })
 
   const step1 = await (await steps.next()).value
   expect(step1).toBeDefined()
   expect((step1 as UiDriverStep).type).toEqual('dialog')
   expect((step1 as UiDriverStepDialog).dialog).toEqual('cctp_withdrawal')
+  const step1UserInput = false
 
-  const step2 = await (await steps.next(false)).value
+  const step2 = await (await steps.next(step1UserInput)).value
   expect(step2).toBeUndefined()
+})
+
+it(`
+  isDepositMode=false,isSmartContractWallet=true
+
+  * dialog(cctp_withdrawal)       -> user confirms
+  * dialog(custom_dest_addr_warn) -> user rejects
+`, async () => {
+  const steps = CctpUiDriver.createSteps({
+    isDepositMode: false,
+    isSmartContractWallet: true
+  })
+
+  const step1 = await (await steps.next()).value
+  expect(step1).toBeDefined()
+  expect((step1 as UiDriverStep).type).toEqual('dialog')
+  expect((step1 as UiDriverStepDialog).dialog).toEqual('cctp_withdrawal')
+  const step1UserInput = true
+
+  const step2 = await (await steps.next(step1UserInput)).value
+  expect((step2 as UiDriverStep).type).toEqual('dialog')
+  expect((step2 as UiDriverStepDialog).dialog).toEqual('custom_dest_addr_warn')
+  const step2UserInput = false
+
+  const step3 = await (await steps.next(step2UserInput)).value
+  expect(step3).toBeUndefined()
+})
+
+it(`
+  isDepositMode=false,isSmartContractWallet=true
+
+  * dialog(cctp_withdrawal)       -> user confirms
+  * dialog(custom_dest_addr_warn) -> user confirms
+`, async () => {
+  const steps = CctpUiDriver.createSteps({
+    isDepositMode: false,
+    isSmartContractWallet: true
+  })
+
+  const step1 = await (await steps.next()).value
+  expect(step1).toBeDefined()
+  expect((step1 as UiDriverStep).type).toEqual('dialog')
+  expect((step1 as UiDriverStepDialog).dialog).toEqual('cctp_withdrawal')
+  const step1UserInput = true
+
+  const step2 = await (await steps.next(step1UserInput)).value
+  expect((step2 as UiDriverStep).type).toEqual('dialog')
+  expect((step2 as UiDriverStepDialog).dialog).toEqual('custom_dest_addr_warn')
+  const step2UserInput = true
+
+  const step3 = await (await steps.next(step2UserInput)).value
+  expect((step3 as UiDriverStep).type).toEqual('dialog')
+  expect((step3 as UiDriverStepDialog).dialog).toEqual('test')
 })
--- packages/arb-token-bridge-ui/src/ui-driver/CctpUiDriver.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/CctpUiDriver.ts
@@ -1,4 +1,8 @@
-export type Dialog = 'cctp_deposit' | 'cctp_withdrawal'
+export type Dialog =
+  | 'cctp_deposit'
+  | 'cctp_withdrawal'
+  | 'custom_dest_addr_warn'
+  | 'test'
 
 export type UiDriverStepDialog = {
   type: 'dialog'
@@ -9,15 +13,36 @@ export type UiDriverStep = UiDriverStepDialog
 
 export type UiDriverContext = {
   isDepositMode: boolean
+  isSmartContractWallet: boolean
 }
 
 export class CctpUiDriver {
   static async *createSteps(
     context: UiDriverContext
-  ): AsyncGenerator<UiDriverStep> {
-    yield {
+  ): AsyncGenerator<UiDriverStep, void, boolean> {
+    const step1UserInput = yield {
       type: 'dialog',
       dialog: context.isDepositMode ? 'cctp_deposit' : 'cctp_withdrawal'
+    }
+
+    if (!step1UserInput) {
+      return
+    }
+
+    if (context.isSmartContractWallet) {
+      const step2UserInput = yield {
+        type: 'dialog',
+        dialog: 'custom_dest_addr_warn'
+      }
+
+      if (!step2UserInput) {
+        return
+      }
+    }
+
+    yield {
+      type: 'dialog',
+      dialog: 'test'
     }
   }
 }
