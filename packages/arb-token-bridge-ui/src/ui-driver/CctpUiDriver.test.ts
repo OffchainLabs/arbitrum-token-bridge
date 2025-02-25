d080ac6cdea41b46df429c7afc79129969eda86d
spsjvc <git@spsjvc.com>
2025-02-25 19:52:13 +0100

    clean up

--- packages/arb-token-bridge-ui/src/ui-driver/CctpUiDriver.test.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/CctpUiDriver.test.ts
@@ -5,26 +5,30 @@ import {
   UiDriverStepDialog
 } from './CctpUiDriver'
 
-it('deposits', async () => {
+it('deposit: user rejects 1st dialog', async () => {
   const steps = CctpUiDriver.createSteps({
     isDepositMode: true
   } as UiDriverContext)
 
   const step1 = await (await steps.next()).value
-
   expect(step1).toBeDefined()
   expect((step1 as UiDriverStep).type).toEqual('dialog')
   expect((step1 as UiDriverStepDialog).dialog).toEqual('cctp_deposit')
+
+  const step2 = await (await steps.next()).value
+  expect(step2).toBeUndefined()
 })
 
-it('withdrawals', async () => {
+it('withdrawal: user rejects 1st dialog', async () => {
   const steps = CctpUiDriver.createSteps({
     isDepositMode: false
   } as UiDriverContext)
 
   const step1 = await (await steps.next()).value
-
   expect(step1).toBeDefined()
   expect((step1 as UiDriverStep).type).toEqual('dialog')
   expect((step1 as UiDriverStepDialog).dialog).toEqual('cctp_withdrawal')
+
+  const step2 = await (await steps.next(false)).value
+  expect(step2).toBeUndefined()
 })
