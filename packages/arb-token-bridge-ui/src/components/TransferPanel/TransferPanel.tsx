dbf82167122aacf9935a13a8da45cc882399f38f
Christophe <contact@christophedeveaux.com>
2025-02-25 15:14:00 +0000

    Clear route when changing networks

--- packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanel.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/TransferPanel.tsx
@@ -186,7 +186,8 @@ export function TransferPanel() {
   useEffect(() => {
     // hide Project listing when networks are changed
     setShowProjectsListing(false)
-  }, [childChain.id, parentChain.id])
+    clearRoute()
+  }, [childChain.id, clearRoute, parentChain.id])
 
   useEffect(() => {
     if (importTokenModalStatus !== ImportTokenModalStatus.IDLE) {
