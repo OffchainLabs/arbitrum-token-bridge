6e5df118def706c2026c0b88faac949c56f7df9d
Christophe <contact@christophedeveaux.com>
2025-02-26 23:24:32 +0000

    Add batch transfer, remove fast withdrawal

--- packages/arb-token-bridge-ui/src/components/TransferPanel/Routes/Route.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/Routes/Route.tsx
@@ -12,18 +12,18 @@ import { useNetworksRelationship } from '../../../hooks/useNetworksRelationship'
 import { RouteType, useRouteStore } from '../hooks/useRouteStore'
 import { SecurityGuaranteed } from '../SecurityLabels'
 import { TokenLogo } from '../TokenLogo'
-import { getConfirmationTime } from '../../../util/WithdrawalUtils'
-import { Tooltip } from '../../common/Tooltip'
-import { InformationCircleIcon } from '@heroicons/react/24/outline'
-import LightningIcon from '@/images/LightningIcon.svg'
+import React from 'react'
+import { useArbQueryParams } from '../../../hooks/useArbQueryParams'
+import { useIsBatchTransferSupported } from '../../../hooks/TransferPanel/useIsBatchTransferSupported'
 
 export type BadgeType = 'security-guaranteed'
-type Token = {
+export type Token = {
   address: string
   decimals: number
   symbol: string
   logoURI?: string
 }
+export type RouteGas = { gasCost: string | undefined; gasToken: Token }
 export type RouteProps = {
   type: RouteType
   amountReceived: string
@@ -31,8 +31,8 @@ export type RouteProps = {
   isLoadingGasEstimate: boolean
   /** Allow overrides of displayed token */
   overrideToken?: Token
-  gasCost: string | undefined
-  gasToken: Token
+  /** We might have multiple gas token, for example an ER20 deposit to XAI from Arb1 */
+  gasCost: RouteGas[]
   bridge: string
   bridgeIconURI: string
   tag?: BadgeType
@@ -76,127 +76,130 @@ function getBadge(badgeType: BadgeType) {
   }
 }
 
-export function Route({
-  type,
-  bridge,
-  bridgeIconURI,
-  durationMs,
-  amountReceived,
-  isLoadingGasEstimate,
-  overrideToken,
-  gasCost,
-  gasToken,
-  tag,
-  selected
-}: RouteProps) {
-  const [networks] = useNetworks()
-  const { childChainProvider, isDepositMode } =
-    useNetworksRelationship(networks)
-  const childNativeCurrency = useNativeCurrency({
-    provider: childChainProvider
-  })
-  const [_token] = useSelectedToken()
-  const { fastWithdrawalActive } =
-    !isDepositMode && type !== 'oftV2'
-      ? getConfirmationTime(networks.sourceChain.id)
-      : { fastWithdrawalActive: false }
-  const setSelectedRoute = useRouteStore(state => state.setSelectedRoute)
+export const Route = React.memo(
+  ({
+    type,
+    bridge,
+    bridgeIconURI,
+    durationMs,
+    amountReceived,
+    isLoadingGasEstimate,
+    overrideToken,
+    gasCost,
+    tag,
+    selected
+  }: RouteProps) => {
+    const [networks] = useNetworks()
+    const { childChainProvider } = useNetworksRelationship(networks)
+    const childNativeCurrency = useNativeCurrency({
+      provider: childChainProvider
+    })
+    const [_token] = useSelectedToken()
+    const isBatchTransferSupported = useIsBatchTransferSupported()
+    const [{ amount2 }] = useArbQueryParams()
 
-  const token = overrideToken || _token || childNativeCurrency
+    const setSelectedRoute = useRouteStore(state => state.setSelectedRoute)
 
-  const { name, icon, width, height } = getBridgeConfigFromType(type)
+    const token = overrideToken || _token || childNativeCurrency
 
-  return (
-    <div
-      className={twMerge(
-        'group cursor-pointer rounded border border-[#ffffff33] text-white transition-colors',
-        selected && 'border-white'
-      )}
-      onClick={() => setSelectedRoute(type)}
-    >
-      <div className="bg-gray-8 flex items-center rounded-t px-4 py-2 text-xs">
-        <Image
-          src={icon}
-          width={width}
-          height={height}
-          alt="protocol"
-          className="mr-1"
-        />
-        Powered by {name}
-      </div>
+    const { name, icon, width, height } = getBridgeConfigFromType(type)
+
+    return (
       <div
         className={twMerge(
-          'relative flex rounded-b bg-[#303030] px-4 py-3 text-sm transition-colors group-hover:bg-[#474747]',
-          selected && 'bg-[#474747]'
+          'group cursor-pointer rounded border border-[#ffffff33] text-white transition-colors',
+          selected && 'border-white'
         )}
+        onClick={() => setSelectedRoute(type)}
       >
-        <div className="flex flex-col">
-          <span>You will receive:</span>
-          <div className="flex items-center text-lg">
-            <TokenLogo srcOverride={'logoURI' in token ? token.logoURI : ''} />
-            {formatAmount(BigNumber.from(amountReceived), {
-              decimals: token.decimals,
-              symbol: token.symbol
-            })}
-          </div>
+        <div className="bg-gray-8 flex items-center rounded-t px-4 py-2 text-xs">
+          <Image
+            src={icon}
+            width={width}
+            height={height}
+            alt="protocol"
+            className="mr-1"
+          />
+          Powered by {name}
         </div>
-        <div className="ml-6 flex flex-col justify-between gap-3">
-          <div className="flex items-center">
-            <Image
-              src="/icons/duration.svg"
-              width={15}
-              height={15}
-              alt="duration"
-            />
-            <span className="ml-1">
-              {dayjs().add(durationMs, 'millisecond').fromNow(true)}
-            </span>
-            {fastWithdrawalActive && (
-              <div className="flex items-center">
-                <Tooltip
-                  content={
-                    'Fast Withdrawals relies on a committee of validators. In the event of a committee outage, your withdrawal falls back to the 7 day challenge period secured by Arbitrum Fraud Proofs.'
-                  }
-                >
-                  <InformationCircleIcon className="h-3 w-3 sm:ml-1" />
-                </Tooltip>
-                <div className="ml-1 flex space-x-0.5 text-[#FFD000]">
-                  <Image src={LightningIcon} alt="Lightning Icon" />
-                  <span className="font-normal">FAST</span>
-                </div>
+        <div
+          className={twMerge(
+            'relative flex rounded-b bg-[#303030] px-4 py-3 text-sm transition-colors group-hover:bg-[#474747]',
+            selected && 'bg-[#474747]'
+          )}
+        >
+          <div className="flex flex-col">
+            <span>You will receive:</span>
+            <div className="flex flex-col text-lg">
+              <div className="flex flex-row items-center gap-2">
+                <TokenLogo
+                  srcOverride={'logoURI' in token ? token.logoURI : ''}
+                />
+                {formatAmount(BigNumber.from(amountReceived), {
+                  decimals: token.decimals,
+                  symbol: token.symbol
+                })}
               </div>
-            )}
-          </div>
-          <div className="flex items-center">
-            <Image src="/icons/gas.svg" width={15} height={15} alt="gas" />
-            <span className="ml-1">
-              {isLoadingGasEstimate ? (
-                <Loader size="small" color="white" />
-              ) : gasCost ? (
-                formatAmount(BigNumber.from(gasCost), {
-                  decimals: gasToken.decimals,
-                  symbol: gasToken.symbol
-                })
-              ) : (
-                'N/A'
+              {isBatchTransferSupported && (
+                <div className="flew-row flex items-center gap-2">
+                  <TokenLogo srcOverride={null} />
+                  {formatAmount(BigNumber.from(amount2), {
+                    decimals: 0,
+                    symbol: childNativeCurrency.symbol
+                  })}
+                </div>
               )}
-            </span>
+            </div>
           </div>
-          <div className="flex items-center">
-            <SafeImage
-              src={bridgeIconURI}
-              width={15}
-              height={15}
-              alt="bridge"
-              className="max-h-3 max-w-3 rounded-full"
-            />
-            <span className="ml-1">{bridge}</span>
+          <div className="ml-6 flex flex-col justify-between gap-3">
+            <div className="flex items-center">
+              <Image
+                src="/icons/duration.svg"
+                width={15}
+                height={15}
+                alt="duration"
+              />
+              <span className="ml-1">
+                {dayjs().add(durationMs, 'millisecond').fromNow(true)}
+              </span>
+            </div>
+            <div className="flex items-center">
+              <Image src="/icons/gas.svg" width={15} height={15} alt="gas" />
+              <span className="ml-1">
+                {isLoadingGasEstimate ? (
+                  <Loader size="small" color="white" />
+                ) : gasCost ? (
+                  gasCost
+                    .map(({ gasCost, gasToken }) =>
+                      formatAmount(BigNumber.from(gasCost), {
+                        decimals: gasToken.decimals,
+                        symbol: gasToken.symbol
+                      })
+                    )
+                    .join(' and ')
+                ) : (
+                  'N/A'
+                )}
+              </span>
+            </div>
+            <div className="flex items-center">
+              <SafeImage
+                src={bridgeIconURI}
+                width={15}
+                height={15}
+                alt="bridge"
+                className="max-h-3 max-w-3 rounded-full"
+              />
+              <span className="ml-1">{bridge}</span>
+            </div>
           </div>
+          {tag ? (
+            <div className="absolute right-2 top-2">{getBadge(tag)()}</div>
+          ) : null}
         </div>
-        {tag ? (
-          <div className="absolute right-2 top-2">{getBadge(tag)()}</div>
-        ) : null}
       </div>
-    </div>
-  )
-}
+    )
+  }
+)
+
+Route.displayName = 'Route'
