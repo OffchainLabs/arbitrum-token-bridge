2af71a5f7c60f9501473b6b1934669edf097bc8d
spsjvc <git@spsjvc.com>
2025-03-04 23:25:53 +0700

    add test util

--- packages/arb-token-bridge-ui/src/ui-driver/UiDriverTestUtils.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/UiDriverTestUtils.ts
@@ -1,6 +1,12 @@
-import { UiDriverStep } from './UiDriver'
+import { Dialog, UiDriverStep } from './UiDriver'
 
 export function expectStartStep(step: UiDriverStep | void) {
   expect(step).toBeDefined()
   expect(step!.type).toEqual('start')
+}
+
+export function expectDialogStep(step: UiDriverStep | void, dialog: Dialog) {
+  expect(step).toBeDefined()
+  expect(step!.type).toEqual('dialog')
+  expect((step as { payload: Dialog }).payload).toEqual(dialog)
 }
