672d35006466f2c8a09da01bea005adb2eaf550d
spsjvc <git@spsjvc.com>
2025-03-04 23:27:36 +0700

    add stuff

--- packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.test.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.test.ts
@@ -1,7 +1,11 @@
 import { stepGeneratorForCctp } from './UiDriverCctp'
-import { expectStartStep } from './UiDriverTestUtils'
+import { expectStartStep, expectDialogStep } from './UiDriverTestUtils'
 
-it('successfully returns steps', async () => {
+it(`successfully returns steps for context:
+
+  isDepositMode=true
+  isSmartContractWallet=false
+`, async () => {
   const generator = stepGeneratorForCctp({
     isDepositMode: true,
     isSmartContractWallet: false
@@ -9,4 +13,24 @@ it('successfully returns steps', async () => {
 
   const step1 = await (await generator.next()).value
   expectStartStep(step1)
+
+  const step2 = await (await generator.next()).value
+  expectDialogStep(step2, 'cctp_deposit')
+})
+
+it(`successfully returns steps for context:
+
+  isDepositMode=false
+  isSmartContractWallet=false
+`, async () => {
+  const generator = stepGeneratorForCctp({
+    isDepositMode: false,
+    isSmartContractWallet: false
+  })
+
+  const step1 = await (await generator.next()).value
+  expectStartStep(step1)
+
+  const step2 = await (await generator.next()).value
+  expectDialogStep(step2, 'cctp_withdrawal')
 })
--- packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.ts
@@ -1,5 +1,17 @@
 import { step, UiDriverStepGenerator } from './UiDriver'
 
-export const stepGeneratorForCctp: UiDriverStepGenerator = async function* () {
+export const stepGeneratorForCctp: UiDriverStepGenerator = async function* (
+  context
+) {
   yield* step({ type: 'start' })
+
+  const userInput = yield* step({
+    type: 'dialog',
+    payload: context.isDepositMode ? 'cctp_deposit' : 'cctp_withdrawal'
+  })
+
+  if (!userInput) {
+    yield* step({ type: 'return' })
+    return
+  }
 }
