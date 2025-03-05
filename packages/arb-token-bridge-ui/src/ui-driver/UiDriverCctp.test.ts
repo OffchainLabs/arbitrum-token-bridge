83d00163fe68f9319e364145f5e6538e7070b009
spsjvc <git@spsjvc.com>
2025-03-05 18:38:30 +0700

    more tests

--- packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.test.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.test.ts
@@ -46,3 +46,59 @@ it(`successfully returns steps for context:
   const step4 = await nextStep(generator)
   expectStep(step4).doesNotExist()
 })
+
+it(`successfully returns steps for context:
+
+  isDepositMode=false
+  isSmartContractWallet=false
+  walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+  destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+`, async () => {
+  const generator = stepGeneratorForCctp({
+    isDepositMode: true,
+    isSmartContractWallet: false,
+    walletAddress: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+    destinationAddress: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
+  })
+
+  const step1 = await nextStep(generator)
+  expectStep(step1).hasType('start')
+
+  const step2 = await nextStep(generator)
+  expectStep(step2).hasType('dialog').hasPayload('cctp_deposit')
+
+  const step3 = await nextStep(generator, [true])
+  expectStep(step3).doesNotExist()
+})
+
+it(`successfully returns steps for context:
+
+  isDepositMode=false
+  isSmartContractWallet=true
+  walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+  destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+`, async () => {
+  const generator = stepGeneratorForCctp({
+    isDepositMode: true,
+    isSmartContractWallet: true,
+    walletAddress: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+    destinationAddress: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
+  })
+
+  const step1 = await nextStep(generator)
+  expectStep(step1).hasType('start')
+
+  const step2 = await nextStep(generator)
+  expectStep(step2).hasType('dialog').hasPayload('cctp_deposit')
+
+  const step3 = await nextStep(generator, [true])
+  expectStep(step3)
+    .hasType('dialog')
+    .hasPayload('scw_custom_destination_address_equal')
+
+  const step4 = await nextStep(generator, [false])
+  expectStep(step4).hasType('return')
+
+  const step5 = await nextStep(generator)
+  expectStep(step5).doesNotExist()
+})
