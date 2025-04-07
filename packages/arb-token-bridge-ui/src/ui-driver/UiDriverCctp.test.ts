27897de36a4da71c95d39904d1628e5245cb2b54
spsjvc <git@spsjvc.com>
2025-04-07 19:11:32 +0200

    add test and update comments

--- packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.test.ts
+++ packages/arb-token-bridge-ui/src/ui-driver/UiDriverCctp.test.ts
@@ -1,9 +1,13 @@
 import { stepGeneratorForCctp } from './UiDriverCctp'
 import { nextStep, expectStep } from './UiDriverTestUtils'
 
-it(`context:
-  isDepositMode=true
-  isSmartContractWallet=false
+it(`
+  context:
+    isDepositMode=true
+    isSmartContractWallet=false
+
+  user actions:
+    1. user rejects "confirm_cctp_deposit" dialog
 `, async () => {
   const generator = stepGeneratorForCctp({
     isDepositMode: true,
@@ -23,9 +27,13 @@ it(`context:
   expectStep(step4).doesNotExist()
 })
 
-it(`context:
-  isDepositMode=false
-  isSmartContractWallet=false
+it(`
+  context:
+    isDepositMode=false
+    isSmartContractWallet=false
+
+  user actions:
+    1. user rejects "confirm_cctp_withdrawal" dialog
 `, async () => {
   const generator = stepGeneratorForCctp({
     isDepositMode: false,
@@ -45,11 +53,15 @@ it(`context:
   expectStep(step4).doesNotExist()
 })
 
-it(`context:
-  isDepositMode=false
-  isSmartContractWallet=false
-  walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
-  destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+it(`
+  context:
+    isDepositMode=false
+    isSmartContractWallet=false
+    walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+    destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+
+  user actions:
+    1. user confirms "confirm_cctp_deposit" dialog
 `, async () => {
   const generator = stepGeneratorForCctp({
     isDepositMode: true,
@@ -68,11 +80,16 @@ it(`context:
   expectStep(step3).doesNotExist()
 })
 
-it(`context:
-  isDepositMode=false
-  isSmartContractWallet=true
-  walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
-  destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+it(`
+  context:
+    isDepositMode=false
+    isSmartContractWallet=true
+    walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+    destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+
+  user actions:
+    1. user confirms "confirm_cctp_deposit" dialog
+    2. user rejects "scw_custom_destination_address" dialog
 `, async () => {
   const generator = stepGeneratorForCctp({
     isDepositMode: true,
@@ -97,4 +114,37 @@ it(`context:
 
   const step5 = await nextStep(generator)
   expectStep(step5).doesNotExist()
+})
+
+it(`
+  context:
+    isDepositMode=false
+    isSmartContractWallet=true
+    walletAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+    destinationAddress=0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+
+  user actions:
+    1. user confirms "confirm_cctp_deposit" dialog
+    2. user confirms "scw_custom_destination_address" dialog
+`, async () => {
+  const generator = stepGeneratorForCctp({
+    isDepositMode: true,
+    isSmartContractWallet: true,
+    walletAddress: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+    destinationAddress: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
+  })
+
+  const step1 = await nextStep(generator)
+  expectStep(step1).hasType('start')
+
+  const step2 = await nextStep(generator)
+  expectStep(step2).hasType('dialog').hasPayload('confirm_cctp_deposit')
+
+  const step3 = await nextStep(generator, [true])
+  expectStep(step3)
+    .hasType('dialog')
+    .hasPayload('scw_custom_destination_address')
+
+  const step4 = await nextStep(generator, [true])
+  expectStep(step4).doesNotExist()
 })
