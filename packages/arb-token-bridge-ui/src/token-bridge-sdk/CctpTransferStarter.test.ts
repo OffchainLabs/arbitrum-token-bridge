d2faca19f214f08a45ad6946666ba4bc560c47cb
spsjvc <git@spsjvc.com>
2025-04-11 15:08:58 +0200

    add test

---
+++ packages/arb-token-bridge-ui/src/token-bridge-sdk/CctpTransferStarter.test.ts
@@ -0,0 +1,27 @@
+import { it, expect } from 'vitest'
+import { BigNumber } from 'ethers'
+import { StaticJsonRpcProvider } from '@ethersproject/providers'
+
+import { CctpTransferStarter } from './CctpTransferStarter'
+import { CommonAddress } from '../util/CommonAddressUtils'
+
+const starter = new CctpTransferStarter({
+  sourceChainProvider: new StaticJsonRpcProvider(
+    'https://sepolia.gateway.tenderly.co'
+  ),
+  destinationChainProvider: new StaticJsonRpcProvider(
+    'https://sepolia-rollup.arbitrum.io/rpc'
+  )
+})
+
+it('returns the correct data for approve token tx request', async () => {
+  const txRequest = await starter.approveTokenPrepareTxRequest({
+    amount: BigNumber.from(1_000_000) // 1 usdc (usdc is 6 decimals)
+  })
+
+  expect(txRequest).toEqual({
+    to: CommonAddress.Sepolia.USDC,
+    data: '0x095ea7b30000000000000000000000009f3b8679c73c2fef8b59b4f3444d4e156fb70aa500000000000000000000000000000000000000000000000000000000000f4240',
+    value: BigNumber.from(0)
+  })
+})
