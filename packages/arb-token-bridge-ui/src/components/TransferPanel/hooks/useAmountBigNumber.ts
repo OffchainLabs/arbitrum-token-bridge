50a74e1945835717892c2169c3dc86a9a312449c
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-04-17 17:11:51 +0100

    remove debounce

--- packages/arb-token-bridge-ui/src/components/TransferPanel/hooks/useAmountBigNumber.ts
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/hooks/useAmountBigNumber.ts
@@ -1,6 +1,5 @@
 import { useMemo } from 'react'
 import { constants, utils } from 'ethers'
-import { useDebounce } from '@uidotdev/usehooks'
 
 import {
   sanitizeAmountQueryParam,
@@ -11,22 +10,21 @@ import { useSelectedTokenDecimals } from '../../../hooks/TransferPanel/useSelect
 
 export function useAmountBigNumber() {
   const [{ amount }, setQueryParams] = useArbQueryParams()
-  const debouncedAmount = useDebounce(amount, 300)
   const selectedTokenDecimals = useSelectedTokenDecimals()
 
   return useMemo(() => {
     try {
-      if (isNaN(Number(debouncedAmount))) {
+      if (isNaN(Number(amount))) {
         return constants.Zero
       }
 
-      const amountSafe = debouncedAmount || '0'
+      const amountSafe = amount || '0'
 
       const sanitizedAmount = sanitizeAmountQueryParam(
         truncateExtraDecimals(amountSafe, selectedTokenDecimals)
       )
 
-      if (debouncedAmount !== sanitizedAmount) {
+      if (amount !== sanitizedAmount) {
         setQueryParams({ amount: sanitizedAmount })
       }
 
