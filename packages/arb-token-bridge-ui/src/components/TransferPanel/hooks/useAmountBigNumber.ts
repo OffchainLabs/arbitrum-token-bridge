3943140a4a9a4767df8e367bcc3721a55d17c56e
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-04-17 17:06:37 +0100

    refactor: use useAmountBigNumber hook in useGasSummary

--- packages/arb-token-bridge-ui/src/components/TransferPanel/hooks/useAmountBigNumber.ts
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/hooks/useAmountBigNumber.ts
@@ -1,5 +1,6 @@
 import { useMemo } from 'react'
 import { constants, utils } from 'ethers'
+import { useDebounce } from '@uidotdev/usehooks'
 
 import {
   sanitizeAmountQueryParam,
@@ -10,15 +11,22 @@ import { useSelectedTokenDecimals } from '../../../hooks/TransferPanel/useSelect
 
 export function useAmountBigNumber() {
   const [{ amount }, setQueryParams] = useArbQueryParams()
+  const debouncedAmount = useDebounce(amount, 300)
   const selectedTokenDecimals = useSelectedTokenDecimals()
 
   return useMemo(() => {
     try {
+      if (isNaN(Number(debouncedAmount))) {
+        return constants.Zero
+      }
+
+      const amountSafe = debouncedAmount || '0'
+
       const sanitizedAmount = sanitizeAmountQueryParam(
-        truncateExtraDecimals(amount, selectedTokenDecimals)
+        truncateExtraDecimals(amountSafe, selectedTokenDecimals)
       )
 
-      if (amount !== sanitizedAmount) {
+      if (debouncedAmount !== sanitizedAmount) {
         setQueryParams({ amount: sanitizedAmount })
       }
 
