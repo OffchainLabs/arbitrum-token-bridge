7b502a148919e668f363b4d3568cbf146a04d421
Fionna <13184582+fionnachan@users.noreply.github.com>
2025-05-16 11:45:26 +0100

    fix import

--- packages/arb-token-bridge-ui/src/hooks/__tests__/useArbTokenBridge.test.ts
+++ packages/arb-token-bridge-ui/src/hooks/__tests__/useArbTokenBridge.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest'
 import { getProviderForChainId } from '@/token-bridge-sdk/utils'
 import { processTokensFromList } from '../useArbTokenBridge'
 import {
