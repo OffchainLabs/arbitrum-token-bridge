7f94ee8e523467040cc7f0994730a2f534d6c56d
Christophe <contact@christophedeveaux.com>
2025-05-27 12:45:10 +0200

    WIP

--- packages/scripts/src/generateLighthouseReport/executeLighthouse.ts
+++ packages/scripts/src/generateLighthouseReport/executeLighthouse.ts
@@ -34,52 +34,52 @@ export async function executeLighthouseFlow() {
     const screenshot = page.screenshot();
     core.setOutput("image", JSON.stringify(screenshot, null, 2));
 
-    await flow.startTimespan();
+    // await flow.startTimespan();
 
-    // Accept ToS
-    await page.click('[aria-label="Agree to Terms and Continue"]');
+    // // Accept ToS
+    // await page.click('[aria-label="Agree to Terms and Continue"]');
 
-    // Type amount
-    const input = await page.waitForSelector('[aria-label="Amount input"]');
-    await input?.type("2");
-    await page.waitForSelector('[aria-label="Route arbitrum"]');
+    // // Type amount
+    // const input = await page.waitForSelector('[aria-label="Amount input"]');
+    // await input?.type("2");
+    // await page.waitForSelector('[aria-label="Route arbitrum"]');
 
-    // Switch network
-    await page.click('[aria-label="Switch Networks"]');
-    await page.waitForSelector("[aria-label='From Arbitrum One']");
+    // // Switch network
+    // await page.click('[aria-label="Switch Networks"]');
+    // await page.waitForSelector("[aria-label='From Arbitrum One']");
 
-    // Open token selection
-    await page.click("[aria-label='Select Token']");
-    const tokenInput = await page.waitForSelector(
-      "[placeholder='Search by token name, symbol, or address']"
-    );
-    await tokenInput?.type("USDC");
-    await page.click('[aria-label="Select USDC.e"]');
-    await page.waitForSelector("xpath///button[contains(., 'USDC.e')]");
+    // // Open token selection
+    // await page.click("[aria-label='Select Token']");
+    // const tokenInput = await page.waitForSelector(
+    //   "[placeholder='Search by token name, symbol, or address']"
+    // );
+    // await tokenInput?.type("USDC");
+    // await page.click('[aria-label="Select USDC.e"]');
+    // await page.waitForSelector("xpath///button[contains(., 'USDC.e')]");
 
-    // Open chain selection
-    await page.click("[aria-label='From Arbitrum One']");
-    const chainInput = await page.waitForSelector(
-      '[placeholder="Search a network name"]'
-    );
-    await chainInput?.type("Xai");
+    // // Open chain selection
+    // await page.click("[aria-label='From Arbitrum One']");
+    // const chainInput = await page.waitForSelector(
+    //   '[placeholder="Search a network name"]'
+    // );
+    // await chainInput?.type("Xai");
 
-    const xaiRow = await page.waitForSelector('[aria-label="Switch to Xai"]');
-    await xaiRow?.click();
-    await page.waitForSelector("[aria-label='From Xai']");
+    // const xaiRow = await page.waitForSelector('[aria-label="Switch to Xai"]');
+    // await xaiRow?.click();
+    // await page.waitForSelector("[aria-label='From Xai']");
 
-    await flow.endTimespan();
-    await flow.snapshot();
+    // await flow.endTimespan();
+    // await flow.snapshot();
 
-    // Get the comprehensive flow report.
-    // writeFileSync("report.html", await flow.generateReport());
-    // Save results as JSON.
-    // const file = JSON.stringify(await flow.createFlowResult(), null, 2);
-    const file = await flow.createFlowResult();
-    // Cleanup.
-    await browser.close();
-    core.endGroup();
-    return file;
+    // // Get the comprehensive flow report.
+    // // writeFileSync("report.html", await flow.generateReport());
+    // // Save results as JSON.
+    // // const file = JSON.stringify(await flow.createFlowResult(), null, 2);
+    // const file = await flow.createFlowResult();
+    // // Cleanup.
+    // await browser.close();
+    // core.endGroup();
+    // return file;
   } catch (error) {
     console.log(error);
     core.setFailed(error);
