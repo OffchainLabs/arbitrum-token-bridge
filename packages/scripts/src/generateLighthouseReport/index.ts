8a3ba4320b569ab1c70585fb2f4f9c49779899d7
Christophe <contact@christophedeveaux.com>
2025-05-27 12:45:10 +0200

    WIP

--- packages/scripts/src/generateLighthouseReport/index.ts
+++ packages/scripts/src/generateLighthouseReport/index.ts
@@ -7,43 +7,43 @@ export async function generateLighthouseReport() {
   try {
     const report = await executeLighthouseFlow();
 
-    if (!report) {
-      core.setFailed("Report wasn't generated");
-      return;
-    }
+    //     if (!report) {
+    //       core.setFailed("Report wasn't generated");
+    //       throw new Error("Report wasn't generated");
+    //     }
 
-    core.startGroup("Parse lighthouse report");
-    const parsedReport = parseLighthouseReport(report);
-    core.endGroup();
+    //     core.startGroup("Parse lighthouse report");
+    //     const parsedReport = parseLighthouseReport(report);
+    //     core.endGroup();
 
-    core.startGroup("Post comment");
-    const octokit = github.getOctokit(core.getInput("token"));
+    //     core.startGroup("Post comment");
+    //     const octokit = github.getOctokit(core.getInput("token"));
 
-    const { data: comment } = await octokit.rest.issues.createComment({
-      ...github.context.repo,
-      issue_number: github.context.issue.number,
-      body: `<details>
-<summary>🗼 Click to expand performance result</summary>
+    //     const { data: comment } = await octokit.rest.issues.createComment({
+    //       ...github.context.repo,
+    //       issue_number: github.context.issue.number,
+    //       body: `<details>
+    // <summary>🗼 Click to expand performance result</summary>
 
-<br>
+    // <br>
 
-| Name       | Result  |
-|------------|---------|
-| Performance     | ${parsedReport[0].performance}  |
-| Accessibility     | ${parsedReport[0].accessibility}   |
-| Best Practices    | ${parsedReport[0]["best_practices"]}  |
-| SEO     | ${parsedReport[0].seo}   |
-| First Contentful Paint     | ${parsedReport[0].fcp.displayValue}  |
-| Largest Contentful Paint     | ${parsedReport[0].cls.displayValue}  |
-| Total Blocking Time     | ${parsedReport[0].tbt.displayValue}  |
-| Cumulative Layout Shift     | ${parsedReport[0].cls.displayValue}  |
-| Speed Index     | ${parsedReport[0].speed.displayValue}  |
+    // | Name       | Result  |
+    // |------------|---------|
+    // | Performance     | ${parsedReport[0].performance}  |
+    // | Accessibility     | ${parsedReport[0].accessibility}   |
+    // | Best Practices    | ${parsedReport[0]["best_practices"]}  |
+    // | SEO     | ${parsedReport[0].seo}   |
+    // | First Contentful Paint     | ${parsedReport[0].fcp.displayValue}  |
+    // | Largest Contentful Paint     | ${parsedReport[0].cls.displayValue}  |
+    // | Total Blocking Time     | ${parsedReport[0].tbt.displayValue}  |
+    // | Cumulative Layout Shift     | ${parsedReport[0].cls.displayValue}  |
+    // | Speed Index     | ${parsedReport[0].speed.displayValue}  |
 
-</details>`,
-    });
-    core.info(
-      `Created comment id '${comment.id}' on issue '${github.context.issue.number}'.`
-    );
-    core.endGroup();
+    // </details>`,
+    //     });
+    //     core.info(
+    //       `Created comment id '${comment.id}' on issue '${github.context.issue.number}'.`
+    //     );
+    //     core.endGroup();
   } catch (error) {}
 }
