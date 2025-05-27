7dd4c0d7da15d3f069e23316b829de4be3a85f64
Christophe <contact@christophedeveaux.com>
2025-05-27 21:56:08 +0200

    Parsing

--- packages/scripts/src/generateLighthouseReport/index.ts
+++ packages/scripts/src/generateLighthouseReport/index.ts
@@ -25,6 +25,8 @@ export async function generateLighthouseReport() {
     core.startGroup("Post comment");
     const github = getOctokit(process.env.GITHUB_TOKEN || "");
 
+    core.info(JSON.stringify(parsedNavigationReport));
+    // core.info(JSON.stringify(parsedTimespanReport));
     const { data: comment } = await github.rest.issues.createComment({
       ...context.repo,
       issue_number: context.issue.number,
@@ -48,16 +50,6 @@ Navigation:
 | Total Blocking Time        | ${parsedNavigationReport.tbt.score * 100} (${parsedNavigationReport.tbt.displayValue}) |
 | Cumulative Layout Shift    | ${parsedNavigationReport.cls.score * 100} (${parsedNavigationReport.cls.displayValue}) |
 | Speed Index                | ${parsedNavigationReport.speed.score * 100} (${parsedNavigationReport.speed.displayValue}) |
-
-Timespan:
-| Name                       | Result                          |
-|----------------------------|---------------------------------|
-| Performance                | ${parsedTimespanReport.performance.total * 100}  |
-| Total Blocking Time        | ${parsedTimespanReport.performance.tbt.score * 100} (${parsedTimespanReport.performance.tbt.displayValue}) |
-| Cumulative Layout Shift    | ${parsedTimespanReport.performance.cls.score * 100} (${parsedTimespanReport.performance.cls.displayValue}) |
-| Interaction to Next Paint  | ${parsedTimespanReport.performance.inp.score * 100} (${parsedTimespanReport.performance.inp.displayValue}) |
-| Best practices | ${parsedTimespanReport.best_practices}   |
-| Long tasks | ${parsedTimespanReport.longTasks.total} (${parsedTimespanReport.longTasks.durationMs}ms)   |
 
 </details>
       `,
