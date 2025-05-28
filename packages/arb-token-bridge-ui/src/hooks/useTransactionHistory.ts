3a9e8c6dfe9173e9d9743fbbee4ea8b273612b98
dewanshparashar <dewanshparashar@yahoo.in>
2025-05-28 15:21:48 +0400

    dev: remove console

--- packages/arb-token-bridge-ui/src/hooks/useTransactionHistory.ts
+++ packages/arb-token-bridge-ui/src/hooks/useTransactionHistory.ts
@@ -278,8 +278,6 @@ const useTransactionHistoryWithoutStatuses = (address: Address | undefined) => {
   const { isFeatureDisabled } = useDisabledFeatures()
   const isTxHistoryEnabled = !isFeatureDisabled(DisabledFeatures.TX_HISTORY)
 
-  console.log('xxxx isTxHistoryEnabled', isTxHistoryEnabled)
-
   const forceFetchReceived = useForceFetchReceived(
     state => state.forceFetchReceived
   )
