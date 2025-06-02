46b0584ddbbf2799a312c345a8c77c36727d1380
Christophe <contact@christophedeveaux.com>
2025-06-02 18:10:54 +0000

    WIP

--- packages/scripts/src/generateLighthouseReport/index.ts
+++ packages/scripts/src/generateLighthouseReport/index.ts
@@ -44,7 +44,7 @@ export async function generateLighthouseReport() {
     const { id } = await artifact.uploadArtifact(
       "lhreport",
       ["/lhreport.html"],
-      "."
+      process.cwd()
     );
     const { downloadPath } = await artifact.downloadArtifact(id);
     core.info(`Created artifact with id: ${id}, (${downloadPath})`);
