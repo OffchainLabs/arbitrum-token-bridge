75a1dc377a350663789e44608ae2908a957f7692
dewanshparashar <dewanshparashar@yahoo.in>
2025-06-04 20:28:08 +0400

    dev: experimental redirect

--- packages/arb-token-bridge-ui/src/components/TransferPanel/ConnectWalletButton.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/ConnectWalletButton.tsx
@@ -1,17 +1,31 @@
 import { useConnectModal } from '@rainbow-me/rainbowkit'
-
+import { useArbQueryParams } from '../../hooks/useArbQueryParams'
+import { isMobileBrowser } from '../../util/CommonUtils'
 import { Button } from '../common/Button'
 
 export function ConnectWalletButton() {
   const { openConnectModal } = useConnectModal()
+  const [{ embedMode }] = useArbQueryParams()
+  const isMobile = isMobileBrowser()
+
+  const handleClick = () => {
+    if (embedMode && isMobile) {
+      // Redirect to bridge.arbitrum.io in mobile browser when in embed mode
+      window.location.href = 'https://bridge.arbitrum.io'
+    } else if (openConnectModal) {
+      openConnectModal()
+    }
+  }
 
   return (
     <Button
       variant="primary"
-      onClick={openConnectModal}
+      onClick={handleClick}
       className="w-full border border-lime-dark bg-lime-dark py-3 text-lg lg:text-2xl"
     >
-      <span className="block w-full truncate">Connect Wallet</span>
+      <span className="block w-full truncate">
+        {embedMode && isMobile ? 'Go to Arbitrum Bridge' : 'Connect Wallet'}
+      </span>
     </Button>
   )
 }
