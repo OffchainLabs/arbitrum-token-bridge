44184516f7f414d26dae5d5421712fbe372b431c
dewanshparashar <dewanshparashar@yahoo.in>
2025-06-04 15:16:03 +0400

    dev: refactor route component

--- packages/arb-token-bridge-ui/src/components/TransferPanel/Routes/Route.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/Routes/Route.tsx
@@ -6,7 +6,10 @@ import { twMerge } from 'tailwind-merge'
 import { formatAmount, formatUSD } from '../../../util/NumberUtils'
 import { Loader } from '../../common/atoms/Loader'
 import { useSelectedToken } from '../../../hooks/useSelectedToken'
-import { useNativeCurrency } from '../../../hooks/useNativeCurrency'
+import {
+  useNativeCurrency,
+  NativeCurrency
+} from '../../../hooks/useNativeCurrency'
 import { useNetworks } from '../../../hooks/useNetworks'
 import { useNetworksRelationship } from '../../../hooks/useNetworksRelationship'
 import { RouteType, SetRoute } from '../hooks/useRouteStore'
@@ -14,7 +17,6 @@ import { TokenLogo } from '../TokenLogo'
 import React, { PropsWithChildren } from 'react'
 import { useArbQueryParams } from '../../../hooks/useArbQueryParams'
 import { useIsBatchTransferSupported } from '../../../hooks/TransferPanel/useIsBatchTransferSupported'
-
 import { useETHPrice } from '../../../hooks/useETHPrice'
 import { isNetwork } from '../../../util/networks'
 import { Tooltip } from '../../common/Tooltip'
@@ -24,6 +26,7 @@ import { shortenAddress } from '../../../util/CommonUtils'
 import { useAppContextState } from '../../App/AppContext'
 import { CompactRouteDisplay } from './CompactRouteDisplay'
 
+// Types
 export type BadgeType = 'security-guaranteed' | 'best-deal' | 'fastest'
 export type Token = {
   address: string
@@ -37,9 +40,7 @@ export type RouteProps = {
   amountReceived: string
   durationMs: number
   isLoadingGasEstimate: boolean
-  /** Allow overrides of displayed token */
   overrideToken?: Token
-  /** We might have multiple gas token, for example an ER20 deposit to XAI from Arb1 */
   gasCost: RouteGas[] | undefined
   bridgeFee?: { fee: string | undefined; token: Token }
   bridge: string
@@ -49,6 +50,7 @@ export type RouteProps = {
   onSelectedRouteClick: SetRoute
 }
 
+// Badge Components
 function Tag({
   children,
   className
@@ -97,10 +99,225 @@ function getBadges(badgeTypes: BadgeType | BadgeType[]) {
   if (Array.isArray(badgeTypes)) {
     return badgeTypes.map(getBadgeFromBadgeType)
   }
-
   return getBadgeFromBadgeType(badgeTypes)
 }
 
+// Route Amount Component
+type RouteAmountProps = {
+  destinationAddress?: string
+  amountReceived: string
+  token: Token | NativeCurrency
+  showUsdValueForReceivedToken: boolean
+  ethToUSD: (amount: number) => number
+  isBatchTransferSupported: boolean
+  amount2?: string
+  childNativeCurrency: Token | NativeCurrency
+}
+
+const RouteAmount = ({
+  destinationAddress,
+  amountReceived,
+  token,
+  showUsdValueForReceivedToken,
+  ethToUSD,
+  isBatchTransferSupported,
+  amount2,
+  childNativeCurrency
+}: RouteAmountProps) => (
+  <div className="flex flex-col md:min-w-36">
+    <span className="flex gap-1">
+      {destinationAddress ? (
+        <Tooltip content={destinationAddress}>
+          {shortenAddress(destinationAddress)}
+        </Tooltip>
+      ) : (
+        'You'
+      )}{' '}
+      will receive:
+    </span>
+    <div className="flex flex-col text-lg">
+      <div className="flex flex-row items-center gap-1">
+        <TokenLogo
+          srcOverride={'logoURI' in token ? token.logoURI : null}
+          fallback={
+            <div className="h-5 w-5 min-w-5 rounded-full bg-gray-dark" />
+          }
+        />
+        {formatAmount(Number(amountReceived))} {token.symbol}
+        <div className="text-sm">
+          {showUsdValueForReceivedToken && (
+            <div className="text-sm tabular-nums opacity-80">
+              {formatUSD(ethToUSD(Number(amountReceived)))}
+            </div>
+          )}
+        </div>
+      </div>
+      {isBatchTransferSupported && Number(amount2) > 0 && (
+        <div className="flew-row flex items-center gap-1">
+          <TokenLogo
+            srcOverride={null}
+            fallback={
+              <div className="h-5 w-5 min-w-5 rounded-full bg-gray-dark" />
+            }
+          />
+          {formatAmount(Number(amount2), {
+            symbol: childNativeCurrency.symbol
+          })}
+        </div>
+      )}
+    </div>
+  </div>
+)
+
+// Route Duration Component
+type RouteDurationProps = {
+  durationMs: number
+  fastWithdrawalActive: boolean
+}
+
+const RouteDuration = ({
+  durationMs,
+  fastWithdrawalActive
+}: RouteDurationProps) => (
+  <div className="flex items-center">
+    <ClockIcon width={18} height={18} className="-ml-[1px]" />
+    <span className="ml-1 whitespace-nowrap">
+      {dayjs().add(durationMs, 'millisecond').fromNow(true)}
+    </span>
+    {fastWithdrawalActive && (
+      <div className="flex items-center">
+        <Tooltip
+          content={
+            'Fast Withdrawals relies on a committee of validators. In the event of a committee outage, your withdrawal falls back to the 7 day challenge period secured by Arbitrum Fraud Proofs.'
+          }
+        >
+          <InformationCircleIcon className="ml-1 h-3 w-3" />
+        </Tooltip>
+      </div>
+    )}
+  </div>
+)
+
+// Route Bridge Component
+type RouteBridgeProps = {
+  bridge: string
+  bridgeIconURI: string
+}
+
+const RouteBridge = ({ bridge, bridgeIconURI }: RouteBridgeProps) => (
+  <div className="flex min-w-0 items-center">
+    <SafeImage
+      src={bridgeIconURI}
+      width={15}
+      height={15}
+      alt="bridge"
+      className="max-h-3 max-w-3 rounded-full"
+      fallback={<div className="h-3 w-3 min-w-3 rounded-full bg-gray-dark" />}
+    />
+    <div className="truncate">
+      <span className="ml-1 whitespace-nowrap">{bridge}</span>
+    </div>
+  </div>
+)
+
+// Route Fees Component
+type RouteFeesProps = {
+  isLoadingGasEstimate: boolean
+  gasCost: RouteGas[] | undefined
+  gasEth?: RouteGas | false
+  ethToUSD: (amount: number) => number
+  bridgeFee?: { fee: string | undefined; token: Token }
+  showUSDValueForBridgeFee: boolean | undefined
+}
+
+const RouteFees = ({
+  isLoadingGasEstimate,
+  gasCost,
+  gasEth,
+  ethToUSD,
+  bridgeFee,
+  showUSDValueForBridgeFee
+}: RouteFeesProps) => (
+  <>
+    <Tooltip content={'The gas fees paid to operate the network'}>
+      <div className="flex items-center">
+        <Image src="/icons/gas.svg" width={15} height={15} alt="gas" />
+        <span className="ml-1">
+          {isLoadingGasEstimate ? (
+            <Loader size="small" color="white" />
+          ) : gasCost ? (
+            <div className="flex items-center gap-1" aria-label="Route gas">
+              {gasCost
+                .map(({ gasCost, gasToken }) =>
+                  formatAmount(BigNumber.from(gasCost), {
+                    decimals: gasToken.decimals,
+                    symbol: gasToken.symbol
+                  })
+                )
+                .join(' and ')}
+              {gasEth && (
+                <div className="text-sm tabular-nums opacity-80">
+                  {formatUSD(
+                    ethToUSD(
+                      Number(utils.formatEther(BigNumber.from(gasEth.gasCost)))
+                    )
+                  )}
+                </div>
+              )}
+            </div>
+          ) : (
+            <div aria-label="Route gas">{'N/A'}</div>
+          )}
+        </span>
+      </div>
+    </Tooltip>
+
+    {bridgeFee && (
+      <Tooltip content={'The fee the bridge takes'}>
+        <div className="flex items-center gap-1">
+          <Image
+            src="/icons/bridge.svg"
+            width={15}
+            height={15}
+            alt="bridge fee"
+          />
+          <span>
+            {formatAmount(BigNumber.from(bridgeFee.fee), {
+              decimals: bridgeFee.token.decimals,
+              symbol: bridgeFee.token.symbol
+            })}
+          </span>
+          {showUSDValueForBridgeFee && (
+            <div className="text-sm tabular-nums opacity-80">
+              {formatUSD(
+                ethToUSD(
+                  Number(utils.formatEther(BigNumber.from(bridgeFee.fee)))
+                )
+              )}
+            </div>
+          )}
+        </div>
+      </Tooltip>
+    )}
+  </>
+)
+
+// Route Badge Component
+type RouteBadgeProps = {
+  tag?: BadgeType | BadgeType[]
+}
+
+const RouteBadge = ({ tag }: RouteBadgeProps) => {
+  if (!tag) return null
+
+  return (
+    <div className="invisible absolute right-2 top-2 flex gap-1 md:visible">
+      {getBadges(tag)}
+    </div>
+  )
+}
+
+// Main Route Component
 export const Route = React.memo(
   ({
     type,
@@ -129,27 +346,29 @@ export const Route = React.memo(
     const [{ amount2, destinationAddress, embedMode }] = useArbQueryParams()
     const isBatchTransferSupported = useIsBatchTransferSupported()
 
-    const token = overrideToken || _token || childNativeCurrency
+    const token = (overrideToken || _token || childNativeCurrency) as Token
 
     const { isTestnet } = isNetwork(networks.sourceChain.id)
     const { ethToUSD } = useETHPrice()
-    // Only display USD values for ETH
     const showUsdValueForReceivedToken = !isTestnet && !('address' in token)
 
     const { fastWithdrawalActive } = !isDepositMode
       ? getConfirmationTime(networks.sourceChain.id)
       : { fastWithdrawalActive: false }
 
-    /* Only show USD values if gas is paid in ETH and we're not on testnet */
     const gasEth =
-      !isTestnet &&
-      gasCost &&
-      gasCost.find(({ gasToken }) => gasToken.address === constants.AddressZero)
+      (!isTestnet &&
+        gasCost &&
+        gasCost.find(
+          ({ gasToken }) => gasToken.address === constants.AddressZero
+        )) ||
+      undefined
 
     const showUSDValueForBridgeFee =
-      !isTestnet &&
-      bridgeFee &&
-      bridgeFee.token.address === constants.AddressZero
+      (!isTestnet &&
+        bridgeFee &&
+        bridgeFee.token.address === constants.AddressZero) ||
+      false
 
     if (embedMode) {
       return (
@@ -184,165 +403,41 @@ export const Route = React.memo(
         className={twMerge(
           'relative flex max-w-[calc(100vw_-_40px)] flex-col gap-4 rounded bg-[#303030] px-4 py-3 text-left text-sm text-white ring-1 ring-[#ffffff33] transition-colors md:flex-row',
           'focus-visible:!outline-none',
-          'focus-within:bg-[#474747] hover:bg-[#474747]', // focused state
+          'focus-within:bg-[#474747] hover:bg-[#474747]',
           !isDisabled && selected && 'bg-[#474747] ring-[#5F7D5B]'
         )}
         onClick={() => onSelectedRouteClick(type)}
         disabled={isDisabled}
         aria-label={`Route ${type}`}
       >
-        <div className="flex flex-col md:min-w-36">
-          <span className="flex gap-1">
-            {destinationAddress ? (
-              <Tooltip content={destinationAddress}>
-                {shortenAddress(destinationAddress)}
-              </Tooltip>
-            ) : (
-              'You'
-            )}{' '}
-            will receive:
-          </span>
-          <div className="flex flex-col text-lg">
-            <div className="flex flex-row items-center gap-1">
-              <TokenLogo
-                srcOverride={'logoURI' in token ? token.logoURI : null}
-                fallback={
-                  <div className="h-5 w-5 min-w-5 rounded-full bg-gray-dark" />
-                }
-              />
-              {formatAmount(Number(amountReceived))} {token.symbol}
-              <div className="text-sm">
-                {showUsdValueForReceivedToken && (
-                  <div className="text-sm tabular-nums opacity-80">
-                    {formatUSD(ethToUSD(Number(amountReceived)))}
-                  </div>
-                )}
-              </div>
-            </div>
-            {isBatchTransferSupported && Number(amount2) > 0 && (
-              <div className="flew-row flex items-center gap-1">
-                <TokenLogo
-                  srcOverride={null}
-                  fallback={
-                    <div className="h-5 w-5 min-w-5 rounded-full bg-gray-dark" />
-                  }
-                />
-                {formatAmount(Number(amount2), {
-                  symbol: childNativeCurrency.symbol
-                })}
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-col justify-between gap-3">
-          <div className="flex flex-row gap-3 md:flex-col md:justify-between">
-            <div className="flex items-center">
-              <ClockIcon width={18} height={18} className="-ml-[1px]" />
-              <span className="ml-1 whitespace-nowrap">
-                {dayjs().add(durationMs, 'millisecond').fromNow(true)}
-              </span>
-              {fastWithdrawalActive && (
-                <div className="flex items-center">
-                  <Tooltip
-                    content={
-                      'Fast Withdrawals relies on a committee of validators. In the event of a committee outage, your withdrawal falls back to the 7 day challenge period secured by Arbitrum Fraud Proofs.'
-                    }
-                  >
-                    <InformationCircleIcon className="ml-1 h-3 w-3" />
-                  </Tooltip>
-                </div>
-              )}
-            </div>
+        <RouteAmount
+          destinationAddress={destinationAddress}
+          amountReceived={amountReceived}
+          token={token}
+          showUsdValueForReceivedToken={showUsdValueForReceivedToken}
+          ethToUSD={ethToUSD}
+          isBatchTransferSupported={isBatchTransferSupported}
+          amount2={amount2}
+          childNativeCurrency={childNativeCurrency}
+        />
 
-            <div className="flex min-w-0 items-center">
-              <SafeImage
-                src={bridgeIconURI}
-                width={15}
-                height={15}
-                alt="bridge"
-                className="max-h-3 max-w-3 rounded-full"
-                fallback={
-                  <div className="h-3 w-3 min-w-3 rounded-full bg-gray-dark" />
-                }
-              />
-              <div className="truncate">
-                <span className="ml-1 whitespace-nowrap">{bridge}</span>
-              </div>
-            </div>
-          </div>
-
-          <Tooltip content={'The gas fees paid to operate the network'}>
-            <div className="flex items-center">
-              <Image src="/icons/gas.svg" width={15} height={15} alt="gas" />
-              <span className="ml-1">
-                {isLoadingGasEstimate ? (
-                  <Loader size="small" color="white" />
-                ) : gasCost ? (
-                  <div
-                    className="flex items-center gap-1"
-                    aria-label="Route gas"
-                  >
-                    {gasCost
-                      .map(({ gasCost, gasToken }) =>
-                        formatAmount(BigNumber.from(gasCost), {
-                          decimals: gasToken.decimals,
-                          symbol: gasToken.symbol
-                        })
-                      )
-                      .join(' and ')}
-                    {gasEth && (
-                      <div className="text-sm tabular-nums opacity-80">
-                        {formatUSD(
-                          ethToUSD(
-                            Number(
-                              utils.formatEther(BigNumber.from(gasEth.gasCost))
-                            )
-                          )
-                        )}
-                      </div>
-                    )}
-                  </div>
-                ) : (
-                  <div aria-label="Route gas">{'N/A'}</div>
-                )}
-              </span>
-            </div>
-          </Tooltip>
-
-          {bridgeFee && (
-            <Tooltip content={'The fee the bridge takes'}>
-              <div className="flex items-center gap-1">
-                <Image
-                  src="/icons/bridge.svg"
-                  width={15}
-                  height={15}
-                  alt="bridge fee"
-                />
-                <span>
-                  {formatAmount(BigNumber.from(bridgeFee.fee), {
-                    decimals: bridgeFee.token.decimals,
-                    symbol: bridgeFee.token.symbol
-                  })}
-                </span>
-                {showUSDValueForBridgeFee && (
-                  <div className="text-sm tabular-nums opacity-80">
-                    {formatUSD(
-                      ethToUSD(
-                        Number(utils.formatEther(BigNumber.from(bridgeFee.fee)))
-                      )
-                    )}
-                  </div>
-                )}
-              </div>
-            </Tooltip>
-          )}
+        <div className="flex flex-row flex-wrap gap-3 md:flex-col md:justify-between">
+          <RouteDuration
+            durationMs={durationMs}
+            fastWithdrawalActive={fastWithdrawalActive}
+          />
+          <RouteBridge bridge={bridge} bridgeIconURI={bridgeIconURI} />
+          <RouteFees
+            isLoadingGasEstimate={isLoadingGasEstimate}
+            gasCost={gasCost}
+            gasEth={gasEth}
+            ethToUSD={ethToUSD}
+            bridgeFee={bridgeFee}
+            showUSDValueForBridgeFee={showUSDValueForBridgeFee}
+          />
         </div>
 
-        {tag ? (
-          <div className="invisible absolute right-2 top-2 flex gap-1 md:visible">
-            {getBadges(tag)}
-          </div>
-        ) : null}
+        <RouteBadge tag={tag} />
       </button>
     )
   }
