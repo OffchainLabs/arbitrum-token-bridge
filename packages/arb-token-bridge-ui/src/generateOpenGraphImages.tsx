f3c50790667ca4da9ba9c3c5c8636e4de4ed90cd
Bartek <bartlomiejkramek@outlook.com>
2025-06-04 15:46:00 +0200

    fix

--- packages/arb-token-bridge-ui/src/generateOpenGraphImages.tsx
+++ packages/arb-token-bridge-ui/src/generateOpenGraphImages.tsx
@@ -11,6 +11,7 @@ import { hideBin } from 'yargs/helpers'
 // to ensure that the environment variables are loaded
 dotenv.config({ path: path.resolve(__dirname, '../.env') })
 
+import { isNetwork } from './util/networks'
 import { ChainId } from './types/ChainId'
 import { getBridgeUiConfigForChain } from './util/bridgeUiConfig'
 import { orbitMainnets, orbitTestnets } from './util/orbitChainsList'
@@ -356,19 +357,9 @@ const generateOptions = {
 
 type Args = Arguments<InferredOptionTypes<typeof generateOptions>>
 
-function isCoreChain(chainId: number) {
-  return [
-    ChainId.Ethereum,
-    ChainId.Sepolia,
-    ChainId.ArbitrumOne,
-    ChainId.ArbitrumNova,
-    ChainId.ArbitrumSepolia
-  ].includes(chainId)
-}
-
 async function generate(argv: Args) {
   for (const combination of configs) {
-    const isChildChainCoreChain = isCoreChain(combination[1])
+    const { isCoreChain: isChildChainCoreChain } = isNetwork(combination[1])
 
     if (argv.orbit && !isChildChainCoreChain) {
       await generateSvg(combination[1])
