ee3cfaf0024c7147bd7928040f5651a33ba7e371
dewanshparashar <dewanshparashar@yahoo.in>
2025-06-09 16:26:18 +0400

    btn style fix

--- packages/arb-token-bridge-ui/src/components/TransferPanel/ConnectWalletButton.tsx
+++ packages/arb-token-bridge-ui/src/components/TransferPanel/ConnectWalletButton.tsx
@@ -11,7 +11,7 @@ export function ConnectWalletButton({ onClick }: { onClick?: () => void }) {
       onClick={onClick ?? openConnectModal}
       className="w-full border border-lime-dark bg-lime-dark py-3 text-lg lg:text-2xl"
     >
-      <span className="block w-[360px] truncate">Connect Wallet</span>
+      <span className="block w-full truncate">Connect Wallet</span>
     </Button>
   )
 }
