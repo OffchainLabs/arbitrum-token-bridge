e0faf2dd3b956504b402a10ab3e577f706d968df
dewanshparashar <dewanshparashar@yahoo.in>
2025-06-20 13:50:22 +0400

    dev: enhanced comment

--- packages/arb-token-bridge-ui/src/util/WithdrawOnlyUtils.ts
+++ packages/arb-token-bridge-ui/src/util/WithdrawOnlyUtils.ts
@@ -305,7 +305,7 @@ async function isLayerZeroToken(
   }
 
   try {
-    // Fetches LayerZero's off-chain metadata to identify OFT tokens.
+    // Fetches LayerZero's off-chain metadata (https://metadata.layerzero-api.com/v1/metadata) to identify OFT tokens.
     // If found in the metadata, it means the token supports OFT transfers - hence shouldn't be deposited through Arbitrum's canonical bridge.
     // Schema:
     // {
