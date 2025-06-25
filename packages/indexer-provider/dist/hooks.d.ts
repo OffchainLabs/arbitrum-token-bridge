4b9a822c798c97b12fd0f59e127d9e07ca1e3e81
Bartek <bartlomiejkramek@outlook.com>
2025-06-25 15:23:53 +0200

    remove package

--- packages/indexer-provider/dist/hooks.d.ts
+++
@@ -1,2 +0,0 @@
-import type { UseArbitrumIndexerResult } from './types';
-export declare function useArbitrumIndexer(address: string): UseArbitrumIndexerResult;
--- packages/indexer-provider/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { ArbitrumIndexerProvider, useArbitrumIndexerContext } from './context';
-export { useArbitrumIndexer } from './hooks';
-export type { Transfer, TransferStatus, UseArbitrumIndexerResult, ArbitrumIndexerConfig, } from './types';
-export { buildTransfersQuery, executeGraphQLQuery, normalizeTransfers, categorizeTransfers, } from './queries';
