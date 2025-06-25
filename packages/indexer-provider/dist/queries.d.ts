1f67b5ee181432b96fc1e699c4445f786d3ab300
Bartek <bartlomiejkramek@outlook.com>
2025-06-25 15:23:53 +0200

    remove package

--- packages/indexer-provider/dist/queries.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Transfer } from './types';
-export declare const buildTransfersQuery: (address: string) => string;
-export declare const executeGraphQLQuery: (apiUrl: string, query: string) => Promise<any>;
-export declare const normalizeTransfers: (erc20Data: any[], ethData: any[]) => Transfer[];
-export declare const categorizeTransfers: (transfers: Transfer[]) => {
-    pending: Transfer[];
-    completed: Transfer[];
-};
