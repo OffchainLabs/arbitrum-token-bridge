5c8f06274a0476486efd9867f64b4e14c44e9ef2
dewanshparashar <dewanshparashar@yahoo.in>
2025-07-01 19:04:52 +0530

    dev: add the flag to disabled-features hook

--- packages/arb-token-bridge-ui/src/hooks/useDisabledFeatures.ts
+++ packages/arb-token-bridge-ui/src/hooks/useDisabledFeatures.ts
@@ -2,10 +2,14 @@ import { useCallback } from 'react'
 import { useArbQueryParams, DisabledFeatures } from './useArbQueryParams'
 
 export const useDisabledFeatures = () => {
-  const [{ disabledFeatures }] = useArbQueryParams()
+  const [{ disabledFeatures, mode }] = useArbQueryParams()
 
   const isFeatureDisabled = useCallback(
     (feature: DisabledFeatures) => {
+      if (feature === DisabledFeatures.TRANSFERS_TO_NON_ARBITRUM_CHAINS) {
+        return typeof mode !== 'undefined'
+      }
+
       return (disabledFeatures as readonly DisabledFeatures[]).includes(feature)
     },
     [disabledFeatures]
