884fd73dec81aee8d8cda9b54df2c4add48eec44
Christophe <contact@christophedeveaux.com>
2025-08-13 11:00:54 +0000

    fix: Remove missing orbit chain in network tests

--- packages/arb-token-bridge-ui/src/util/__tests__/networks.test.ts
+++ packages/arb-token-bridge-ui/src/util/__tests__/networks.test.ts
@@ -20,75 +20,76 @@ import { orbitTestnets, orbitMainnets } from '../orbitChainsList'
 import { createMockOrbitChain } from '../../hooks/__tests__/helpers'
 
 const xaiTestnetChainId = 37714555429
-
 beforeAll(() => {
-  const xaiTestnet = orbitTestnets[xaiTestnetChainId]
+  function setupOrbitChains() {
+    const xaiTestnet = orbitTestnets[xaiTestnetChainId]
 
-  if (!xaiTestnet) {
-    throw new Error(`Could not find Xai Testnet in the Orbit chains list.`)
-  }
-
-  // add local
-  registerCustomArbitrumNetwork({
-    chainId: 412346,
-    confirmPeriodBlocks: 20,
-    ethBridge: {
-      bridge: '0x2b360a9881f21c3d7aa0ea6ca0de2a3341d4ef3c',
-      inbox: '0xff4a24b22f94979e9ba5f3eb35838aa814bad6f1',
-      outbox: '0x49940929c7cA9b50Ff57a01d3a92817A414E6B9B',
-      rollup: '0x65a59d67da8e710ef9a01eca37f83f84aedec416',
-      sequencerInbox: '0xe7362d0787b51d8c72d504803e5b1d6dcda89540'
-    },
-    isCustom: true,
-    isTestnet: true,
-    name: 'Nitro Testnode L2',
-    parentChainId: 1337,
-    tokenBridge: {
-      parentCustomGateway: '0x75E0E92A79880Bd81A69F72983D03c75e2B33dC8',
-      parentErc20Gateway: '0x4Af567288e68caD4aA93A272fe6139Ca53859C70',
-      parentGatewayRouter: '0x85D9a8a4bd77b9b5559c1B7FCb8eC9635922Ed49',
-      parentMultiCall: '0xA39FFA43ebA037D67a0f4fe91956038ABA0CA386',
-      parentProxyAdmin: '0x7E32b54800705876d3b5cFbc7d9c226a211F7C1a',
-      parentWeth: '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48',
-      parentWethGateway: '0x408Da76E87511429485C32E4Ad647DD14823Fdc4',
-      childCustomGateway: '0x525c2aBA45F66987217323E8a05EA400C65D06DC',
-      childErc20Gateway: '0xe1080224B632A93951A7CFA33EeEa9Fd81558b5e',
-      childGatewayRouter: '0x1294b86822ff4976BfE136cB06CF43eC7FCF2574',
-      childMultiCall: '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48',
-      childProxyAdmin: '0xda52b25ddB0e3B9CC393b0690Ac62245Ac772527',
-      childWeth: '0x408Da76E87511429485C32E4Ad647DD14823Fdc4',
-      childWethGateway: '0x4A2bA922052bA54e29c5417bC979Daaf7D5Fe4f4'
+    if (!xaiTestnet) {
+      throw new Error(`Could not find Xai Testnet in the Orbit chains list.`)
     }
-  })
 
-  registerCustomArbitrumNetwork(xaiTestnet)
+    // add local
+    registerCustomArbitrumNetwork({
+      chainId: 412346,
+      confirmPeriodBlocks: 20,
+      ethBridge: {
+        bridge: '0x2b360a9881f21c3d7aa0ea6ca0de2a3341d4ef3c',
+        inbox: '0xff4a24b22f94979e9ba5f3eb35838aa814bad6f1',
+        outbox: '0x49940929c7cA9b50Ff57a01d3a92817A414E6B9B',
+        rollup: '0x65a59d67da8e710ef9a01eca37f83f84aedec416',
+        sequencerInbox: '0xe7362d0787b51d8c72d504803e5b1d6dcda89540'
+      },
+      isCustom: true,
+      isTestnet: true,
+      name: 'Nitro Testnode L2',
+      parentChainId: 1337,
+      tokenBridge: {
+        parentCustomGateway: '0x75E0E92A79880Bd81A69F72983D03c75e2B33dC8',
+        parentErc20Gateway: '0x4Af567288e68caD4aA93A272fe6139Ca53859C70',
+        parentGatewayRouter: '0x85D9a8a4bd77b9b5559c1B7FCb8eC9635922Ed49',
+        parentMultiCall: '0xA39FFA43ebA037D67a0f4fe91956038ABA0CA386',
+        parentProxyAdmin: '0x7E32b54800705876d3b5cFbc7d9c226a211F7C1a',
+        parentWeth: '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48',
+        parentWethGateway: '0x408Da76E87511429485C32E4Ad647DD14823Fdc4',
+        childCustomGateway: '0x525c2aBA45F66987217323E8a05EA400C65D06DC',
+        childErc20Gateway: '0xe1080224B632A93951A7CFA33EeEa9Fd81558b5e',
+        childGatewayRouter: '0x1294b86822ff4976BfE136cB06CF43eC7FCF2574',
+        childMultiCall: '0xDB2D15a3EB70C347E0D2C2c7861cAFb946baAb48',
+        childProxyAdmin: '0xda52b25ddB0e3B9CC393b0690Ac62245Ac772527',
+        childWeth: '0x408Da76E87511429485C32E4Ad647DD14823Fdc4',
+        childWethGateway: '0x4A2bA922052bA54e29c5417bC979Daaf7D5Fe4f4'
+      }
+    })
 
-  const xmtpTestnetChainId = 241320161
-  const xmtpTestnet = orbitTestnets[xmtpTestnetChainId]
+    registerCustomArbitrumNetwork(xaiTestnet)
 
-  if (!xmtpTestnet) {
-    throw new Error(
-      `Could not find XMTP Testnet Testnet in the Orbit chains list.`
-    )
+    const uniteTestnetChainId = 888991
+    const uniteTestnet = orbitTestnets[uniteTestnetChainId]
+
+    if (!uniteTestnet) {
+      throw new Error(`Could not find unite Testnet in the Orbit chains list.`)
+    }
+
+    registerCustomArbitrumNetwork(uniteTestnet)
+
+    const apeChain = orbitMainnets[ChainId.ApeChain]
+
+    if (!apeChain) {
+      throw new Error(`Could not find Ape Chain in the Orbit chains list.`)
+    }
+
+    registerCustomArbitrumNetwork(apeChain)
+
+    const superposition = orbitMainnets[ChainId.Superposition]
+
+    if (!superposition) {
+      throw new Error(`Could not find Superposition in the Orbit chains list.`)
+    }
+
+    registerCustomArbitrumNetwork(superposition)
   }
 
-  registerCustomArbitrumNetwork(xmtpTestnet)
-
-  const apeChain = orbitMainnets[ChainId.ApeChain]
-
-  if (!apeChain) {
-    throw new Error(`Could not find Ape Chain in the Orbit chains list.`)
-  }
-
-  registerCustomArbitrumNetwork(apeChain)
-
-  const superposition = orbitMainnets[ChainId.Superposition]
-
-  if (!superposition) {
-    throw new Error(`Could not find Superposition in the Orbit chains list.`)
-  }
-
-  registerCustomArbitrumNetwork(superposition)
+  expect(setupOrbitChains).not.toThrowError()
 })
 
 describe('getBlockNumberReferenceChainIdByChainId', () => {
@@ -355,7 +356,7 @@ describe('getDestinationChainIds', () => {
     const defaultChainId = destinationChainIds[0]
     const nonDefaultChainIds = destinationChainIds.slice(1)
 
-    expect(defaultChainId).toBe(241320161)
+    expect(defaultChainId).toBe(888991)
     expect(isAscending(nonDefaultChainIds)).toBe(true)
   })
 
