d66d2a946591cc426feba9814b336ecd3ace8008
Christophe <contact@christophedeveaux.com>
2025-09-10 13:02:42 +0000

    test

--- packages/arb-token-bridge-ui/src/hooks/__tests__/useTransactionHistory.test.ts
+++ packages/arb-token-bridge-ui/src/hooks/__tests__/useTransactionHistory.test.ts
@@ -133,6 +133,7 @@ describe.sequential('useTransactionHistory', () => {
 
       // finally, no more transactions left to be fetched
       expect(result.current.completed).toBe(true)
+      expect(3).toBe(2)
     }
   )
 })
