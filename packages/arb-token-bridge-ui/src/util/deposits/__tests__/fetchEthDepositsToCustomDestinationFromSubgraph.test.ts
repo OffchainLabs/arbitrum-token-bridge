dfaf8a60e9ce5137ef4d375ce4a2963315c8d8e9
Christophe Deveaux <contact@christophedeveaux.com>
2025-09-11 18:06:15 +0200

    fix: migrate subgraph test to mainnet (#2632)

--- packages/arb-token-bridge-ui/src/util/deposits/__tests__/fetchEthDepositsToCustomDestinationFromSubgraph.test.ts
+++ packages/arb-token-bridge-ui/src/util/deposits/__tests__/fetchEthDepositsToCustomDestinationFromSubgraph.test.ts
@@ -19,12 +19,16 @@ describe('fetchEthDepositsToCustomDestinationFromSubgraph', () => {
       getQueryCoveringNitroWithResults()
     )
 
-    expect(result).toHaveLength(1)
+    expect(result).toHaveLength(2)
     expect(result).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
           transactionHash:
-            '0x309c2f9778797445dc3b9da63be5569bb33edf88222775060532bc85edce1897'
+            '0x49db903399a3b4caa9d99bbac4ca704bd4bad3d247c2d26ccdd0296c96ebf7dd'
+        }),
+        expect.objectContaining({
+          transactionHash:
+            '0x6429e7cb8fa7f501d336ec0672755404c9021abe5d19ae07ad875ad2f1ae6537'
         })
       ])
     )
